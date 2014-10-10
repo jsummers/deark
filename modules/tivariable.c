2320be2c6a392d7771feb1e1cf395c512bc0c81a
Jason Summers <jason1@pobox.com>
2014-10-10 10:48:00 -0400

    tivariable: Support TI83 pictures

--- modules/tivariable.c
+++ modules/tivariable.c
@@ -9,6 +9,9 @@ typedef struct localctx_struct {
 #define DE_FMT_TI92  1
 #define DE_FMT_TI89  2
 #define DE_FMT_TI92P 3
+#define DE_FMT_TI83  4
+#define DE_FMT_TI83F 5
+#define DE_FMT_TI_UNKNOWN 100
 	int fmt;
 } lctx;
 
@@ -17,9 +20,12 @@ static int identify_internal(deark *c)
 	de_byte buf[8];
 
 	de_read(buf, 0, 8);
-	if(!de_memcmp(buf, "**TI92**", 8)) return DE_FMT_TI92;
+	if(!de_memcmp(buf, "**TI83**", 8)) return DE_FMT_TI83;
+	if(!de_memcmp(buf, "**TI83F*", 8)) return DE_FMT_TI83F;
 	if(!de_memcmp(buf, "**TI89**", 8)) return DE_FMT_TI89;
+	if(!de_memcmp(buf, "**TI92**", 8)) return DE_FMT_TI92;
 	if(!de_memcmp(buf, "**TI92P*", 8)) return DE_FMT_TI92P;
+	if(!de_memcmp(buf, "**TI", 4)) return DE_FMT_TI_UNKNOWN;
 	return 0;
 }
 
@@ -52,6 +58,18 @@ done:
 	return retval;
 }
 
+static int do_ti83_picture_var(deark *c, lctx *d, de_int64 pos)
+{
+	de_int64 x;
+
+	de_dbg(c, "picture at %d\n", (int)pos);
+	x = de_getui16le(pos);
+	de_dbg(c, "picture size: %d\n", (int)x);
+	d->w = 95;
+	d->h = 63;
+	return do_bitmap(c, d, pos+2);
+}
+
 static int do_ti92_picture_var(deark *c, lctx *d, de_int64 pos)
 {
 	de_int64 x;
@@ -82,6 +100,48 @@ static void do_ti92_var_table_entry(deark *c, lctx *d, de_int64 pos)
 	}
 	de_dbg(c, "data offset: %d\n", (int)data_offset);
 	do_ti92_picture_var(c, d, data_offset);
+}
+
+static void do_ti83(deark *c, lctx *d)
+{
+	de_int64 pos;
+	de_int64 data_section_size;
+	de_int64 data_section_end;
+	de_int64 var_data_size;
+	de_byte type_id;
+
+	// 0-7: signature
+	// 8-10: 0x1a 0x0a 0x00
+	// 11-52: comment
+
+	data_section_size = de_getui16le(53);
+	de_dbg(c, "data section size: %d\n", (int)data_section_size);
+	data_section_end = 55+data_section_size;
+	if(data_section_end > c->infile->len) {
+		de_err(c, "Data section goes beyond end of file\n");
+		goto done;
+	}
+
+	// Read the variables
+	pos = 55;
+	while(pos < data_section_end) {
+		var_data_size = de_getui16le(pos+2);
+		type_id = de_getbyte(pos+4);
+		pos += 15;
+		if(d->fmt==DE_FMT_TI83F)
+			pos += 2;
+		de_dbg(c, "var type=0x%02x pos=%d len=%d\n", (unsigned int)type_id,
+			(int)pos, (int)var_data_size);
+
+		if(type_id==0x07) { // guess
+			do_ti83_picture_var(c, d, pos);
+		}
+
+		pos += var_data_size;
+	}
+
+done:
+	;
 }
 
 static void do_ti92(deark *c, lctx *d)
@@ -122,24 +182,41 @@ static void de_run_tivariable(deark *c, const char *params)
 	d = de_malloc(c, sizeof(lctx));
 	d->fmt = identify_internal(c);
 	switch(d->fmt) {
-	case DE_FMT_TI92:
-		de_declare_fmt(c, "TI92 variable file");
+	case DE_FMT_TI83:
+		de_declare_fmt(c, "TI83 variable file");
+		do_ti83(c, d);
+		break;
+	case DE_FMT_TI83F:
+		de_declare_fmt(c, "TI83F variable file");
+		do_ti83(c, d);
 		break;
 	case DE_FMT_TI89:
 		de_declare_fmt(c, "TI89 variable file");
+		do_ti92(c, d);
+		break;
+	case DE_FMT_TI92:
+		de_declare_fmt(c, "TI92 variable file");
+		do_ti92(c, d);
 		break;
 	case DE_FMT_TI92P:
 		de_declare_fmt(c, "TI92P variable file");
+		do_ti92(c, d);
 		break;
+	default:
+		de_err(c, "Unknown or unsupported TI variable file version\n");
+		goto done;
 	}
 
-	do_ti92(c, d);
+done:
 	de_free(c, d);
 }
 
 static int de_identify_tivariable(deark *c)
 {
-	if(identify_internal(c)!=0) return 100;
+	int fmt;
+	fmt = identify_internal(c);
+	if(fmt==DE_FMT_TI_UNKNOWN) return 10;
+	if(fmt!=0) return 100;
 	return 0;
 }
 
