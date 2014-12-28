86b800fae2c9002d4c5b545fc181cdde9d292731
Jason Summers <jason1@pobox.com>
2014-12-28 16:19:23 -0500

    Working on shg module

--- modules/shg.c
+++ modules/shg.c
@@ -16,7 +16,7 @@ typedef struct localctx_struct {
 	de_byte packing_method;
 } lctx;
 
-// compressed unsigned short
+// "compressed unsigned short" - a variable-length integer format
 static de_int64 get_cus(dbuf *f, de_int64 *pos)
 {
 	de_int64 x1, x2;
@@ -33,7 +33,7 @@ static de_int64 get_cus(dbuf *f, de_int64 *pos)
 	return (x1>>1) | (x2<<7);
 }
 
-// compressed unsigned long
+// "compressed unsigned long" - a variable-length integer format
 static de_int64 get_cul(dbuf *f, de_int64 *pos)
 {
 	de_int64 x1, x2;
@@ -50,6 +50,41 @@ static de_int64 get_cul(dbuf *f, de_int64 *pos)
 	return (x1>>1) | (x2<<15);
 }
 
+static void do_uncompress_rle(deark *c, lctx *d, dbuf *unc_pixels,
+	de_int64 pos1, de_int64 len)
+{
+	de_int64 pos;
+	de_int64 endpos;
+	de_byte b;
+	de_int64 count;
+
+	de_dbg(c, "uncompressing RLE data\n");
+	endpos = pos1 + len;
+	pos = pos1;
+	while(pos<endpos) {
+		b = de_getbyte(pos);
+		pos++;
+		if(b&0x80) {
+			// uncompressed run
+			count = (de_int64)(b&0x7f);
+			dbuf_copy(c->infile, pos, count, unc_pixels);
+			pos += count;
+		}
+		else {
+			// compressed run
+			count = (de_int64)b;
+			b = de_getbyte(pos);
+			pos++;
+			dbuf_write_run(unc_pixels, b, count);
+		}
+	}
+}
+
+static de_int64 per_inch_to_per_meter(de_int64 dpi)
+{
+	return (de_int64)(0.5 + (100.0/2.54)*(double)dpi);
+}
+
 static int do_dib(deark *c, lctx *d, de_int64 pos1)
 {
 	de_int64 xdpi, ydpi;
@@ -63,8 +98,21 @@ static int do_dib(deark *c, lctx *d, de_int64 pos1)
 	de_int64 compressed_offset;
 	de_int64 hotspot_offset;
 	de_int64 pos;
+	de_int64 pal_offset;
+	de_int64 pal_size_in_colors;
+	de_int64 pal_size_in_bytes;
+	de_int64 image_size;
+	dbuf *unc_pixels = NULL;
+	dbuf *outf = NULL;
+	int retval = 1;
 
-	pos = pos1;
+	if(d->picture_type==5) {
+		// TODO: Support this
+		de_err(c, "DDB image format is not supported\n");
+		goto done;
+	}
+
+	pos = pos1 + 2;
 
 	xdpi = get_cul(c->infile, &pos);
 	ydpi = get_cul(c->infile, &pos);
@@ -85,14 +133,106 @@ static int do_dib(deark *c, lctx *d, de_int64 pos1)
 	compressed_size = get_cul(c->infile, &pos);
 	hotspot_size = get_cul(c->infile, &pos);
 	compressed_offset = de_getui32le(pos);
+	compressed_offset += pos1;
 	hotspot_offset = de_getui32le(pos+4);
+	hotspot_offset += pos1;
 	de_dbg(c, "bits offset=%d, size=%d\n", (int)compressed_offset,
 		(int)compressed_size);
 	de_dbg(c, "hotspot offset=%d, size=%d\n", (int)hotspot_offset,
 		(int)hotspot_size);
 	pos+=8;
 
-	return 1;
+	if(bitcount!=1 && bitcount!=4 && bitcount!=8 && bitcount!=24) {
+		de_err(c, "Unsupported bit count: %d\n", (int)bitcount);
+		goto done;
+	}
+
+	if(planes!=1) {
+		de_err(c, "Unsupported planes: %d\n", (int)planes);
+		goto done;
+	}
+
+	if(!de_good_image_dimensions(c, width, height)) goto done;
+
+	if(compressed_offset + compressed_size > c->infile->len) {
+		de_err(c, "Image goes beyond end of file\n");
+		goto done;
+	}
+
+	pal_offset = pos;
+
+	if(bitcount>8) {
+		pal_size_in_colors = 0;
+	}
+	else if(colors_used==0) {
+		pal_size_in_colors = ((de_int64)1)<<bitcount;
+	}
+	else {
+		pal_size_in_colors = colors_used;
+		if(pal_size_in_colors<1 || pal_size_in_colors>(((de_int64)1)<<bitcount)) {
+			goto done;
+		}
+	}
+
+	pal_size_in_bytes = 4*pal_size_in_colors;
+
+	image_size = height * (((width*bitcount +31)/32)*4);
+
+	if(d->packing_method==0) { // Uncompressed
+		unc_pixels = dbuf_open_input_subfile(c->infile,
+			compressed_offset, compressed_size);
+	}
+	else if(d->packing_method==1) { // RLE
+		unc_pixels = dbuf_create_membuf(c, image_size);
+		dbuf_set_max_length(unc_pixels, image_size);
+		do_uncompress_rle(c, d, unc_pixels, compressed_offset, compressed_size);
+
+		if(unc_pixels->len < image_size) {
+			de_warn(c, "Expected %d bytes after decompression, only got %d\n",
+				(int)image_size, (int)unc_pixels->len);
+		}
+	}
+	else if(d->packing_method==2 || d->packing_method==3) {
+		de_err(c, "LZ77 compression is not supported\n");
+		goto done;
+	}
+	else {
+		de_err(c, "Unsupported compression type: %d\n", (int)d->packing_method);
+		goto done;
+	}
+
+	outf = dbuf_create_output_file(c, "bmp", NULL);
+
+	// Write fileheader
+	dbuf_write(outf, (const de_byte*)"BM", 2);
+	dbuf_writeui32le(outf, 14 + 40 + pal_size_in_bytes + image_size);
+	dbuf_write_zeroes(outf, 4);
+	dbuf_writeui32le(outf, 14 + 40 + pal_size_in_bytes);
+
+	// Write infoheader
+	dbuf_writeui32le(outf, 40);
+	dbuf_writeui32le(outf, width);
+	dbuf_writeui32le(outf, height);
+	dbuf_writeui16le(outf, planes);
+	dbuf_writeui16le(outf, bitcount);
+	dbuf_writeui32le(outf, 0); // compression
+	dbuf_writeui32le(outf, 0); // SizeImage
+	dbuf_writeui32le(outf, per_inch_to_per_meter(xdpi));
+	dbuf_writeui32le(outf, per_inch_to_per_meter(ydpi));
+	dbuf_writeui32le(outf, colors_used);
+	dbuf_writeui32le(outf, colors_important);
+
+	// Write color table
+	dbuf_copy(c->infile, pal_offset, pal_size_in_bytes, outf);
+
+	// Write pixels
+	dbuf_copy(unc_pixels, 0, image_size, outf);
+
+	retval = 1;
+done:
+	dbuf_close(unc_pixels);
+	dbuf_close(outf);
+	return retval;
 }
 
 static int do_picture(deark *c, lctx *d, de_int64 pic_index)
@@ -118,7 +258,7 @@ static int do_picture(deark *c, lctx *d, de_int64 pic_index)
 	de_dbg(c, "packing method: %d\n", (int)d->packing_method);
 
 	if(d->picture_type==5 || d->picture_type==6) { // DDB or DIB
-		do_dib(c, d, pic_offset+2);
+		do_dib(c, d, pic_offset);
 	}
 	else {
 		de_warn(c, "Unsupported picture type: %d\n", (int)d->picture_type);
@@ -173,7 +313,6 @@ static void de_run_shg(deark *c, const char *params)
 	do_shg(c, d);
 
 	de_free(c, d);
-	de_err(c, "SHG/MRB support is not implemented.\n");
 }
 
 static int de_identify_shg(deark *c)
