177d1b42102b1d0dc6152830d12dd586696ff7a4
Jason Summers <jason1@pobox.com>
2015-02-10 22:11:40 -0500

    cardfile: Optionally extract text from cards

--- modules/cardfile.c
+++ modules/cardfile.c
@@ -1,7 +1,7 @@
 // This file is part of Deark, by Jason Summers.
 // This software is in the public domain. See the file COPYING for details.
 
-// Extract graphics from Windows Cardfile .crd format
+// Extract graphics and text from Windows Cardfile .crd format
 
 #include <deark-config.h>
 #include <deark-modules.h>
@@ -13,42 +13,90 @@ typedef struct localctx_struct {
 	de_int64 numcards;
 } lctx;
 
+static void do_text_data(deark *c, lctx *d, de_finfo *fi, de_int64 text_pos, de_int64 text_len)
+{
+	if(c->extract_level<2) return;
+	if(text_len<1) return;
+	if(text_pos + text_len > c->infile->len) return;
+
+	dbuf_create_file_from_slice(c->infile, text_pos, text_len, "txt", fi);
+}
+
 static void do_card_index(deark *c, lctx *d, de_int64 cardnum, de_int64 pos)
 {
 	de_int64 datapos;
-	de_int64 bitmap_length;
+	de_int64 bitmap_len;
 	de_int64 w, h;
 	de_int64 src_rowspan;
+	de_int64 text_len;
+	de_int64 text_pos;
 	struct deark_bitmap *img = NULL;
-	de_finfo *fi = NULL;
+	de_finfo *fi_bitmap = NULL;
+	de_finfo *fi_text = NULL;
 
 	datapos = de_getui32le(pos+6);
 	de_dbg(c, "card #%d, data offset = %d\n", (int)cardnum, (int)datapos);
 
 	if(datapos>=c->infile->len) return;
-	bitmap_length = de_getui16le(datapos);
-	if(bitmap_length<=0) {
-		de_dbg(c, "not a graphics card\n");
+	bitmap_len = de_getui16le(datapos);
+	de_dbg(c, "bitmap length: %d\n", (int)bitmap_len);
+
+	if(bitmap_len==0) {
+		text_len = de_getui16le(datapos+2);
+		text_pos = datapos+4;
+	}
+	else {
+		text_len = de_getui16le(datapos + bitmap_len + 10);
+		text_pos = datapos + bitmap_len + 10;
+	}
+	de_dbg(c, "text length: %d\n", (int)text_len);
+
+	if(bitmap_len==0 && text_len==0) {
+		de_dbg(c, "empty card\n");
 		goto done;
 	}
+	if(bitmap_len==0) {
+		de_dbg(c, "text-only card\n");
+	}
+	else if(text_len==0) {
+		de_dbg(c, "graphics-only card\n");
+	}
+	else {
+		de_dbg(c, "graphics+text card\n");
+	}
 
-	fi = de_finfo_create(c);
+	// Text
+
+	if(text_len!=0 && c->extract_level>=2) {
+		fi_text = de_finfo_create(c);
+		if(c->filenames_from_file)
+			de_finfo_set_name_from_slice(c, fi_text, c->infile, pos+11, 40, DE_CONVFLAG_STOP_AT_NUL);
+
+		do_text_data(c, d, fi_text, text_pos, text_len);
+	}
+
+	// Bitmap
+
+	if(bitmap_len==0) goto done;
+
+	fi_bitmap = de_finfo_create(c);
 	if(c->filenames_from_file)
-		de_finfo_set_name_from_slice(c, fi, c->infile, pos+11, 40, DE_CONVFLAG_STOP_AT_NUL);
+		de_finfo_set_name_from_slice(c, fi_bitmap, c->infile, pos+11, 40, DE_CONVFLAG_STOP_AT_NUL);
 
 	w = de_getui16le(datapos+2);
 	h = de_getui16le(datapos+4);
-	de_dbg(c, "bitmap %dx%d, length=%d\n", (int)w, (int)h, (int)bitmap_length);
+	de_dbg(c, "bitmap dimensions: %dx%d\n", (int)w, (int)h);
 
 	img = de_bitmap_create(c, w, h, 1);
 	src_rowspan = ((w+15)/16)*2;
 
 	de_convert_and_write_image_bilevel(c->infile, datapos+10,
-		w, h, src_rowspan, 0, fi);
+		w, h, src_rowspan, 0, fi_bitmap);
 
 done:
 	de_bitmap_destroy(img);
-	de_finfo_destroy(c, fi);
+	de_finfo_destroy(c, fi_bitmap);
+	de_finfo_destroy(c, fi_text);
 }
 
 static void de_run_cardfile(deark *c, const char *params)
