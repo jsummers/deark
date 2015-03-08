88b7430900a1b1861414276fb4507f57cd84f1c1
Jason Summers <jason1@pobox.com>
2015-03-08 10:47:44 -0400

    Working on ansiart module

--- modules/ansiart.c
+++ modules/ansiart.c
@@ -2,6 +2,7 @@
 // This software is in the public domain. See the file COPYING for details.
 
 // ANSI art
+// (very incomplete)
 
 #include <deark-config.h>
 #include <deark-modules.h>
@@ -19,10 +20,22 @@ typedef struct localctx_struct {
 	struct cell_struct *cells; // Array of height*width cells
 
 	de_int64 xpos, ypos; // 0-based
+	de_int64 saved_xpos, saved_ypos;
 
-	de_int64 param_buf_used;
-	de_byte param_buf[100];
+	de_byte curr_fgcol;
+	de_byte curr_bgcol;
+
+	de_byte param_string_buf[100];
+
+#define MAX_ESC_PARAMS 16
+	int num_params;
+	de_int64 params[MAX_ESC_PARAMS];
 } lctx;
+
+static const de_uint32 ansi_palette[16] = {
+	0x000000,0xaa0000,0x00aa00,0xaa5500,0x0000aa,0xaa00aa,0x00aaaa,0xaaaaaa,
+	0x555555,0xff5555,0x55ff55,0xffff55,0x5555ff,0xff55ff,0x55ffff,0xffffff
+};
 
 static struct cell_struct *get_cell_at(deark *c, lctx *d, de_int64 xpos, de_int64 ypos)
 {
@@ -31,16 +44,9 @@ static struct cell_struct *get_cell_at(deark *c, lctx *d, de_int64 xpos, de_int6
 	return &d->cells[ypos*d->width + xpos];
 }
 
-static void set_cur_cell_codepoint(deark *c, lctx *d, de_int32 u)
-{
-	struct cell_struct *cell;
-	cell = get_cell_at(c, d, d->xpos, d->ypos);
-	if(!cell) return;
-	cell->codepoint = u;
-}
-
 static void do_normal_char(deark *c, lctx *d, de_byte ch)
 {
+	struct cell_struct *cell;
 	de_int32 u;
 
 	if(ch==13) { // CR
@@ -51,57 +57,97 @@ static void do_normal_char(deark *c, lctx *d, de_byte ch)
 	}
 	else if(ch>=32) {
 		u = de_cp437g_to_unicode(c, (int)ch);
-		set_cur_cell_codepoint(c, d, u);
+
+		cell = get_cell_at(c, d, d->xpos, d->ypos);
+		if(cell) {
+			cell->codepoint = u;
+			cell->fgcol = d->curr_fgcol;
+			cell->bgcol = d->curr_bgcol;
+		}
+
 		d->xpos++;
 	}
 }
 
-// Read x;y from a NUL-terminated buffer.
-static void read_two_ints(deark *c, lctx *d, const de_byte *buf,
-  de_int64 *a, de_int64 *b, de_int64 a_default, de_int64 b_default)
+// Convert d->param_string_buf to d->params and d->num_params.
+static void parse_params(deark *c, lctx *d, de_int64 default_val)
 {
-	char *b_ptr;
 	de_int64 buf_len;
-	de_int64 a_len, b_len;
+	de_int64 ppos;
+	de_int64 param_len;
+	char *p_ptr;
+	int last_param = 0;
 
-	*a = a_default;
-	*b = b_default;
+	d->num_params = 0;
 
-	buf_len = de_strlen((const char*)buf);
-	if(buf_len<1) return;
+	buf_len = de_strlen((const char*)d->param_string_buf);
 
-	b_ptr = de_strchr((const char*)buf, ';');
-	if(b_ptr) {
-		// String contains a ';'
-		a_len = (const unsigned char*)b_ptr - buf;
-		b_len = buf_len - a_len - 1;
-		b_ptr++;
+	ppos = 0;
+	while(1) {
+		if(d->num_params >= MAX_ESC_PARAMS) {
+			break;
+		}
+
+		p_ptr = de_strchr((const char*)&d->param_string_buf[ppos], ';');
+		if(p_ptr) {
+			param_len = p_ptr - (char*)&d->param_string_buf[ppos];
+		}
+		else {
+			param_len = buf_len - ppos;
+			last_param = 1;
+		}
+
+		if(param_len>=1) {
+			d->params[d->num_params] = de_atoi64((const char*)&d->param_string_buf[ppos]);
+		}
+		else {
+			d->params[d->num_params] = default_val;
+		}
+		d->num_params++;
+
+		if(last_param) {
+			break;
+		}
+
+		// Advance past the parameter data and the terminating semicolon.
+		ppos += param_len + 1;
 	}
-	else {
-		a_len = buf_len;
-		b_len = 0;
-	}
 
-	if(a_len > 0) {
-		*a = de_atoi64((const char*)buf);
-	}
-
-	if(b_ptr && b_len>0 ) {
-		*b = de_atoi64(b_ptr);
-	}
 }
 
 static void read_one_int(deark *c, lctx *d, const de_byte *buf,
   de_int64 *a, de_int64 a_default)
 {
-	de_int64 buf_len;
+	parse_params(c, d, a_default);
 
-	*a = a_default;
+	if(d->num_params>=1) {
+		*a = d->params[0];
+	}
+	else {
+		*a = a_default;
+	}
+}
 
-	buf_len = de_strlen((const char*)buf);
-	if(buf_len<1) return;
+// m - Select Graphic Rendition
+static void do_code_m(deark *c, lctx *d)
+{
+	de_int64 i;
+	de_int64 sgi_code;
 
-	*a = de_atoi64((const char*)buf);
+	parse_params(c, d, 0);
+
+	for(i=0; i<d->num_params; i++) {
+		sgi_code = d->params[i];
+
+		if(sgi_code>=30 && sgi_code<=37) {
+			// Set foreground color
+			d->curr_fgcol = (de_byte)(sgi_code-30);
+		}
+		else if(sgi_code>=40 && sgi_code<=47) {
+			// Set background color
+			d->curr_bgcol = (de_byte)(sgi_code-40);
+		}
+	}
 }
 
 // H: Set cursor position
@@ -109,8 +155,14 @@ static void do_code_H(deark *c, lctx *d)
 {
 	de_int64 row, col;
 
-	read_two_ints(c, d, d->param_buf, &row, &col, 1, 1);
-	de_dbg(c, "(->%d %d)\n", (int)row, (int)col);
+	parse_params(c, d, 1);
+
+	if(d->num_params>=1) row = d->params[0];
+	else row = 1;
+
+	if(d->num_params>=2) col = d->params[1];
+	else col = 1;
+
 	d->xpos = col-1;
 	d->ypos = row-1;
 }
@@ -119,7 +171,7 @@ static void do_code_H(deark *c, lctx *d)
 static void do_code_A(deark *c, lctx *d)
 {
 	de_int64 n;
-	read_one_int(c, d, d->param_buf, &n, 1);
+	read_one_int(c, d, d->param_string_buf, &n, 1);
 	d->ypos -= n;
 }
 
@@ -127,7 +179,7 @@ static void do_code_A(deark *c, lctx *d)
 static void do_code_B(deark *c, lctx *d)
 {
 	de_int64 n;
-	read_one_int(c, d, d->param_buf, &n, 1);
+	read_one_int(c, d, d->param_string_buf, &n, 1);
 	d->ypos += n;
 }
 
@@ -135,7 +187,7 @@ static void do_code_B(deark *c, lctx *d)
 static void do_code_C(deark *c, lctx *d)
 {
 	de_int64 n;
-	read_one_int(c, d, d->param_buf, &n, 1);
+	read_one_int(c, d, d->param_string_buf, &n, 1);
 	d->xpos += n;
 }
 
@@ -143,7 +195,7 @@ static void do_code_C(deark *c, lctx *d)
 static void do_code_D(deark *c, lctx *d)
 {
 	de_int64 n;
-	read_one_int(c, d, d->param_buf, &n, 1);
+	read_one_int(c, d, d->param_string_buf, &n, 1);
 	d->xpos -= n;
 }
 
@@ -152,14 +204,14 @@ static void do_control_sequence(deark *c, lctx *d, de_byte code,
 {
 	de_dbg2(c, "[%c at %d %d]\n", (char)code, (int)param_start, (int)param_len);
 
-	if(param_len > sizeof(d->param_buf)-1) {
+	if(param_len > sizeof(d->param_string_buf)-1) {
 		de_warn(c, "Ignoring long escape sequence (len %d at %d)\n",
 			(int)param_len, (int)param_start);
 		return;
 	}
 
-	de_read(d->param_buf, param_start, param_len);
-	d->param_buf[param_len] = '\0';
+	de_read(d->param_string_buf, param_start, param_len);
+	d->param_string_buf[param_len] = '\0';
 
 	switch(code) {
 	case 'A': do_code_A(c, d); break;
@@ -167,6 +219,15 @@ static void do_control_sequence(deark *c, lctx *d, de_byte code,
 	case 'C': do_code_C(c, d); break;
 	case 'D': do_code_D(c, d); break;
 	case 'H': do_code_H(c, d); break;
+	case 'm': do_code_m(c, d); break;
+	case 's':
+		d->saved_xpos = d->xpos;
+		d->saved_ypos = d->ypos;
+		break;
+	case 'u':
+		d->xpos = d->saved_xpos;
+		d->ypos = d->saved_ypos;
+		break;
 	default:
 		de_dbg(c, "[unsupported escape sequence %c]\n", (char)code);
 	}
@@ -225,19 +286,51 @@ static void do_main(deark *c, lctx *d)
 	}
 }
 
+static void ansi_16_color_to_css(int index, char *buf, int buflen)
+{
+	de_uint32 clr;
+
+	if(index>=0 && index<16) clr = ansi_palette[index];
+	else clr = 0;
+
+	de_color_to_css(clr, buf, buflen);
+}
+
 static void do_output_main(deark *c, lctx *d)
 {
 	const struct cell_struct *cell;
 	int i, j;
 	de_int32 n;
+	int span_count = 0;
+	de_byte active_fgcol = 0;
+	de_byte active_bgcol = 0;
+	char fgcol_css[16];
+	char bgcol_css[16];
 
 	dbuf_fputs(d->ofile, "<pre>\n");
 	for(j=0; j<d->height; j++) {
 		for(i=0; i<d->width; i++) {
+
 			cell = get_cell_at(c, d, i, j);
 			if(!cell) continue;
 
+			if(cell->fgcol!=active_fgcol || cell->bgcol!=active_bgcol) {
+				while(span_count>0) {
+					dbuf_fprintf(d->ofile, "</span>");
+					span_count--;
+				}
+
+				ansi_16_color_to_css(cell->fgcol, fgcol_css, sizeof(fgcol_css));
+				ansi_16_color_to_css(cell->bgcol, bgcol_css, sizeof(bgcol_css));
+				dbuf_fprintf(d->ofile, "<span style='color:%s;background-color:%s'>", fgcol_css, bgcol_css);
+				span_count++;
+				active_fgcol = cell->fgcol;
+				active_bgcol = cell->bgcol;
+
+			}
+
 			n = cell->codepoint;
+			if(n==0x00) n=0x20;
 			if(n<0x20) n='?';
 
 			if(n=='<' || n=='>' || n>126) {
@@ -249,6 +342,12 @@ static void do_output_main(deark *c, lctx *d)
 		}
 		dbuf_fputs(d->ofile, "\n");
 	}
+
+	while(span_count>0) {
+		dbuf_fprintf(d->ofile, "</span>");
+		span_count--;
+	}
+
 	dbuf_fputs(d->ofile, "</pre>\n");
 }
 
