c8f0f4322415254c40f879a458ac5a1855b23a10
Jason Summers <jason1@pobox.com>
2015-08-12 19:29:50 -0400

    tga: Support RLE decompression

--- modules/tga.c
+++ modules/tga.c
@@ -4,6 +4,10 @@
 #include <deark-config.h>
 #include <deark-modules.h>
 
+#define TGA_CMPR_UNKNOWN 0
+#define TGA_CMPR_NONE    1
+#define TGA_CMPR_RLE     2
+
 typedef struct localctx_struct {
 	de_int64 id_field_len;
 	de_byte color_map_type;
@@ -14,6 +18,10 @@ typedef struct localctx_struct {
 	de_int64 num_attribute_bits;
 	de_byte top_down;
 	int has_signature;
+	int cmpr_type;
+	dbuf *unc_pixels;
+	de_int64 img_size_in_bytes;
+	de_int64 bytes_per_pixel;
 } lctx;
 
 static void setup_img(deark *c, lctx *d, struct deark_bitmap *img)
@@ -21,7 +29,7 @@ static void setup_img(deark *c, lctx *d, struct deark_bitmap *img)
 	img->flipped = !d->top_down;
 }
 
-static void do_decode_gray(deark *c, lctx *d, de_int64 pos)
+static void do_decode_gray(deark *c, lctx *d)
 {
 	struct deark_bitmap *img = NULL;
 	de_int64 i, j;
@@ -41,7 +49,7 @@ static void do_decode_gray(deark *c, lctx *d, de_int64 pos)
 
 	for(j=0; j<d->height; j++) {
 		for(i=0; i<d->width; i++) {
-			b = de_getbyte(pos + j*rowspan + i);
+			b = dbuf_getbyte(d->unc_pixels, j*rowspan + i);
 			de_bitmap_setpixel_gray(img, i, j, b);
 		}
 	}
@@ -51,7 +59,7 @@ done:
 	de_bitmap_destroy(img);
 }
 
-static void do_decode_rgb(deark *c, lctx *d, de_int64 pos)
+static void do_decode_rgb(deark *c, lctx *d)
 {
 	struct deark_bitmap *img = NULL;
 	de_int64 i, j;
@@ -78,7 +86,7 @@ static void do_decode_rgb(deark *c, lctx *d, de_int64 pos)
 
 	for(j=0; j<d->height; j++) {
 		for(i=0; i<d->width; i++) {
-			clr = dbuf_getRGB(c->infile, pos + j*rowspan + i*bytes_per_pixel, DE_GETRGBFLAG_BGR);
+			clr = dbuf_getRGB(d->unc_pixels, j*rowspan + i*bytes_per_pixel, DE_GETRGBFLAG_BGR);
 			de_bitmap_setpixel_rgb(img, i, j, clr);
 		}
 	}
@@ -86,6 +94,38 @@ static void do_decode_rgb(deark *c, lctx *d, de_int64 pos)
 done:
 	de_bitmap_write_to_file(img, NULL);
 	de_bitmap_destroy(img);
+}
+
+static int do_decode_rle(deark *c, lctx *d, de_int64 pos)
+{
+	de_byte b;
+	de_int64 count;
+	de_int64 k;
+	de_byte buf[8];
+
+	while(1) {
+		if(pos >= c->infile->len) break;
+		if(d->unc_pixels->len >= d->img_size_in_bytes) break;
+
+		b = de_getbyte(pos);
+		pos++;
+
+		if(b & 0x80) { // RLE block
+			count = (de_int64)(b - 0x80) + 1;
+			de_read(buf, pos, d->bytes_per_pixel);
+			pos += d->bytes_per_pixel;
+			for(k=0; k<count; k++) {
+				dbuf_write(d->unc_pixels, buf, d->bytes_per_pixel);
+			}
+		}
+		else { // uncompressed block
+			count = (de_int64)(b) + 1;
+			dbuf_copy(c->infile, pos, count * d->bytes_per_pixel, d->unc_pixels);
+			pos += count * d->bytes_per_pixel;
+		}
+	}
+
+	return 1;
 }
 
 static int has_signature(deark *c)
@@ -101,6 +141,7 @@ static void de_run_tga(deark *c, const char *params)
 {
 	lctx *d = NULL;
 	de_int64 pos;
+	const char *cmpr_name = NULL;
 
 	d = de_malloc(c, sizeof(lctx));
 
@@ -113,6 +154,23 @@ static void de_run_tga(deark *c, const char *params)
 	d->color_map_type = de_getbyte(1);
 	d->img_type = de_getbyte(2);
 	de_dbg(c, "image type code: %d\n", (int)d->img_type);
+
+	switch(d->img_type) {
+	case 1: case 2: case 3:
+		d->cmpr_type = TGA_CMPR_NONE;
+		cmpr_name = "none";
+		break;
+	case 9: case 10: case 11:
+		d->cmpr_type = TGA_CMPR_RLE;
+		cmpr_name = "RLE";
+		break;
+	default:
+		d->cmpr_type = TGA_CMPR_UNKNOWN;
+		cmpr_name = "unknown";
+	}
+	de_dbg_indent(c, 1);
+	de_dbg(c, "compression: %s\n", cmpr_name);
+	de_dbg_indent(c, -1);
 
 	d->width = de_getui16le(12);
 	d->height = de_getui16le(14);
@@ -155,12 +213,28 @@ static void de_run_tga(deark *c, const char *params)
 
 	de_dbg(c, "bitmap at %d\n", (int)pos);
 
+	d->bytes_per_pixel = ((d->pixel_depth+7)/8);
+	d->img_size_in_bytes = d->height * d->width * d->bytes_per_pixel;
+
+	if(d->cmpr_type==TGA_CMPR_RLE) {
+		d->unc_pixels = dbuf_create_membuf(c, d->img_size_in_bytes);
+		dbuf_set_max_length(d->unc_pixels, d->img_size_in_bytes);
+		if(!do_decode_rle(c, d, pos)) goto done;
+	}
+	else if(d->cmpr_type==TGA_CMPR_NONE) {
+		d->unc_pixels = dbuf_open_input_subfile(c->infile, pos, d->img_size_in_bytes);
+	}
+	else {
+		de_err(c, "Unsupported compression type (%s)\n", cmpr_name);
+		goto done;
+	}
+
 	switch(d->img_type) {
-	case 2:
-		do_decode_rgb(c, d, pos);
+	case 2: case 10:
+		do_decode_rgb(c, d);
 		break;
-	case 3:
-		do_decode_gray(c, d, pos);
+	case 3: case 11:
+		do_decode_gray(c, d);
 		break;
 	default:
 		de_err(c, "This TGA image type (%d) is not supported.\n", (int)d->img_type);
@@ -168,6 +242,7 @@ static void de_run_tga(deark *c, const char *params)
 	}
 
 done:
+	dbuf_close(d->unc_pixels);
 	de_free(c, d);
 }
 
