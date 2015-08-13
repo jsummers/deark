14a15633aa10591860fe68e837ea465eb1b15d97
Jason Summers <jason1@pobox.com>
2015-08-12 21:40:10 -0400

    tga: Support for paletted images

--- modules/tga.c
+++ modules/tga.c
@@ -8,6 +8,9 @@ typedef struct localctx_struct {
 	de_int64 id_field_len;
 	de_byte color_map_type;
 	de_byte img_type;
+	de_int64 cmap_start;
+	de_int64 cmap_length;
+	de_int64 cmap_depth;
 	de_int64 width, height;
 	de_int64 pixel_depth;
 	de_byte image_descriptor;
@@ -19,13 +22,16 @@ typedef struct localctx_struct {
 #define TGA_CMPR_RLE     2
 	int cmpr_type;
 #define TGA_CLRTYPE_UNKNOWN   0
-#define TGA_CLRTYPE_PALETTED  1
+#define TGA_CLRTYPE_PALETTE   1
 #define TGA_CLRTYPE_TRUECOLOR 2
 #define TGA_CLRTYPE_GRAYSCALE 3
 	int color_type;
 	dbuf *unc_pixels;
 	de_int64 img_size_in_bytes;
 	de_int64 bytes_per_pixel;
+	de_int64 bytes_per_pal_entry;
+	de_int64 pal_size_in_bytes;
+	de_uint32 pal[256];
 } lctx;
 
 static void do_decode_image(deark *c, lctx *d)
@@ -62,6 +68,10 @@ static void do_decode_image(deark *c, lctx *d)
 			else if(d->color_type==TGA_CLRTYPE_GRAYSCALE) {
 				b = dbuf_getbyte(d->unc_pixels, j*rowspan + i*d->bytes_per_pixel);
 				de_bitmap_setpixel_gray(img, i, j, b);
+			}
+			else if(d->color_type==TGA_CLRTYPE_PALETTE) {
+				b = dbuf_getbyte(d->unc_pixels, j*rowspan + i*d->bytes_per_pixel);
+				de_bitmap_setpixel_rgb(img, i, j, d->pal[(unsigned int)b]);
 			}
 		}
 	}
@@ -103,6 +113,35 @@ static int do_decode_rle(deark *c, lctx *d, de_int64 pos)
 	return 1;
 }
 
+static int do_read_palette(deark *c, lctx *d, de_int64 pos)
+{
+	de_int64 i;
+	de_int64 idx;
+
+	if(d->color_type != TGA_CLRTYPE_PALETTE) {
+		return 1; // don't care about the palette
+	}
+
+	if(d->cmap_depth != 24) {
+		de_err(c, "Palettes with depth=%d are not supported.\n", (int)d->cmap_depth);
+		return 0;
+	}
+	if(d->pixel_depth != 8) {
+		de_err(c, "Paletted images with depth=%d are not supported.\n", (int)d->pixel_depth);
+		return 0;
+	}
+
+	for(i=0; i<d->cmap_length; i++) {
+		idx = d->cmap_start + i;
+		if(idx<0 || idx>255) continue;
+		d->pal[idx] = dbuf_getRGB(c->infile, pos + i*d->bytes_per_pal_entry, DE_GETRGBFLAG_BGR);
+		de_dbg2(c, "pal[%3d] = (%3d,%3d,%3d)\n", (int)idx,
+			(int)DE_COLOR_R(d->pal[idx]), (int)DE_COLOR_G(d->pal[idx]),
+			(int)DE_COLOR_B(d->pal[idx]));
+	}
+	return 1;
+}
+
 static int has_signature(deark *c)
 {
 	if(c->infile->len<18) return 0;
@@ -134,7 +173,7 @@ static void de_run_tga(deark *c, const char *params)
 	switch(d->img_type) {
 	case 1: case 9:
 	case 32: case 33:
-		d->color_type = TGA_CLRTYPE_PALETTED;
+		d->color_type = TGA_CLRTYPE_PALETTE;
 		clrtype_name = "palette";
 		break;
 	case 2: case 10:
@@ -168,6 +207,14 @@ static void de_run_tga(deark *c, const char *params)
 	de_dbg(c, "color type: %s\n", clrtype_name);
 	de_dbg(c, "compression: %s\n", cmpr_name);
 	de_dbg_indent(c, -1);
+
+	if(d->color_map_type != 0) {
+		d->cmap_start = de_getui16le(3);
+		d->cmap_length = de_getui16le(5);
+		d->cmap_depth = (de_int64)de_getbyte(7);
+		de_dbg(c, "color map start: %d, len: %d, depth: %d\n", (int)d->cmap_start,
+			(int)d->cmap_length, (int)d->cmap_depth);
+	}
 
 	d->width = de_getui16le(12);
 	d->height = de_getui16le(14);
@@ -203,9 +250,14 @@ static void de_run_tga(deark *c, const char *params)
 	}
 
 	if(d->color_map_type!=0) {
-		de_dbg(c, "color map at %d\n", (int)pos);
-		de_err(c, "TGA images with a color map are not supported.\n");
-		goto done;
+		d->bytes_per_pal_entry = (d->cmap_depth+7)/8;
+		d->pal_size_in_bytes = d->cmap_length * d->bytes_per_pal_entry;
+		de_dbg(c, "color map at %d (%d colors, %d bytes)\n", (int)pos,
+			(int)d->cmap_length, (int)d->pal_size_in_bytes);
+
+		if(!do_read_palette(c, d, pos)) goto done;
+
+		pos += d->pal_size_in_bytes;
 	}
 
 	de_dbg(c, "bitmap at %d\n", (int)pos);
@@ -213,7 +265,7 @@ static void de_run_tga(deark *c, const char *params)
 	d->bytes_per_pixel = ((d->pixel_depth+7)/8);
 	d->img_size_in_bytes = d->height * d->width * d->bytes_per_pixel;
 
-	if(d->color_type!=TGA_CLRTYPE_PALETTED && d->color_type!=TGA_CLRTYPE_TRUECOLOR &&
+	if(d->color_type!=TGA_CLRTYPE_PALETTE && d->color_type!=TGA_CLRTYPE_TRUECOLOR &&
 		d->color_type!=TGA_CLRTYPE_GRAYSCALE)
 	{
 		de_err(c, "Unsupported color type (%d, %s)\n", (int)d->color_type, clrtype_name);
