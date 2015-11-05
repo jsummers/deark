36b0d3e25d2868fdaa7fc33194b35f0e12d012eb
Jason Summers <jason1@pobox.com>
2015-11-05 12:12:34 -0500

    Modified miniz to make zip timestamps use universal time instead of local time

--- src/miniz.h
+++ src/miniz.h
@@ -3043,14 +3043,14 @@ static void mz_zip_time_to_dos_time(time_t time, mz_uint16 *pDOS_time, mz_uint16
 #ifdef _MSC_VER
   struct tm tm_struct;
   struct tm *tm = &tm_struct;
-  errno_t err = localtime_s(tm, &time);
+  errno_t err = gmtime_s(tm, &time);
   if (err)
   {
     *pDOS_date = 0; *pDOS_time = 0;
     return;
   }
 #else
-  struct tm *tm = localtime(&time);
+  struct tm *tm = gmtime(&time);
 #endif
   *pDOS_time = (mz_uint16)(((tm->tm_hour) << 11) + ((tm->tm_min) << 5) + ((tm->tm_sec) >> 1));
   *pDOS_date = (mz_uint16)(((tm->tm_year + 1900 - 1980) << 9) + ((tm->tm_mon + 1) << 5) + tm->tm_mday);
