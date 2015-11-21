045bf5b7813e9b86df3d9e3afac4328dede59d05
Jason Summers <jason1@pobox.com>
2015-11-21 11:39:51 -0500

    psd: Print the names of some Photoshop Resource elements

--- modules/psd.c
+++ modules/psd.c
@@ -7,6 +7,38 @@
 #include <deark-modules.h>
 #include "fmtutil.h"
 
+struct rsrc_info {
+	de_uint16 id;
+	const char *name;
+};
+
+static const struct rsrc_info rsrc_info_arr[] = {
+	{ 0x03e9, "Macintosh print manager print info" },
+	{ 0x03ed, "Resolution info" },
+	{ 0x03f3, "Print flags" },
+	{ 0x03f5, "Color halftoning info" },
+	{ 0x03f8, "Color transfer functions" },
+	{ 0x0404, "IPTC-NAA" },
+	{ 0x0406, "JPEG quality" },
+	{ 0x0408, "Grid and guides info" },
+	{ 0x0409, "Thumbnail resource - Photoshop 4.0" },
+	{ 0x040a, "Copyright flag" },
+	{ 0x040c, "Thumbnail resource" },
+	{ 0x040d, "Global Angle" },
+	{ 0x0414, "Document-specific IDs seed number" },
+	{ 0x0419, "Global Altitude" },
+	{ 0x041a, "Slices" },
+	{ 0x041e, "URL List" },
+	{ 0x0421, "Version Info" },
+	{ 0x0422, "EXIF data 1" },
+	{ 0x0423, "EXIF data 3" },
+	{ 0x0425, "Caption digest" },
+	{ 0x0426, "Print scale" },
+	{ 0x0428, "Pixel Aspect Ratio" },
+	{ 0x2710, "Print flags info" },
+	{ 0, NULL }
+};
+
 static const char* units_name(de_int64 u)
 {
 	switch(u) {
@@ -57,72 +89,96 @@ static void do_thumbnail_resource(deark *c, de_int64 resource_id,
 	dbuf_create_file_from_slice(c->infile, pos+28, len-28, "psdthumb.jpg", NULL);
 }
 
-static void do_image_resource_blocks(deark *c, de_int64 startpos, de_int64 len)
+static int do_image_resource(deark *c, de_int64 pos1, de_int64 *bytes_consumed)
 {
 	de_byte buf[4];
-	de_int64 pos;
 	de_int64 resource_id;
-	de_int64 resource_pos;
 	de_int64 name_len;
 	de_int64 bytes_used_by_name_field;
 	de_int64 block_data_len;
+	de_int64 pos;
+	const char *idname;
+	const struct rsrc_info *ri = NULL;
+	de_int64 i;
 
-	pos = startpos;
+	pos = pos1;
+	*bytes_consumed = 0;
 
+	// Check the "8BIM" signature
+	de_read(buf, pos, 4);
+	if(buf[0]!='8' || buf[1]!='B' || buf[2]!='I' || buf[3]!='M') {
+		de_warn(c, "Bad Photoshop resource block signature at %d\n", (int)pos);
+		return 0;
+	}
+	pos+=4;
+
+	resource_id = de_getui16be(pos);
+	pos+=2;
+
+	// Read resource name. We don't care about this, but we have to read it
+	// because it has a variable size, and determines where the next field
+	// will be.
+	name_len = (de_int64)de_getbyte(pos);
+	bytes_used_by_name_field = 1 + name_len;
+	if(bytes_used_by_name_field&1) bytes_used_by_name_field++; // padding byte
+
+	pos+=bytes_used_by_name_field;
+
+	block_data_len = de_getui32be(pos);
+	pos+=4;
+
+	for(i=0; rsrc_info_arr[i].id!=0; i++) {
+		if(rsrc_info_arr[i].id == resource_id) {
+			ri = &rsrc_info_arr[i];
+			break;
+		}
+	}
+	if(ri && ri->name)
+		idname = ri->name;
+	else
+		idname = "?";
+
+	de_dbg(c, "Photoshop rsrc 0x%04x (%s) pos=%d nlen=%d dpos=%d dlen=%d\n",
+		(int)resource_id, idname, (int)pos1, (int)name_len, (int)pos, (int)block_data_len);
+
+	switch(resource_id) {
+	case 0x03ed: // ResolutionInfo
+		de_dbg_indent(c, 1);
+		do_resolutioninfo_resource(c, pos, block_data_len);
+		de_dbg_indent(c, -1);
+		break;
+	case 0x0404: // IPTC
+		if(c->extract_level>=2 && block_data_len>0) {
+			dbuf_create_file_from_slice(c->infile, pos, block_data_len, "iptc", NULL);
+		}
+		break;
+	case 0x0409: // PhotoshopThumbnail 4.0
+	case 0x040c: // PhotoshopThumbnail
+		do_thumbnail_resource(c, resource_id, pos, block_data_len);
+		break;
+	case 0x0422: // EXIFInfo
+		de_dbg(c, "Exif segment at %d datasize=%d\n", (int)pos, (int)block_data_len);
+		de_fmtutil_handle_exif(c, pos, block_data_len);
+		break;
+	}
+
+	pos+=block_data_len;
+	if(block_data_len&1) pos++; // padding byte
+
+	*bytes_consumed = pos - pos1;
+	return 1;
+}
+
+static void do_image_resource_blocks(deark *c, de_int64 pos1, de_int64 len)
+{
+	de_int64 pos;
+	de_int64 bytes_consumed;
+
+	pos = pos1;
 	while(1) {
-		if(pos>=startpos+len) break;
-
-		// Check the "8BIM" signature
-		resource_pos = pos;
-		de_read(buf, pos, 4);
-		if(buf[0]!='8' || buf[1]!='B' || buf[2]!='I' || buf[3]!='M') {
-			de_warn(c, "Bad Photoshop resource block signature at %d\n", (int)pos);
-			break;
-		}
-		pos+=4;
-
-		resource_id = de_getui16be(pos);
-		pos+=2;
-
-		// Read resource name. We don't care about this, but we have to read it
-		// because it has a variable size, and determines where the next field
-		// will be.
-		name_len = (de_int64)de_getbyte(pos);
-		bytes_used_by_name_field = 1 + name_len;
-		if(bytes_used_by_name_field&1) bytes_used_by_name_field++; // padding byte
-		// de_dbg(c, "name bytes: %d\n", (int)bytes_used_by_name_field);
-
-		pos+=bytes_used_by_name_field;
-
-		block_data_len = de_getui32be(pos);
-		pos+=4;
-
-		de_dbg(c, "Photoshop resource ID 0x%04x pos=%d data_pos=%d data_len=%d\n",
-			(int)resource_id, (int)resource_pos, (int)pos, (int)block_data_len);
-
-		switch(resource_id) {
-		case 0x03ed: // ResolutionInfo
-			de_dbg_indent(c, 1);
-			do_resolutioninfo_resource(c, pos, block_data_len);
-			de_dbg_indent(c, -1);
-			break;
-		case 0x0404: // IPTC
-			if(c->extract_level>=2 && block_data_len>0) {
-				dbuf_create_file_from_slice(c->infile, pos, block_data_len, "iptc", NULL);
-			}
-			break;
-		case 0x0409: // PhotoshopThumbnail 4.0
-		case 0x040c: // PhotoshopThumbnail
-			do_thumbnail_resource(c, resource_id, pos, block_data_len);
-			break;
-		case 0x0422: // EXIFInfo
-			de_dbg(c, "Exif segment at %d datasize=%d\n", (int)pos, (int)block_data_len);
-			de_fmtutil_handle_exif(c, pos, block_data_len);
-			break;
-		}
-
-		pos+=block_data_len;
-		if(block_data_len&1) pos++; // padding byte
+		if(pos>=pos1+len) break;
+		if(!do_image_resource(c, pos, &bytes_consumed)) break;
+		pos += bytes_consumed;
 	}
 }
 
