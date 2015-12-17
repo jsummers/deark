621e8cd68a89d22756f5d7bf024b91720761fc58
Jason Summers <jason1@pobox.com>
2015-12-17 08:53:57 -0500

    jpeg2000: Refactoring to make the parser reusable

--- modules/jpeg2000.c
+++ modules/jpeg2000.c
@@ -7,11 +7,36 @@
 #include <deark-modules.h>
 #include "fmtutil.h"
 
+struct de_boxesctx;
+
+// return 0 to stop reading
+typedef int (*de_handle_box_fn)(deark *c, struct de_boxesctx *bctx);
+
+struct de_boxesctx {
+	void *userdata;
+	dbuf *f; // Input file
+	de_handle_box_fn handle_box_fn;
+
+	// Per-box info supplied to handle_box_fn:
+	de_byte boxtype[4];
+	int is_uuid;
+	de_byte uuid[16]; // Valid only if is_uuid is set.
+	de_int64 box_pos;
+	de_int64 box_len;
+	// Note: for UUID boxes, payload does not include the UUID
+	de_int64 payload_pos;
+	de_int64 payload_len;
+
+	// To be filled in by handle_box_fn:
+	int is_superbox;
+};
+
 typedef struct localctx_struct {
 	int reserved;
 } lctx;
 
-static void do_box_sequence(deark *c, lctx *d, de_int64 pos1, de_int64 len, int level);
+static void do_box_sequence(deark *c, struct de_boxesctx *bctx,
+	de_int64 pos1, de_int64 len, int level);
 
 // Caller supplies s.
 static void render_uuid(deark *c, const de_byte *uuid, char *s, size_t s_len)
@@ -21,115 +46,82 @@ static void render_uuid(deark *c, const de_byte *uuid, char *s, size_t s_len)
 		uuid[8], uuid[9], uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]);
 }
 
-static int do_box(deark *c, lctx *d, de_int64 pos, de_int64 len, int level,
-	de_int64 *pbytes_consumed)
+static int do_box(deark *c, struct de_boxesctx *bctx, de_int64 pos, de_int64 len,
+	int level, de_int64 *pbytes_consumed)
 {
 	de_int64 size32, size64;
-	de_int64 header_size;
-	de_int64 payload_size;
-	de_byte boxtype[4];
+	de_int64 header_len;
+	de_int64 payload_len;
 	char boxtype_printable[16];
-	de_byte uuid[16];
 	char uuid_string[50];
-	int i;
-	int is_uuid = 0;
-	// TODO: Prune this list? We don't really need to know *all* superbox
-	// types -- only those that could contain data we want to extract.
-	static const char *superboxes[] = {
-		"jp2h", "res ", "uinf", // JP2
-		"jpch", "jplh", "cgrp", "ftbl", "comp", "asoc", "drep", // JPX
-		"page", "lobj", "objc", "sdat", // JPM
-		NULL };
 
+	bctx->is_uuid = 0;
 	size32 = de_getui32be(pos);
-	de_read(boxtype, pos+4, 4);
+	de_read(bctx->boxtype, pos+4, 4);
 
 	if(size32>=8) {
-		header_size = 8;
-		payload_size = size32-8;
+		header_len = 8;
+		payload_len = size32-8;
 	}
 	else if(size32==0) {
-		header_size = 8;
-		payload_size = len-8;
+		header_len = 8;
+		payload_len = len-8;
 	}
 	else if(size32==1) {
-		header_size = 16;
+		header_len = 16;
 		size64 = de_geti64be(pos+8);
 		if(size64<16) return 0;
-		payload_size = size64-16;
+		payload_len = size64-16;
 	}
 	else {
 		// Invalid or unsupported format.
 		return 0;
 	}
 
-	if(payload_size>=16 && !de_memcmp(boxtype, "uuid", 4)) {
-		is_uuid = 1;
-		de_read(uuid, pos+header_size, 16);
+	if(payload_len>=16 && !de_memcmp(bctx->boxtype, "uuid", 4)) {
+		bctx->is_uuid = 1;
+		de_read(bctx->uuid, pos+header_len, 16);
 	}
 
 	if(c->debug_level>0) {
-		de_make_printable_ascii(boxtype, 4, boxtype_printable, sizeof(boxtype_printable), 0);
-		if(is_uuid) {
-			render_uuid(c, uuid, uuid_string, sizeof(uuid_string));
+		de_make_printable_ascii(bctx->boxtype, 4, boxtype_printable, sizeof(boxtype_printable), 0);
+		if(bctx->is_uuid) {
+			render_uuid(c, bctx->uuid, uuid_string, sizeof(uuid_string));
 			de_dbg(c, "box '%s'{%s} at %d, size=%d\n",
 				boxtype_printable, uuid_string,
-				(int)pos, (int)payload_size);
+				(int)pos, (int)payload_len);
 		}
 		else {
 			de_dbg(c, "box '%s' at %d, size=%d\n", boxtype_printable,
-				(int)pos, (int)payload_size);
+				(int)pos, (int)payload_len);
 		}
 	}
 
-	if(is_uuid) {
-		de_int64 upos, ulen;
-		upos = pos+header_size+16;
-		ulen = payload_size-16;
-
-		if(!de_memcmp(uuid, "\xb1\x4b\xf8\xbd\x08\x3d\x4b\x43\xa5\xae\x8c\xd7\xd5\xa6\xce\x03", 16)) {
-			de_dbg(c, "GeoTIFF data at %d, size=%d\n", (int)upos, (int)ulen);
-			dbuf_create_file_from_slice(c->infile, upos, ulen, "geo.tif", NULL);
-		}
-		else if(!de_memcmp(uuid, "\xbe\x7a\xcf\xcb\x97\xa9\x42\xe8\x9c\x71\x99\x94\x91\xe3\xaf\xac", 16)) {
-			de_dbg(c, "XMP data at %d, size=%d\n", (int)upos, (int)ulen);
-			dbuf_create_file_from_slice(c->infile, upos, ulen, "xmp", NULL);
-		}
-		else if(!de_memcmp(uuid, "\x2c\x4c\x01\x00\x85\x04\x40\xb9\xa0\x3e\x56\x21\x48\xd6\xdf\xeb", 16)) {
-			de_dbg(c, "Photoshop resources at %d, size=%d\n", (int)upos, (int)ulen);
-			de_fmtutil_handle_photoshop_rsrc(c, upos, ulen);
-		}
-		else if(!de_memcmp(uuid, "\x05\x37\xcd\xab\x9d\x0c\x44\x31\xa7\x2a\xfa\x56\x1f\x2a\x11\x3e", 16)) {
-			de_dbg(c, "Exif data at %d, size=%d\n", (int)upos, (int)ulen);
-			de_fmtutil_handle_exif(c, upos, ulen);
-		}
+	bctx->is_superbox = 0; // Default value. Client can change it.
+	bctx->box_pos = pos;
+	bctx->box_len = header_len + payload_len;
+	bctx->payload_pos = pos+header_len;
+	bctx->payload_len = payload_len;
+	if(bctx->is_uuid) {
+		bctx->payload_pos += 16;
+		bctx->payload_len -= 16;
 	}
-	else if(!de_memcmp(boxtype, "jp2c", 4)) { // Contiguous Codestream box
-		dbuf_create_file_from_slice(c->infile, pos+header_size, payload_size, "j2c", NULL);
-	}
-	else if(!de_memcmp(boxtype, "xml ", 4)) { // XML box
-		// TODO: Detect the specific XML format, and use it to choose a better
-		// filename.
-		dbuf_create_file_from_slice(c->infile, pos+header_size, payload_size, "xml", NULL);
-	}
-	else {
-		// Check if this box type is known to contain other boxes that we might
-		// want to recurse into.
-		for(i=0; superboxes[i]; i++) {
-			if(!de_memcmp(boxtype, superboxes[i], 4)) {
-				de_dbg_indent(c, 1);
-				do_box_sequence(c, d, pos+header_size, payload_size, level+1);
-				de_dbg_indent(c, -1);
-				break;
-			}
-		}
+	if(!bctx->handle_box_fn(c, bctx)) {
+		return 0;
 	}
 
-	*pbytes_consumed = header_size + payload_size;
+	if(bctx->is_superbox) {
+		de_dbg_indent(c, 1);
+		do_box_sequence(c, bctx, pos+header_len, payload_len, level+1);
+		de_dbg_indent(c, -1);
+	}
+
+	*pbytes_consumed = header_len + payload_len;
 	return 1;
 }
 
-static void do_box_sequence(deark *c, lctx *d, de_int64 pos1, de_int64 len, int level)
+static void do_box_sequence(deark *c, struct de_boxesctx *bctx,
+	de_int64 pos1, de_int64 len, int level)
 {
 	de_int64 pos;
 	de_int64 box_len;
@@ -144,20 +136,81 @@ static void do_box_sequence(deark *c, lctx *d, de_int64 pos1, de_int64 len, int 
 	endpos = pos1 + len;
 
 	while(pos < endpos) {
-		ret = do_box(c, d, pos, endpos-pos, level, &box_len);
+		ret = do_box(c, bctx, pos, endpos-pos, level, &box_len);
 		if(!ret) break;
 		pos += box_len;
 	}
 }
 
+static void de_read_boxes_format(deark *c, struct de_boxesctx *bctx)
+{
+	do_box_sequence(c, bctx, 0, bctx->f->len, 0);
+}
+
+static int my_box_handler(deark *c, struct de_boxesctx *bctx)
+{
+	static const char *superboxes[] = {
+		"jp2h", "res ", "uinf", // JP2
+		"jpch", "jplh", "cgrp", "ftbl", "comp", "asoc", "drep", // JPX
+		"page", "lobj", "objc", "sdat", // JPM
+		NULL };
+	int i;
+
+	if(bctx->is_uuid) {
+		if(!de_memcmp(bctx->uuid, "\xb1\x4b\xf8\xbd\x08\x3d\x4b\x43\xa5\xae\x8c\xd7\xd5\xa6\xce\x03", 16)) {
+			de_dbg(c, "GeoTIFF data at %d, size=%d\n", (int)bctx->payload_pos, (int)bctx->payload_len);
+			dbuf_create_file_from_slice(bctx->f, bctx->payload_pos, bctx->payload_len, "geo.tif", NULL);
+		}
+		else if(!de_memcmp(bctx->uuid, "\xbe\x7a\xcf\xcb\x97\xa9\x42\xe8\x9c\x71\x99\x94\x91\xe3\xaf\xac", 16)) {
+			de_dbg(c, "XMP data at %d, size=%d\n", (int)bctx->payload_pos, (int)bctx->payload_len);
+			dbuf_create_file_from_slice(bctx->f, bctx->payload_pos, bctx->payload_len, "xmp", NULL);
+		}
+		else if(!de_memcmp(bctx->uuid, "\x2c\x4c\x01\x00\x85\x04\x40\xb9\xa0\x3e\x56\x21\x48\xd6\xdf\xeb", 16)) {
+			de_dbg(c, "Photoshop resources at %d, size=%d\n", (int)bctx->payload_pos, (int)bctx->payload_len);
+			de_fmtutil_handle_photoshop_rsrc(c, bctx->payload_pos, bctx->payload_len);
+		}
+		else if(!de_memcmp(bctx->uuid, "\x05\x37\xcd\xab\x9d\x0c\x44\x31\xa7\x2a\xfa\x56\x1f\x2a\x11\x3e", 16)) {
+			de_dbg(c, "Exif data at %d, size=%d\n", (int)bctx->payload_pos, (int)bctx->payload_len);
+			de_fmtutil_handle_exif(c, bctx->payload_pos, bctx->payload_len);
+		}
+	}
+	else if(!de_memcmp(bctx->boxtype, "jp2c", 4)) { // Contiguous Codestream box
+		dbuf_create_file_from_slice(bctx->f, bctx->payload_pos, bctx->payload_len, "j2c", NULL);
+	}
+	else if(!de_memcmp(bctx->boxtype, "xml ", 4)) { // XML box
+		// TODO: Detect the specific XML format, and use it to choose a better
+		// filename.
+		dbuf_create_file_from_slice(bctx->f, bctx->payload_pos, bctx->payload_len, "xml", NULL);
+	}
+	else {
+		// Check if this box type is known to contain other boxes that we might
+		// want to recurse into.
+		for(i=0; superboxes[i]; i++) {
+			if(!de_memcmp(bctx->boxtype, superboxes[i], 4)) {
+				bctx->is_superbox = 1;
+				break;
+			}
+		}
+	}
+
+	return 1;
+}
+
 static void de_run_jpeg2000(deark *c, de_module_params *mparams)
 {
 	lctx *d = NULL;
+	struct de_boxesctx *bctx = NULL;
 
 	d = de_malloc(c, sizeof(lctx));
+	bctx = de_malloc(c, sizeof(struct de_boxesctx));
 
-	do_box_sequence(c, d, 0, c->infile->len, 0);
+	bctx->userdata = (void*)d;
+	bctx->f = c->infile;
+	bctx->handle_box_fn = my_box_handler;
 
+	de_read_boxes_format(c, bctx);
+
+	de_free(c, bctx);
 	de_free(c, d);
 }
 
