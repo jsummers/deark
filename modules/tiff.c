da221bf77c69c65c2d2c2e6b8baf561991242119
Jason Summers <jason1@pobox.com>
2016-07-12 13:49:18 -0400

    tiff: More debug info

--- modules/tiff.c
+++ modules/tiff.c
@@ -8,6 +8,7 @@
 #include "fmtutil.h"
 DE_DECLARE_MODULE(de_module_tiff);
 
+#define ITEMS_IN_ARRAY(x) (sizeof(x)/sizeof(x[0]))
 #define MAX_IFDS 1000
 
 #define TAGTYPE_BYTE      1
@@ -50,14 +51,34 @@ static void handler_colormap(deark *c, lctx *d, const struct taginfo *tg, const 
 static void handler_subifd(deark *c, lctx *d, const struct taginfo *tg, const struct tagtypeinfo *tti);
 
 typedef int (*val_decoder_fn_type)(deark *c, lctx *d, const struct taginfo *tg,
-	de_int64 n, char *buf, size_t buf_len);
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
 
+static int valdec_newsubfiletype(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
+static int valdec_oldsubfiletype(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
 static int valdec_compression(deark *c, lctx *d, const struct taginfo *tg,
-	de_int64 n, char *buf, size_t buf_len);
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
 static int valdec_photometric(deark *c, lctx *d, const struct taginfo *tg,
-	de_int64 n, char *buf, size_t buf_len);
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
+static int valdec_fillorder(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
+static int valdec_orientation(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
+static int valdec_planarconfiguration(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
 static int valdec_resolutionunit(deark *c, lctx *d, const struct taginfo *tg,
-	de_int64 n, char *buf, size_t buf_len);
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
+static int valdec_pagenumber(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
+static int valdec_predictor(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
+static int valdec_extrasamples(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
+static int valdec_sampleformat(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
+static int valdec_ycbcrpositioning(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len);
 
 struct tagtypeinfo {
 	int tagnum;
@@ -70,41 +91,73 @@ struct tagtypeinfo {
 	val_decoder_fn_type vdfn;
 };
 static const struct tagtypeinfo tagtypeinfo_arr[] = {
-	{ 254, 0x00, "NewSubfileType", NULL, NULL },
+	{ 254, 0x00, "NewSubfileType", NULL, valdec_newsubfiletype },
+	{ 255, 0x00, "OldSubfileType", NULL, valdec_oldsubfiletype },
 	{ 256, 0x00, "ImageWidth", NULL, NULL },
 	{ 257, 0x00, "ImageLength", NULL, NULL },
 	{ 258, 0x00, "BitsPerSample", NULL, NULL },
 	{ 259, 0x00, "Compression", NULL, valdec_compression },
 	{ 262, 0x00, "PhotometricInterpretation", NULL, valdec_photometric },
-	{ 266, 0x00, "FillOrder", NULL, NULL },
+	{ 263, 0x00, "Threshholding", NULL, NULL },
+	{ 266, 0x00, "FillOrder", NULL, valdec_fillorder },
 	{ 269, 0x00, "DocumentName", NULL, NULL },
 	{ 270, 0x00, "ImageDescription", NULL, NULL },
 	{ 271, 0x00, "Make", NULL, NULL },
 	{ 272, 0x00, "Model", NULL, NULL },
 	{ 273, 0x00, "StripOffsets", NULL, NULL },
-	{ 274, 0x00, "Orientation", NULL, NULL },
+	{ 274, 0x00, "Orientation", NULL, valdec_orientation },
 	{ 277, 0x00, "SamplesPerPixel", NULL, NULL },
 	{ 278, 0x00, "RowsPerStrip", NULL, NULL },
 	{ 279, 0x00, "StripByteCounts", NULL, NULL },
+	{ 280, 0x00, "MinSampleValue", NULL, NULL },
+	{ 281, 0x00, "MaxSampleValue", NULL, NULL },
 	{ 282, 0x00, "XResolution", NULL, NULL },
 	{ 283, 0x00, "YResolution", NULL, NULL },
-	{ 284, 0x00, "PlanarConfiguration", NULL, NULL },
+	{ 284, 0x00, "PlanarConfiguration", NULL, valdec_planarconfiguration },
+	{ 285, 0x00, "PageName", NULL, NULL },
+	{ 286, 0x00, "XPosition", NULL, NULL },
+	{ 287, 0x00, "YPosition", NULL, NULL },
+	{ 292, 0x00, "T4Options", NULL, NULL },
 	{ 296, 0x00, "ResolutionUnit", NULL, valdec_resolutionunit },
-	{ 297, 0x00, "PageNumber", NULL, NULL },
+	{ 297, 0x00, "PageNumber", NULL, valdec_pagenumber },
+	{ 301, 0x00, "TransferFunction", NULL, NULL },
 	{ 305, 0x00, "Software", NULL, NULL },
 	{ 306, 0x00, "DateTime", NULL, NULL },
+	{ 315, 0x00, "Artist", NULL, NULL },
+	{ 316, 0x00, "HostComputer", NULL, NULL },
+	{ 317, 0x00, "Predictor", NULL, valdec_predictor },
+	{ 318, 0x00, "WhitePoint", NULL, NULL },
+	{ 319, 0x00, "PrimaryChromaticities", NULL, NULL },
 	{ 320, 0x08, "ColorMap", handler_colormap, NULL },
+	{ 322, 0x00, "TileWidth", NULL, NULL },
+	{ 323, 0x00, "TileLength", NULL, NULL },
+	{ 324, 0x00, "TileOffsets", NULL, NULL },
+	{ 325, 0x00, "TileByteCounts", NULL, NULL },
 	{ 330, 0x08, "SubIFD", NULL, NULL },
+	{ 338, 0x00, "ExtraSamples", NULL, valdec_extrasamples },
+	{ 339, 0x00, "SampleFormat", NULL, valdec_sampleformat },
+	{ 512, 0x00, "JPEGProc", NULL, NULL },
 #define TAG_JPEGINTERCHANGEFORMAT 513
 	{ TAG_JPEGINTERCHANGEFORMAT, 0x00, "JPEGInterchangeFormat", NULL, NULL },
 #define TAG_JPEGINTERCHANGEFORMATLENGTH 514
 	{ TAG_JPEGINTERCHANGEFORMATLENGTH, 0x00, "JPEGInterchangeFormatLength", NULL, NULL },
+	{ 515, 0x00, "JPEGRestartInterval", NULL, NULL },
+	{ 517, 0x00, "JPEGLosslessPredictors", NULL, NULL },
+	{ 518, 0x00, "JPEGPointTransforms", NULL, NULL },
+	{ 519, 0x00, "JPEGQTables", NULL, NULL },
+	{ 520, 0x00, "JPEGDCTables", NULL, NULL },
+	{ 521, 0x00, "JPEGACTables", NULL, NULL },
+	{ 529, 0x00, "YCbCrCoefficients", NULL, NULL },
+	{ 530, 0x00, "YCbCrSubSampling", NULL, NULL },
+	{ 531, 0x00, "YCbCrPositioning", NULL, valdec_ycbcrpositioning },
+	{ 532, 0x00, "ReferenceBlackWhite", NULL, NULL },
 #define TAG_XMP               700
-	{ TAG_XMP, 0x00, "XMP", NULL, NULL },
+	{ TAG_XMP, 0x08, "XMP", NULL, NULL },
+	{ 33432, 0x00, "Copyright", NULL, NULL },
 #define TAG_IPTC              33723
-	{ TAG_IPTC, 0x00, "IPTC", NULL, NULL },
+	{ TAG_IPTC, 0x08, "IPTC", NULL, NULL },
 #define TAG_PHOTOSHOPRESOURCES 34377
-	{ TAG_PHOTOSHOPRESOURCES, 0x00, "PhotoshopImageResources", NULL, NULL },
+	{ TAG_PHOTOSHOPRESOURCES, 0x08, "PhotoshopImageResources", NULL, NULL },
 	{ 34665, 0x08, "Exif IFD", handler_subifd, NULL },
 #define TAG_ICCPROFILE        34675
 	{ TAG_ICCPROFILE, 0x00, "ICC Profile", NULL, NULL },
@@ -256,6 +309,12 @@ static int read_tag_value_as_int64(deark *c, lctx *d, const struct taginfo *tg,
 		*n = (de_int64)de_getbyte(tg->val_offset + value_index*tg->unit_size);
 		return 1;
 	}
+	else if(tg->tagtype==TAGTYPE_UINT64 || tg->tagtype==TAGTYPE_IFD64) {
+		// TODO: What should we do about unsigned 64-bit ints that don't fit into
+		// a de_int64?
+		*n = dbuf_geti64x(c->infile, tg->val_offset + value_index*tg->unit_size, d->is_le);
+		return 1;
+	}
 	return 0;
 }
 
@@ -344,48 +403,181 @@ static void do_leaf_metadata(deark *c, lctx *d, de_int64 pos1, de_int64 len)
 	}
 }
 
-static int valdec_compression(deark *c, lctx *d, const struct taginfo *tg,
+struct int_and_str {
+	de_int64 n;
+	const char *s;
+};
+
+static int lookup_str_and_copy_to_buf(const struct int_and_str *items, size_t num_items,
 	de_int64 n, char *buf, size_t buf_len)
 {
-	const char *s = NULL;
-	if(n==1) s="uncompressed";
-	else if(n==2) s="CCITTRLE";
-	else if(n==3) s="Fax3";
-	else if(n==4) s="Fax4";
-	else if(n==5) s="LZW";
-	else if(n==6) s="OldJPEG";
-	else if(n==7) s="NewJPEG";
-	else if(n==8 || n==32946) s="DEFLATE";
-	else if(n==32773) s="PackBits";
-	if(!s) return 0;
-	de_strlcpy(buf, s, buf_len);
+	de_int64 i;
+
+	for(i=0; i<(de_int64)num_items; i++) {
+		if(items[i].n==n) {
+			de_strlcpy(buf, items[i].s, buf_len);
+			return 1;
+		}
+	}
+	de_strlcpy(buf, "?", buf_len);
+	return 0;
+}
+
+static int valdec_newsubfiletype(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	dbuf *s = NULL;
+
+	if(n<1) return 0;
+	s = dbuf_create_membuf(c, (de_int64)buf_len, 0);
+
+	if(n&0x1) {
+		dbuf_puts(s, "reduced-res");
+	}
+	if(n&0x2) {
+		if(s->len) dbuf_writebyte(s, ',');
+		dbuf_puts(s, "one-page-of-many");
+	}
+	if(n&0x4) {
+		if(s->len) dbuf_writebyte(s, ',');
+		dbuf_puts(s, "mask");
+	}
+
+	dbuf_copy_all_to_sz(s, buf, buf_len);
+	dbuf_close(s);
+	return 1;
+}
+
+static int valdec_oldsubfiletype(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	static const struct int_and_str name_map[] = {
+		{1, "full-res"}, {2, "reduced-res"}, {3, "one-page-of-many"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
+	return 1;
+}
+
+static int valdec_compression(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	static const struct int_and_str name_map[] = {
+		{1, "uncompressed"}, {2, "CCITTRLE"}, {3, "Fax3"}, {4, "Fax4"},
+		{5, "LZW"}, {6, "OldJPEG"}, {7, "NewJPEG"}, {8, "DEFLATE"},
+		{32773, "PackBits"}, {32946, "DEFLATE"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
 	return 1;
 }
 
 static int valdec_photometric(deark *c, lctx *d, const struct taginfo *tg,
-	de_int64 n, char *buf, size_t buf_len)
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
 {
-	const char *s = NULL;
-	if(n==0) s="grayscale/white-is-0";
-	else if(n==1) s="grayscale/black-is-0";
-	else if(n==2) s="RGB";
-	else if(n==3) s="palette";
-	else if(n==5) s="CMYK";
-	else if(n==6) s="YCbCr";
-	if(!s) return 0;
-	de_strlcpy(buf, s, buf_len);
+	static const struct int_and_str name_map[] = {
+		{0, "grayscale/white-is-0"}, {1, "grayscale/black-is-0"},
+		{2, "RGB"}, {3, "palette"}, {5, "CMYK"}, {6, "YCbCr"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
+	return 1;
+}
+
+static int valdec_fillorder(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	static const struct int_and_str name_map[] = {
+		{1, "MSB-first"}, {2, "LSB-first"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
+	return 1;
+}
+
+static int valdec_orientation(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	static const struct int_and_str name_map[] = {
+		{1, "top-left"}, {2, "top-right"}, {3, "bottom-right"}, {4, "bottom-left"},
+		{5, "left-top"}, {6, "right-top"}, {7, "right-bottom"}, {8, "left-bottom"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
+	return 1;
+}
+
+static int valdec_planarconfiguration(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	static const struct int_and_str name_map[] = {
+		{1, "contiguous"}, {2, "separated"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
 	return 1;
 }
 
 static int valdec_resolutionunit(deark *c, lctx *d, const struct taginfo *tg,
-	de_int64 n, char *buf, size_t buf_len)
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
 {
-	const char *s = NULL;
-	if(n==1) s="unspecified";
-	else if(n==2) s="pixels/inch";
-	else if(n==3) s="pixels/cm";
-	if(!s) return 0;
-	de_strlcpy(buf, s, buf_len);
+	static const struct int_and_str name_map[] = {
+		{1, "unspecified"}, {2, "pixels/inch"}, {3, "pixels/cm"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
+	return 1;
+}
+
+static int valdec_pagenumber(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	if(idx==0) {
+		de_snprintf(buf, buf_len, "page %d", (int)(n+1));
+		return 1;
+	}
+	if(idx==1) {
+		if(n==0) {
+			de_strlcpy(buf, "of an unknown number", buf_len);
+		}
+		else {
+			de_snprintf(buf, buf_len, "of %d", (int)(n));
+		}
+		return 1;
+	}
+	return 0;
+}
+
+static int valdec_predictor(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	static const struct int_and_str name_map[] = {
+		{1, "none"}, {2, "horizontal differencing"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
+	return 1;
+}
+
+static int valdec_extrasamples(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	static const struct int_and_str name_map[] = {
+		{0, "unspecified"}, {1, "assoc-alpha"}, {2, "unassoc-alpha"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
+	return 1;
+}
+
+static int valdec_sampleformat(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	static const struct int_and_str name_map[] = {
+		{1, "uint"}, {2, "signed int"}, {3, "float"}, {4, "undefined"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
+	return 1;
+}
+
+static int valdec_ycbcrpositioning(deark *c, lctx *d, const struct taginfo *tg,
+	de_int64 idx, de_int64 n, char *buf, size_t buf_len)
+{
+	static const struct int_and_str name_map[] = {
+		{1, "centered"}, {2, "cosited"}
+	};
+	lookup_str_and_copy_to_buf(name_map, ITEMS_IN_ARRAY(name_map), n, buf, buf_len);
 	return 1;
 }
 
@@ -445,12 +637,13 @@ static void do_dbg_print_numeric_values(deark *c, lctx *d, const struct taginfo 
 		case TAGTYPE_BYTE: case TAGTYPE_UNDEF:
 		case TAGTYPE_UINT16:
 		case TAGTYPE_UINT32: case TAGTYPE_IFD32:
+		case TAGTYPE_UINT64: case TAGTYPE_IFD64:
 			if(read_tag_value_as_int64(c, d, tg, i, &v_int64)) {
 				dbuf_printf(dbglinedbuf, "%" INT64_FMT, v_int64);
 
 				if(tti->vdfn) {
 					valbuf[0]='\0';
-					if(tti->vdfn(c, d, tg, v_int64, valbuf, sizeof(valbuf))) {
+					if(tti->vdfn(c, d, tg, i, v_int64, valbuf, sizeof(valbuf))) {
 						dbuf_printf(dbglinedbuf, "(=%s)", valbuf);
 					}
 				}
