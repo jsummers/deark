4c88b028bca30c3401a7612cc4a9c247f60dba4d
Jason Summers <jason1@pobox.com>
2016-08-08 11:15:33 -0400

    iccprofile: Decode the 'mluc' tag type

--- modules/iccprofile.c
+++ modules/iccprofile.c
@@ -8,10 +8,18 @@
 #include <deark-private.h>
 DE_DECLARE_MODULE(de_module_iccprofile);
 
+struct tag_seen_type {
+	de_int64 offset;
+	de_int64 len;
+};
+
 typedef struct localctx_struct {
 	unsigned int profile_ver_major;
 	unsigned int profile_ver_minor;
 	unsigned int profile_ver_bugfix;
+
+	de_int64 num_tags;
+	struct tag_seen_type *tags_seen;
 } lctx;
 
 #define ITEMS_IN_ARRAY(x) (sizeof(x)/sizeof(x[0]))
@@ -20,6 +28,7 @@ typedef void (*datatype_decoder_fn_type)(deark *c, lctx *d, de_int64 pos, de_int
 
 static void typedec_text(deark *c, lctx *d, de_int64 pos, de_int64 len);
 static void typedec_desc(deark *c, lctx *d, de_int64 pos, de_int64 len);
+static void typedec_mluc(deark *c, lctx *d, de_int64 pos, de_int64 len);
 
 struct datatypeinfo {
 	de_uint32 id;
@@ -38,7 +47,7 @@ static const struct datatypeinfo datatypeinfo_arr[] = {
 	{ 0x6D656173U, "measurement", NULL }, // meas
 	{ 0x6D667431U, "lut8", NULL }, // mft1
 	{ 0x6D667432U, "lut16", NULL }, // mft2
-	{ 0x6D6C7563U, "multiLocalizedUnicode", NULL }, // mluc
+	{ 0x6D6C7563U, "multiLocalizedUnicode", typedec_mluc }, // mluc
 	{ 0x73663332U, "s15Fixed16Array", NULL }, // sf32
 	{ 0x73696720U, "signature", NULL }, // sig
 	{ 0x74657874U, "text", typedec_text }, // text
@@ -63,6 +72,7 @@ static const struct taginfo taginfo_arr[] = {
 	{ 0x6258595AU, "blueColorant", NULL }, // bXYZ
 	{ 0x626B7074U, "mediaBlackPoint", NULL }, // bkpt
 	{ 0x63686164U, "chromaticAdaptation", NULL }, // chad
+	{ 0x6368726DU, "chromaticity", NULL }, // chrm
 	{ 0x636C7274U, "colorantTable", NULL }, // clrt
 	{ 0x63707274U, "copyright", NULL }, // cprt
 	{ 0x64657363U, "profileDescription", NULL }, // desc
@@ -144,8 +154,10 @@ static void typedec_desc(deark *c, lctx *d, de_int64 pos1, de_int64 len)
 	if(bytes_to_read>300) bytes_to_read=300;
 	dbuf_read_to_ucstring(c->infile, pos, bytes_to_read, s, 0, DE_ENCODING_UTF16BE);
 	ucstring_truncate_at_NUL(s);
-	ucstring_to_printable_sz(s, buf, sizeof(buf));
-	de_dbg(c, "Unicode localizable desc.: \"%s\"\n", buf);
+	if(s->len>0) {
+		ucstring_to_printable_sz(s, buf, sizeof(buf));
+		de_dbg(c, "Unicode localizable desc.: \"%s\"\n", buf);
+	}
 	pos += uloclen*2;
 	if(pos >= pos1+len) goto done;
 
@@ -154,7 +166,79 @@ static void typedec_desc(deark *c, lctx *d, de_int64 pos1, de_int64 len)
 
 done:
 	ucstring_destroy(s);
-	return;
+}
+
+static void do_mluc_record(deark *c, lctx *d, de_int64 tagstartpos,
+	de_int64 pos, de_int64 recsize)
+{
+	de_ucstring *s = NULL;
+	de_int64 string_len;
+	de_int64 string_offset;
+	de_int64 bytes_to_read;
+	de_byte buf1[300];
+	char buf2[300];
+
+	dbuf_read(c->infile, buf1, pos, 2);
+	de_bytes_to_printable_sz(buf1, 2, buf2, sizeof(buf2), 0, DE_ENCODING_ASCII);
+	de_dbg(c, "language code: '%s'\n", buf2);
+
+	dbuf_read(c->infile, buf1, pos+2, 2);
+	de_bytes_to_printable_sz(buf1, 2, buf2, sizeof(buf2), 0, DE_ENCODING_ASCII);
+	de_dbg(c, "country code: '%s'\n", buf2);
+
+	string_len = de_getui32be(pos+4);
+	string_offset = de_getui32be(pos+8);
+	de_dbg(c, "string offset=%d+%d, len=%d bytes\n", (int)tagstartpos,
+		(int)string_offset, (int)string_len);
+
+	bytes_to_read = string_len;
+	if(bytes_to_read>300) bytes_to_read=300;
+	if(bytes_to_read%2) bytes_to_read--; // UTF-16 should have an even number of bytes
+
+	s = ucstring_create(c);
+	dbuf_read_to_ucstring(c->infile, tagstartpos+string_offset, bytes_to_read,
+		s, 0, DE_ENCODING_UTF16BE);
+	ucstring_truncate_at_NUL(s);
+	ucstring_to_printable_sz(s, buf2, sizeof(buf2));
+	de_dbg(c, "string: \"%s\"\n", buf2);
+
+	ucstring_destroy(s);
+}
+
+static void typedec_mluc(deark *c, lctx *d, de_int64 pos1, de_int64 len)
+{
+	de_int64 pos = pos1;
+	de_int64 num_recs;
+	de_int64 recsize;
+	de_int64 rec_array_startpos;
+	de_int64 rec;
+
+	if(len<12) goto done;
+	pos += 8;
+
+	num_recs = de_getui32be(pos);
+	de_dbg(c, "number of records: %d\n", (int)num_recs);
+	pos += 4;
+
+	recsize = de_getui32be(pos);
+	de_dbg(c, "record size: %d\n", (int)recsize);
+	if(recsize<12) goto done;
+	pos += 4;
+
+	rec_array_startpos = pos;
+
+	for(rec=0; rec<num_recs; rec++) {
+		if(rec_array_startpos+rec*recsize > pos1+len) break;
+
+		de_dbg(c, "record #%d at %d\n", (int)rec, (int)pos);
+		de_dbg_indent(c, 1);
+		do_mluc_record(c, d, pos1, pos, recsize);
+		de_dbg_indent(c, -1);
+		pos += recsize;
+	}
+
+done:
+	;
 }
 
 static void do_read_header(deark *c, lctx *d, de_int64 pos)
@@ -248,13 +332,41 @@ static const struct taginfo *lookup_taginfo(de_uint32 id)
 	return NULL;
 }
 
-static void do_tag_data(deark *c, lctx *d, const struct de_fourcc *tag4cc,
-	const struct taginfo *ti,
+static int is_duplicate_data(deark *c, lctx *d, de_int64 tagindex,
+	de_int64 tagdataoffset, de_int64 tagdatalen,
+	de_int64 *idx_of_dup)
+{
+	de_int64 k;
+
+	for(k=0; k<tagindex; k++) {
+		if(d->tags_seen[k].offset==tagdataoffset &&
+			d->tags_seen[k].len==tagdatalen)
+		{
+			*idx_of_dup = k;
+			return 1;
+		}
+	}
+
+	*idx_of_dup = -1;
+	return 0;
+}
+
+static void do_tag_data(deark *c, lctx *d, de_int64 tagindex,
+	const struct de_fourcc *tag4cc, const struct taginfo *ti,
 	de_int64 tagdataoffset, de_int64 tagdatalen)
 {
 	struct de_fourcc tagtype4cc;
 	const struct datatypeinfo *dti;
 	const char *dtname;
+	de_int64 idx_of_dup;
+
+	if(tagdataoffset>=c->infile->len) return;
+	if(tagdatalen<4) return;
+
+	if(is_duplicate_data(c, d, tagindex, tagdataoffset, tagdatalen, &idx_of_dup)) {
+		de_dbg(c, "[data is a duplicate of tag #%d]\n", (int)idx_of_dup);
+		return;
+	}
 
 	dbuf_read_fourcc(c->infile, tagdataoffset, &tagtype4cc, 0);
 	dti = lookup_datatypeinfo(tagtype4cc.id);
@@ -290,31 +402,37 @@ static void do_tag(deark *c, lctx *d, de_int64 tagindex, de_int64 pos_in_tagtabl
 		(int)tagdataoffset, (int)tagdatalen);
 
 	de_dbg_indent(c, 1);
-	do_tag_data(c, d, &tag4cc, ti, tagdataoffset, tagdatalen);
+	do_tag_data(c, d, tagindex, &tag4cc, ti, tagdataoffset, tagdatalen);
 	de_dbg_indent(c, -1);
+
+	d->tags_seen[tagindex].offset = tagdataoffset;
+	d->tags_seen[tagindex].len = tagdatalen;
 }
 
 static void do_read_tags(deark *c, lctx *d, de_int64 pos1)
 {
 	de_int64 tagindex;
-	de_int64 num_tags;
 
 	de_dbg(c, "tag table at %d\n", (int)pos1);
 	de_dbg_indent(c, 1);
 
-	num_tags = de_getui32be(pos1);
-	de_dbg(c, "number of tags: %d\n", (int)num_tags);
-	if(num_tags>500) {
-		de_err(c, "Invalid or excessive number of tags: %d\n", (int)num_tags);
+	d->num_tags = de_getui32be(pos1);
+	de_dbg(c, "number of tags: %d\n", (int)d->num_tags);
+	if(d->num_tags>500) {
+		de_err(c, "Invalid or excessive number of tags: %d\n", (int)d->num_tags);
 		goto done;
 	}
-	de_dbg(c, "expected start of data segment: %d\n", (int)(pos1+4+12*num_tags));
+	de_dbg(c, "expected start of data segment: %d\n", (int)(pos1+4+12*d->num_tags));
 
-	for(tagindex=0; tagindex<num_tags; tagindex++) {
+	// Make a place to record some information about each tag we encounter in the table.
+	d->tags_seen = de_malloc(c, d->num_tags * sizeof(struct tag_seen_type));
+
+	for(tagindex=0; tagindex<d->num_tags; tagindex++) {
 		do_tag(c, d, tagindex, pos1+4+12*tagindex);
 	}
 
 done:
+	de_free(c, d->tags_seen);
 	de_dbg_indent(c, -1);
 }
 
