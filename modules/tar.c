cc36efbdc7f85ae686058e94f45482af3a3777c5
Jason Summers <jason1@pobox.com>
2016-12-01 10:14:29 -0500

    Working on tar module

--- modules/tar.c
+++ modules/tar.c
@@ -9,15 +9,19 @@
 DE_DECLARE_MODULE(de_module_tar);
 
 struct member_data {
+#define TARFMT_UNKNOWN  0
+#define TARFMT_POSIX    1
+#define TARFMT_GNU      2
+	int fmt;
+	de_byte linkflag;
 	de_int64 mode;
 	de_int64 filesize;
-	de_int64 filesize_padded;
 	de_ucstring *filename;
 	de_finfo *fi;
 };
 
 typedef struct localctx_struct {
-	int reserved;
+	int found_trailer;
 } lctx;
 
 static char byte_to_printable_char(de_byte b)
@@ -26,25 +30,33 @@ static char byte_to_printable_char(de_byte b)
 	return '_';
 }
 
+static const char* get_fmt_name(int fmt)
+{
+	const char *n = "unknown or old-style";
+	switch(fmt) {
+	case TARFMT_POSIX: n = "POSIX"; break;
+	case TARFMT_GNU: n = "GNU"; break;
+	}
+	return n;
+}
+
 static int read_member(deark *c, lctx *d, de_int64 pos1, de_int64 *bytes_consumed_member)
 {
 	struct member_data *md = NULL;
-	char name_orig[100];
+	char rawname_sz[100];
 	char magic[8];
-	de_int64 ext_name_len;
-	de_int64 longlink_data_len = 0;
-	size_t name_orig_len;
-	int is_ustar = 0;
-	de_byte linkflag1;
-	de_byte linkflag;
-	de_ucstring *rawname_ucstring = NULL;
-	de_int64 modtime_unix;
 	char timestamp_buf[64];
-	int is_dir;
-	int retval = 0;
+	de_int64 ext_name_len;
+	size_t rawname_sz_len;
+	de_byte linkflag1;
+	de_ucstring *rawname = NULL;
+	de_int64 modtime_unix;
+	int is_dir, is_regular_file;
 	int ret;
+	de_int64 longpath_data_len = 0;
 	de_int64 pos = pos1;
 	int saved_indent_level;
+	int retval = 0;
 
 	de_dbg_indent_save(c, &saved_indent_level);
 
@@ -55,12 +67,14 @@ static int read_member(deark *c, lctx *d, de_int64 pos1, de_int64 *bytes_consume
 	de_dbg(c, "archive member at %d\n", (int)pos1);
 	de_dbg_indent(c, 1);
 
-	// Try to figure out some things about the format of this member.
+	// Look ahead to try to figure out some things about the format of this member.
 
 	if(de_getbyte(pos1) == 0x00) {
 		// "The end of the archive is indicated by two records consisting
 		// entirely of zero bytes."
+		// TODO: We should maybe test more than just the first byte.
 		de_dbg(c, "[trailer record]\n");
+		d->found_trailer = 1;
 		retval = 1;
 		goto done;
 	}
@@ -68,45 +82,52 @@ static int read_member(deark *c, lctx *d, de_int64 pos1, de_int64 *bytes_consume
 	linkflag1 = de_getbyte(pos1+156);
 
 	de_read((de_byte*)magic, 257, 8);
-	if(!de_memcmp(magic, (const void*)"ustar", 5)) {
-		is_ustar = 1;
+	if(!de_memcmp(magic, (const void*)"ustar  \0", 8)) {
+		md->fmt = TARFMT_GNU;
+	}
+	else if(!de_memcmp(magic, (const void*)"ustar\0", 6)) {
+		md->fmt = TARFMT_POSIX;
 	}
 
+	de_dbg(c, "tar format: %s\n", get_fmt_name(md->fmt));
+
+	////
+
 	if(linkflag1 == 'L') {
-		de_dbg(c, "LongLink data at %d\n", (int)pos);
+		de_dbg(c, "LongPath data at %d\n", (int)pos);
 		de_dbg_indent(c, 1);
 		ret = dbuf_read_ascii_number(c->infile, pos+124, 11, 8, &ext_name_len);
 		if(!ret) goto done;
 		de_dbg(c, "ext. filename len: %d\n", (int)ext_name_len);
 		if(ext_name_len<1 || ext_name_len>32768) goto done;
 
-		pos += 512; // LongLink header record
+		pos += 512; // LongPath header record
 		de_dbg(c, "ext. filename at %d\n", (int)pos);
 		dbuf_read_to_ucstring(c->infile, pos, ext_name_len-1, md->filename, 0, DE_ENCODING_UTF8);
 		de_dbg(c, "ext. filename: \"%s\"\n", ucstring_get_printable_sz_n(md->filename, 300));
 
 		pos += de_pad_to_n(ext_name_len, 512); // The long filename
 
-		longlink_data_len = pos - pos1;
+		longpath_data_len = pos - pos1;
 		de_dbg_indent(c, -1);
 	}
 
 	de_dbg(c, "header at %d\n", (int)pos);
 	de_dbg_indent(c, 1);
 
-	de_read((de_byte*)name_orig, pos, 99);
-	name_orig[99] = '\0';
+	de_read((de_byte*)rawname_sz, pos, 99);
+	rawname_sz[99] = '\0';
 	pos += 100;
-	name_orig_len = de_strlen(name_orig);
+	rawname_sz_len = de_strlen(rawname_sz);
 
 	md->fi = de_finfo_create(c);
 
-	rawname_ucstring = ucstring_create(c);
-	ucstring_append_bytes(rawname_ucstring, (const de_byte*)name_orig, name_orig_len, 0, DE_ENCODING_UTF8);
-	de_dbg(c, "member raw name: \"%s\"\n", ucstring_get_printable_sz(rawname_ucstring));
+	rawname = ucstring_create(c);
+	ucstring_append_bytes(rawname, (const de_byte*)rawname_sz, rawname_sz_len, 0, DE_ENCODING_UTF8);
+	de_dbg(c, "member raw name: \"%s\"\n", ucstring_get_printable_sz(rawname));
 
 	if(md->filename->len==0) {
-		ucstring_append_ucstring(md->filename, rawname_ucstring);
+		ucstring_append_ucstring(md->filename, rawname);
 	}
 
 	ret = dbuf_read_ascii_number(c->infile, pos, 7, 8, &md->mode);
@@ -125,9 +146,6 @@ static int read_member(deark *c, lctx *d, de_int64 pos1, de_int64 *bytes_consume
 	pos += 12;
 	de_dbg(c, "size: %"INT64_FMT"\n", md->filesize);
 
-
-	md->filesize_padded = de_pad_to_n(md->filesize, 512);
-
 	ret = dbuf_read_ascii_number(c->infile, pos, 11, 8, &modtime_unix);
 	if(!ret) goto done;
 	de_unix_time_to_timestamp(modtime_unix, &md->fi->mod_time);
@@ -137,37 +155,57 @@ static int read_member(deark *c, lctx *d, de_int64 pos1, de_int64 *bytes_consume
 
 	pos += 8; // checksum
 
-	linkflag = de_getbyte(pos);
-	de_dbg(c, "linkflag/typeflag: 0x%02x ('%c')\n", (unsigned int)linkflag,
-		byte_to_printable_char(linkflag));
+	md->linkflag = de_getbyte(pos);
+	de_dbg(c, "linkflag/typeflag: 0x%02x ('%c')\n", (unsigned int)md->linkflag,
+		byte_to_printable_char(md->linkflag));
 	pos += 1;
 
 	pos += 100; // linkname (TODO)
 
 	pos += 255; // pad or magic, ... (TODO)
 
-	is_dir = 0;
-	if(is_ustar) {
-		if(linkflag == '5')
-			is_dir = 1;
-	}
-	else if(name_orig_len>0 && name_orig[name_orig_len-1]=='/') {
-		is_dir = 1;
-	}
-	if(is_dir) {
-		if(is_dir) md->filesize = 0;
-	}
-
 	de_dbg_indent(c, -1);
+
+	// Try to figure out what kind of "file" this is.
+
+	is_dir = 0;
+	is_regular_file = 0;
+
+	if(md->fmt==TARFMT_POSIX) {
+		if(md->linkflag=='0' || md->linkflag==0) {
+			is_regular_file = 1;
+		}
+		else if(md->linkflag=='5') {
+			is_dir = 1;
+		}
+	}
+	else if(md->fmt==TARFMT_GNU) {
+		if(md->linkflag=='0' || md->linkflag=='7' || md->linkflag==0) {
+			is_regular_file = 1;
+		}
+		else if(md->linkflag=='5') { // TODO: 'D'
+			is_dir = 1;
+		}
+	}
+	else {
+		if(rawname_sz_len>0 && rawname_sz[rawname_sz_len-1]=='/') {
+			is_dir = 1;
+		}
+		else if(md->linkflag==0 || md->linkflag=='0') {
+			is_regular_file = 1;
+		}
+	}
 
 	if(is_dir) {
 		de_dbg(c, "[directory, not extracting]\n");
+		md->filesize = 0;
 		retval = 1;
 		goto done;
 	}
 
-	if(is_ustar && linkflag!='0' && linkflag!=0) {
+	if(!is_regular_file) {
 		de_dbg(c, "[not a regular file, not extracting]\n");
+		md->filesize = 0; // FIXME: There may be cases where this is wrong.
 		retval = 1;
 		goto done;
 	}
@@ -186,8 +224,8 @@ static int read_member(deark *c, lctx *d, de_int64 pos1, de_int64 *bytes_consume
 	retval = 1;
 
 done:
-	*bytes_consumed_member = longlink_data_len + 512 + md->filesize_padded;
-	ucstring_destroy(rawname_ucstring);
+	*bytes_consumed_member = longpath_data_len + 512 + de_pad_to_n(md->filesize, 512);
+	ucstring_destroy(rawname);
 
 	if(md) {
 		ucstring_destroy(md->filename);
@@ -212,6 +250,7 @@ static void de_run_tar(deark *c, de_module_params *mparams)
 
 	pos = 0;
 	while(1) {
+		if(d->found_trailer) break;
 		if(pos >= c->infile->len) break;
 		if(pos+512 > c->infile->len) {
 			de_warn(c, "Ignoring %d extra bytes at end of file\n", (int)(c->infile->len - pos));
@@ -228,8 +267,9 @@ static void de_run_tar(deark *c, de_module_params *mparams)
 
 static int de_identify_tar(deark *c)
 {
-	//if(!dbuf_memcmp(c->infile, 257, "ustar", 5))
-	//	return 75;
+	if(!dbuf_memcmp(c->infile, 257, "ustar", 5))
+		return 90;
+	// TODO: Detect tar formats that don't have the "ustar" identifier.
 	return 0;
 }
 
@@ -239,5 +279,4 @@ void de_module_tar(deark *c, struct deark_module_info *mi)
 	mi->desc = "tar archive";
 	mi->run_fn = de_run_tar;
 	mi->identify_fn = de_identify_tar;
-	mi->flags |= DE_MODFLAG_NONWORKING;
 }
