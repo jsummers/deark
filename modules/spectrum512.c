50d181ef7ec8473b75a7ad595b978778edb496d0
Jason Summers <jason1@pobox.com>
2017-01-21 13:03:55 -0500

    spectrum512.c: Refactoring

--- modules/spectrum512.c
+++ modules/spectrum512.c
@@ -76,7 +76,7 @@ void de_module_spectrum512u(deark *c, struct deark_module_info *mi)
 // **************************************************************************
 
 // This is almost PackBits, but not quite.
-static void do_uncompress_spc_pixels(dbuf *f, de_int64 pos1, de_int64 len,
+static void spc_uncompress_pixels(dbuf *f, de_int64 pos1, de_int64 len,
 	dbuf *unc_pixels)
 {
 	de_int64 pos;
@@ -110,168 +110,7 @@ static void do_uncompress_spc_pixels(dbuf *f, de_int64 pos1, de_int64 len,
 	}
 }
 
-// After SPC decompression, we have a 320x199 image, but with the
-// bytes in an inconvenient order, different from SPU format.
-// This converts an SPC after-decompression byte offset (from 0 to 31839)
-// to the corresponding SPU file offset (from 160 to 31999).
-static de_int64 spc_cvtpos(de_int64 a)
-{
-	de_int64 b;
-
-	if(a%2)
-		b = 160 + (a-1)*4 + 1; // if odd
-	else
-		b = 160 + a*4; // if even
-
-	while(b>=32000) {
-		b -= (32000-(160+2));
-	}
-	return b;
-}
-
-static void spc_deplanarize(deark *c, dbuf *src, dbuf *dst)
-{
-	de_int64 i;
-	de_byte b;
-
-	for(i=0; i<src->len; i++) {
-		b = dbuf_getbyte(src, i);
-		dbuf_writebyte_at(dst, spc_cvtpos(i), b);
-	}
-}
-
-// Read from c->infile at offset pos1, append to uncmpr_pal
-static void spc_uncompress_pal(deark *c, de_int64 pos1, dbuf *uncmpr_pal)
-{
-	static const de_int64 num_pals = 199*3;
-	de_int64 pos = pos1;
-	de_int64 i, k;
-	unsigned int code;
-
-	for(i=0; i<num_pals; i++) {
-		code = (unsigned int)de_getui16be(pos);
-		pos += 2;
-		for(k=0; k<16; k++) {
-			// Bit 15 is ignored. The corresponding pal entry will always be black.
-			if(k<=14 && (code&0x1)) {
-				dbuf_copy(c->infile, pos, 2, uncmpr_pal);
-				pos += 2;
-			}
-			else {
-				dbuf_write_zeroes(uncmpr_pal, 2);
-			}
-			code >>= 1;
-		}
-	}
-}
-
-static void de_run_spectrum512c(deark *c, de_module_params *mparams)
-{
-	de_int64 pixels_cmpr_len;
-	de_int64 pal_cmpr_len;
-	de_int64 pos;
-	dbuf *unc_pixels_planar = NULL;
-	dbuf *spufile = NULL;
-	int to_spu = 0;
-
-	if(de_get_ext_option(c, "spectrum512:tospu")) {
-		to_spu = 1;
-	}
-
-	pos = 4;
-	pixels_cmpr_len = de_getui32be(pos);
-	de_dbg(c, "pixels compressed len: %d\n", (int)pixels_cmpr_len);
-	pos += 4;
-	pal_cmpr_len = de_getui32be(pos);
-	de_dbg(c, "palette compressed len: %d\n", (int)pal_cmpr_len);
-	pos += 4;
-
-	de_dbg(c, "pixels at %d\n", (int)pos);
-	// Decompress the pixel data into an in-memory buffer.
-	unc_pixels_planar = dbuf_create_membuf(c, 32000, 1);
-	do_uncompress_spc_pixels(c->infile, pos, pixels_cmpr_len, unc_pixels_planar);
-	pos += pixels_cmpr_len;
-
-	// We'll construct an in-memory SPU file, then (usually) use our
-	// SPU module's decoder to process it.
-	spufile = dbuf_create_membuf(c, 51104, 0x1);
-
-	// Rearrange the bytes in the image data, as we write them to our
-	// in-memory image of an SPU file.
-	// (This could be done more efficiently during decompression,
-	// but the code would be messier.)
-	spc_deplanarize(c, unc_pixels_planar, spufile);
-
-	// Make sure we write the uncompressed palette at exactly offset 32000.
-	dbuf_truncate(spufile, 32000);
-
-	de_dbg(c, "palette at %d\n", (int)pos);
-	spc_uncompress_pal(c, pos, spufile);
-
-	if(to_spu) {
-		// Instead of decoding the image, write it in .SPU format
-		dbuf *outf = NULL;
-		outf = dbuf_create_output_file(c, "spu", NULL, 0);
-		dbuf_copy(spufile, 0, spufile->len, outf);
-		dbuf_close(outf);
-	}
-	else {
-		do_spu_internal(c, spufile);
-	}
-
-	dbuf_close(unc_pixels_planar);
-	dbuf_close(spufile);
-}
-
-static int de_identify_spectrum512c(deark *c)
-{
-	if(dbuf_memcmp(c->infile, 0, "\x53\x50\x00\x00", 4))
-		return 0;
-
-	if(de_input_file_has_ext(c, "spc")) {
-		return 100;
-	}
-
-	if(de_input_file_has_ext(c, "sps")) {
-		return 0;
-	}
-
-	return 10;
-}
-
-void de_module_spectrum512c(deark *c, struct deark_module_info *mi)
-{
-	mi->id = "spectrum512c";
-	mi->desc = "Spectrum 512 Compressed";
-	mi->run_fn = de_run_spectrum512c;
-	mi->identify_fn = de_identify_spectrum512c;
-}
-
-// **************************************************************************
-
-// After SPS0 decompression, we have a 320x199 image, but with the
-// bytes in an inconvenient order, different from SPU format.
-// This converts an SPS0 after-decompression byte offset (from 0 to 31839)
-// to the corresponding SPU file offset (from 160 to 31999).
-static de_int64 sps0_cvtpos(de_int64 a)
-{
-	de_int64 b;
-	b = 160 + (a%199)*160 + ((a%7960)/398)*8 + (a/(199*40))*2 + (a%398)/199;
-	return b;
-}
-
-static void sps0_deplanarize(deark *c, dbuf *src, dbuf *dst)
-{
-	de_int64 i;
-	de_byte b;
-
-	for(i=0; i<src->len; i++) {
-		b = dbuf_getbyte(src, i);
-		dbuf_writebyte_at(dst, sps0_cvtpos(i), b);
-	}
-}
-
-static void do_uncompress_sps_pixels(dbuf *f, de_int64 pos1, de_int64 len,
+static void sps_uncompress_pixels(dbuf *f, de_int64 pos1, de_int64 len,
 	dbuf *unc_pixels)
 {
 	de_int64 pos;
@@ -305,6 +144,72 @@ static void do_uncompress_sps_pixels(dbuf *f, de_int64 pos1, de_int64 len,
 	}
 }
 
+
+// After SPC decompression, we have a 320x199 image, but with the
+// bytes in an inconvenient order, different from SPU format.
+// This converts an SPC after-decompression byte offset (from 0 to 31839)
+// to the corresponding SPU file offset (from 160 to 31999).
+static de_int64 reorderfn_spc(de_int64 a)
+{
+	de_int64 b;
+
+	if(a%2)
+		b = 160 + (a-1)*4 + 1; // if odd
+	else
+		b = 160 + a*4; // if even
+
+	while(b>=32000) {
+		b -= (32000-(160+2));
+	}
+	return b;
+}
+
+// For SPS type 0. See reorderfn_spc() comments for details.
+static de_int64 reorderfn_sps0(de_int64 a)
+{
+	de_int64 b;
+	b = 160 + (a%199)*160 + ((a%7960)/398)*8 + (a/7960)*2 + (a%398)/199;
+	return b;
+}
+
+typedef de_int64 (*reorder_fn)(de_int64 a);
+
+static void reorder_img_bytes(deark *c, dbuf *src, dbuf *dst, reorder_fn rfn)
+{
+	de_int64 i;
+	de_byte b;
+
+	for(i=0; i<src->len; i++) {
+		b = dbuf_getbyte(src, i);
+		dbuf_writebyte_at(dst, rfn(i), b);
+	}
+}
+
+// Read from c->infile at offset pos1, append to uncmpr_pal
+static void spc_uncompress_pal(deark *c, de_int64 pos1, dbuf *uncmpr_pal)
+{
+	static const de_int64 num_pals = 199*3;
+	de_int64 pos = pos1;
+	de_int64 i, k;
+	unsigned int code;
+
+	for(i=0; i<num_pals; i++) {
+		code = (unsigned int)de_getui16be(pos);
+		pos += 2;
+		for(k=0; k<16; k++) {
+			// Bit 15 is ignored. The corresponding pal entry will always be black.
+			if(k<=14 && (code&0x1)) {
+				dbuf_copy(c->infile, pos, 2, uncmpr_pal);
+				pos += 2;
+			}
+			else {
+				dbuf_write_zeroes(uncmpr_pal, 2);
+			}
+			code >>= 1;
+		}
+	}
+}
+
 struct bit_reader {
 	de_int64 nextbytepos;
 	de_byte cur_byte;
@@ -332,7 +237,7 @@ static unsigned int bit_reader_getint(dbuf *f, struct bit_reader *br, unsigned i
 }
 
 // Read from c->infile at offset pos1, append to uncmpr_pal
-static void spu_uncompress_pal(deark *c, de_int64 pos1, dbuf *uncmpr_pal)
+static void sps_uncompress_pal(deark *c, de_int64 pos1, dbuf *uncmpr_pal)
 {
 	static const de_int64 num_pals = 199*3;
 	de_int64 i;
@@ -365,7 +270,7 @@ static void spu_uncompress_pal(deark *c, de_int64 pos1, dbuf *uncmpr_pal)
 	}
 }
 
-static void de_run_spectrum512s(deark *c, de_module_params *mparams)
+static void do_run_spectrum512c_s_internal(deark *c, de_module_params *mparams, int is_sps)
 {
 	de_int64 pixels_cmpr_len;
 	de_int64 pal_cmpr_len;
@@ -374,9 +279,7 @@ static void de_run_spectrum512s(deark *c, de_module_params *mparams)
 	dbuf *unc_pixels_planar = NULL;
 	dbuf *spufile = NULL;
 	int to_spu = 0;
-	unsigned int format_code;
-
-	// TODO: Consolidate most of this code with .SPC
+	unsigned int sps_format_code = 0;
 
 	if(de_get_ext_option(c, "spectrum512:tospu")) {
 		to_spu = 1;
@@ -397,14 +300,21 @@ static void de_run_spectrum512s(deark *c, de_module_params *mparams)
 		goto done;
 	}
 
-	format_code = de_getbyte(pal_pos + pal_cmpr_len-1);
-	format_code &= 0x1;
-	de_dbg(c, "format code: %u\n", format_code);
+	if(is_sps) {
+		sps_format_code = de_getbyte(pal_pos + pal_cmpr_len-1);
+		sps_format_code &= 0x1;
+		de_dbg(c, "format code: %u\n", sps_format_code);
+	}
 
 	de_dbg(c, "pixels at %d\n", (int)pos);
 	// Decompress the pixel data into an in-memory buffer.
 	unc_pixels_planar = dbuf_create_membuf(c, 32000, 1);
-	do_uncompress_sps_pixels(c->infile, pos, pixels_cmpr_len, unc_pixels_planar);
+	if(is_sps) {
+		sps_uncompress_pixels(c->infile, pos, pixels_cmpr_len, unc_pixels_planar);
+	}
+	else {
+		spc_uncompress_pixels(c->infile, pos, pixels_cmpr_len, unc_pixels_planar);
+	}
 	pos += pixels_cmpr_len;
 
 	// We'll construct an in-memory SPU file, then (usually) use our
@@ -415,11 +325,11 @@ static void de_run_spectrum512s(deark *c, de_module_params *mparams)
 	// in-memory image of an SPU file.
 	// (This could be done more efficiently during decompression,
 	// but the code would be messier.)
-	if(format_code==0) {
-		sps0_deplanarize(c, unc_pixels_planar, spufile);
+	if(is_sps && sps_format_code==0) {
+		reorder_img_bytes(c, unc_pixels_planar, spufile, reorderfn_sps0);
 	}
 	else {
-		spc_deplanarize(c, unc_pixels_planar, spufile);
+		reorder_img_bytes(c, unc_pixels_planar, spufile, reorderfn_spc);
 	}
 
 	// Make sure we write the uncompressed palette at exactly offset 32000.
@@ -427,7 +337,12 @@ static void de_run_spectrum512s(deark *c, de_module_params *mparams)
 
 	pos = pal_pos;
 	de_dbg(c, "palette at %d\n", (int)pos);
-	spu_uncompress_pal(c, pos, spufile);
+	if(is_sps) {
+		sps_uncompress_pal(c, pos, spufile);
+	}
+	else {
+		spc_uncompress_pal(c, pos, spufile);
+	}
 
 	if(to_spu) {
 		// Instead of decoding the image, write it in .SPU format
@@ -443,6 +358,40 @@ static void de_run_spectrum512s(deark *c, de_module_params *mparams)
 done:
 	dbuf_close(unc_pixels_planar);
 	dbuf_close(spufile);
+}
+
+static void de_run_spectrum512c(deark *c, de_module_params *mparams)
+{
+	do_run_spectrum512c_s_internal(c, mparams, 0);
+}
+
+static int de_identify_spectrum512c(deark *c)
+{
+	if(dbuf_memcmp(c->infile, 0, "\x53\x50\x00\x00", 4))
+		return 0;
+
+	if(de_input_file_has_ext(c, "spc")) {
+		return 100;
+	}
+
+	if(de_input_file_has_ext(c, "sps")) {
+		return 0;
+	}
+
+	return 10;
+}
+
+void de_module_spectrum512c(deark *c, struct deark_module_info *mi)
+{
+	mi->id = "spectrum512c";
+	mi->desc = "Spectrum 512 Compressed";
+	mi->run_fn = de_run_spectrum512c;
+	mi->identify_fn = de_identify_spectrum512c;
+}
+
+static void de_run_spectrum512s(deark *c, de_module_params *mparams)
+{
+	do_run_spectrum512c_s_internal(c, mparams, 1);
 }
 
 static int de_identify_spectrum512s(deark *c)
