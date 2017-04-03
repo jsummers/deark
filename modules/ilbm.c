01f000b9fca2162d31d79b2217d44b8f915f951c
Jason Summers <jason1@pobox.com>
2017-04-03 12:12:39 -0400

    New "anim" module

--- modules/ilbm.c
+++ modules/ilbm.c
@@ -8,8 +8,10 @@
 #include <deark-private.h>
 #include "fmtutil.h"
 DE_DECLARE_MODULE(de_module_ilbm);
+DE_DECLARE_MODULE(de_module_anim);
 
 #define CODE_ABIT  0x41424954
+#define CODE_ANHD  0x414e4844U
 #define CODE_BMHD  0x424d4844
 #define CODE_BODY  0x424f4459
 #define CODE_CAMG  0x43414d47
@@ -827,7 +829,7 @@ static void do_multipalette(deark *c, lctx *d, de_uint32 chunktype)
 	d->errflag = 1;
 }
 
-static int my_iff_chunk_handler(deark *c, struct de_iffctx *ictx)
+static int my_ilbm_chunk_handler(deark *c, struct de_iffctx *ictx)
 {
 	int quitflag = 0;
 	int is_vdat;
@@ -1004,7 +1006,7 @@ static void de_run_ilbm(deark *c, de_module_params *mparams)
 	if(s) d->opt_fixpal = de_atoi(s);
 
 	ictx->userdata = (void*)d;
-	ictx->handle_chunk_fn = my_iff_chunk_handler;
+	ictx->handle_chunk_fn = my_ilbm_chunk_handler;
 	ictx->on_std_container_start_fn = my_on_std_container_start_fn;
 	ictx->on_container_end_fn = my_on_container_end_fn;
 	ictx->f = c->infile;
@@ -1037,4 +1039,107 @@ void de_module_ilbm(deark *c, struct deark_module_info *mi)
 	mi->desc = "IFF-ILBM and related image formats";
 	mi->run_fn = de_run_ilbm;
 	mi->identify_fn = de_identify_ilbm;
+}
+
+// -----------------------------------
+
+typedef struct animctx_struct {
+	int reserved;
+} animctx;
+
+static void do_anim_anhd(deark *c, animctx *d, de_int64 pos, de_int64 len)
+{
+	de_byte op;
+	de_int64 tmp;
+
+	if(len<24) return;
+
+	op = de_getbyte(pos++);
+	de_dbg(c, "operation: %d\n", (int)op);
+
+	pos++; // Mask
+	pos+=2; // w
+	pos+=2; // h
+	pos+=2; // x
+	pos+=2; // y
+	pos+=4; // abstime
+
+	tmp = de_getui32be(pos); // reltime
+	de_dbg(c, "reltime: %.5f sec\n", ((double)tmp)/60.0);
+	pos+=4;
+
+	pos++; // interleave
+	pos++; // pad0
+
+	// bits
+	if(op==4 || op==5) {
+		tmp = de_getui32be(pos);
+		de_dbg(c, "flags: 0x%08u\n", (unsigned int)tmp);
+	}
+	pos+=4;
+}
+
+static int my_anim_chunk_handler(deark *c, struct de_iffctx *ictx)
+{
+	int quitflag = 0;
+	int saved_indent_level;
+	animctx *d = (animctx*)ictx->userdata;
+
+	de_dbg_indent_save(c, &saved_indent_level);
+
+	// Pretend we can handle all nonstandard chunks
+	if(!de_fmtutil_is_standard_iff_chunk(c, ictx, ictx->chunkctx->chunk4cc.id)) {
+		ictx->handled = 1;
+	}
+
+	switch(ictx->chunkctx->chunk4cc.id) {
+	case CODE_ANHD:
+		do_anim_anhd(c, d, ictx->chunkctx->chunk_dpos, ictx->chunkctx->chunk_dlen);
+		break;
+
+	case CODE_FORM:
+		if(ictx->level>1) break;
+		ictx->is_std_container = 1;
+		break;
+	}
+
+	de_dbg_indent_restore(c, saved_indent_level);
+	return (quitflag) ? 0 : 1;
+}
+
+static void de_run_anim(deark *c, de_module_params *mparams)
+{
+	animctx *d = NULL;
+	struct de_iffctx *ictx = NULL;
+
+	d = de_malloc(c, sizeof(lctx));
+
+	ictx = de_malloc(c, sizeof(struct de_iffctx));
+	ictx->userdata = (void*)d;
+	ictx->handle_chunk_fn = my_anim_chunk_handler;
+	ictx->f = c->infile;
+	de_fmtutil_read_iff_format(c, ictx, 0, c->infile->len);
+
+	de_free(c, ictx);
+	de_free(c, d);
+}
+
+static int de_identify_anim(deark *c)
+{
+	de_byte buf[12];
+	de_read(buf, 0, 12);
+
+	if(!de_memcmp(buf, "FORM", 4)) {
+		if(!de_memcmp(&buf[8], "ANIM", 4)) return 100;
+	}
+	return 0;
+}
+
+void de_module_anim(deark *c, struct deark_module_info *mi)
+{
+	mi->id = "anim";
+	mi->desc = "IFF-ANIM animation";
+	mi->run_fn = de_run_anim;
+	mi->identify_fn = de_identify_anim;
+	mi->flags |= DE_MODFLAG_NONWORKING;
 }
