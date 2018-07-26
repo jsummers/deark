8a8ba1f854c28749f6246ec15a5f1be28d00f788
Jason Summers <jason1@pobox.com>
2018-07-26 12:24:33 -0400

    pict: Hexdump LongComment records

--- modules/pict.c
+++ modules/pict.c
@@ -812,9 +812,13 @@ static int handler_a1(deark *c, lctx *d, de_int64 opcode, de_int64 data_pos, de_
 		dbuf_read_fourcc(c->infile, data_pos+4, &sig4cc, 4, 0x0);
 		de_dbg(c, "application comment, signature=0x%08x '%s'",
 			(unsigned int)sig4cc.id, sig4cc.id_dbgstr);
+		de_dbg_hexdump(c, c->infile, data_pos+8, len-4, 256, NULL, 0x1);
 	}
 	else if(kind==224) {
 		do_iccprofile_item(c, d, data_pos+4, len);
+	}
+	else {
+		de_dbg_hexdump(c, c->infile, data_pos+4, len, 256, NULL, 0x1);
 	}
 
 	return 1;
