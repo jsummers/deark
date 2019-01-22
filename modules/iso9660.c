e398f4a528b0abedf8e6545201de909f56b5e2c9
Jason Summers <jason1@pobox.com>
2019-01-22 11:59:50 -0500

    Working on iso9660 module

--- modules/iso9660.c
+++ modules/iso9660.c
@@ -11,9 +11,14 @@ DE_DECLARE_MODULE(de_module_iso9660);
 struct dir_record {
 	u8 file_flags;
 	u8 is_dir;
-	u8 is_dir_00;
-	u8 is_dir_01;
+	u8 is_thisdir;
+	u8 is_parentdir;
+	i64 len_dir_rec;
+	i64 len_ext_attr_rec;
+	i64 data_len;
 	i64 file_id_len;
+	i64 extent_blk;
+	de_ucstring *fname;
 };
 
 typedef struct localctx_struct {
@@ -22,6 +27,9 @@ typedef struct localctx_struct {
 	i64 path_table_size;
 	i64 path_table_L_secnum;
 	i64 path_table_M_secnum;
+	struct dir_record *root_dr;
+	struct de_strarray *curpath;
+	struct de_inthashtable *dirs_seen;
 } lctx;
 
 static i64 getu16bbo_p(dbuf *f, i64 *ppos)
@@ -50,6 +58,8 @@ static void read_iso_string(deark *c, lctx *d, i64 pos, i64 len, de_ucstring *s)
 
 static void free_dir_record(deark *c, struct dir_record *dr)
 {
+	if(!dr) return;
+	ucstring_destroy(dr->fname);
 	de_free(c, dr);
 }
 
@@ -66,21 +76,71 @@ static const char *get_vol_descr_type_name(u8 t)
 	return name?name:"?";
 }
 
-static void do_directory_record(deark *c, lctx *d, i64 pos1, struct dir_record *dr)
+static void fixup_filename(deark *c, lctx *d, de_ucstring *fname)
+{
+	if(fname->len<3) return;
+	if(fname->str[fname->len-2]==';' &&
+		fname->str[fname->len-1]=='1')
+	{
+		ucstring_truncate(fname, fname->len-2);
+	}
+}
+
+// Handle (presumably extract) the contents of the file represented by the
+// given dir_record.
+static void do_file(deark *c, lctx *d, struct dir_record *dr)
+{
+	i64 dpos, dlen;
+	de_finfo *fi = NULL;
+	de_ucstring *final_name = NULL;
+
+	if(dr->extent_blk<1) goto done;
+	dpos = dr->extent_blk * d->secsize;
+	dlen = dr->data_len;
+
+	if(dpos+dlen > c->infile->len) goto done;
+
+	fi = de_finfo_create(c);
+
+	final_name = ucstring_create(c);
+	de_strarray_make_path(d->curpath, final_name, 0);
+
+	if(ucstring_isnonempty(dr->fname)) {
+		ucstring_append_ucstring(final_name, dr->fname);
+		fixup_filename(c, d, final_name);
+		de_finfo_set_name_from_ucstring(c, fi, final_name);
+		fi->original_filename_flag = 1;
+	}
+
+	dbuf_create_file_from_slice(c->infile, dpos, dlen, NULL, fi, 0);
+
+done:
+	ucstring_destroy(final_name);
+	de_finfo_destroy(c, fi);
+}
+
+static void do_directory(deark *c, lctx *d, i64 pos1, i64 len, int nesting_level);
+
+// Caller allocates dr
+static int do_directory_record(deark *c, lctx *d, i64 pos1, struct dir_record *dr, int nesting_level)
 {
 	i64 n;
 	i64 pos = pos1;
 	u8 b;
 	de_ucstring *tmps = NULL;
-	de_ucstring *s = NULL;
+	int retval = 0;
 
-	pos++; // len of dir record
-	pos++; // extended attr rec lengh
+	dr->len_dir_rec = (i64)de_getbyte_p(&pos);
+	de_dbg(c, "dir rec len: %u", (unsigned int)dr->len_dir_rec);
+	if(dr->len_dir_rec<1) goto done;
 
-	n = getu32bbo_p(c->infile, &pos);
-	de_dbg(c, "loc. of extent: block #%u", (unsigned int)n);
-	n = getu32bbo_p(c->infile, &pos);
-	de_dbg(c, "data length: %u", (unsigned int)n);
+	dr->len_ext_attr_rec = (i64)de_getbyte_p(&pos);
+	de_dbg(c, "ext attrib rec len: %u", (unsigned int)dr->len_ext_attr_rec);
+
+	dr->extent_blk = getu32bbo_p(c->infile, &pos);
+	de_dbg(c, "loc. of extent: block #%u", (unsigned int)dr->extent_blk);
+	dr->data_len = getu32bbo_p(c->infile, &pos);
+	de_dbg(c, "data length: %u", (unsigned int)dr->data_len);
 	pos += 7; // recording time
 
 	dr->file_flags = de_getbyte_p(&pos);
@@ -105,23 +165,80 @@ static void do_directory_record(deark *c, lctx *d, i64 pos1, struct dir_record *
 	de_dbg(c, "volume sequence number: %u", (unsigned int)n);
 	dr->file_id_len = (i64)de_getbyte_p(&pos);
 
-	s = ucstring_create(c);
-	dbuf_read_to_ucstring(c->infile, pos, dr->file_id_len, s, 0, DE_ENCODING_ASCII);
-	de_dbg(c, "file id: \"%s\"", ucstring_getpsz_d(s));
+	dr->fname = ucstring_create(c);
+	dbuf_read_to_ucstring(c->infile, pos, dr->file_id_len, dr->fname, 0, DE_ENCODING_PRINTABLEASCII);
+	de_dbg(c, "file id: \"%s\"", ucstring_getpsz_d(dr->fname));
 	if(dr->is_dir && dr->file_id_len==1) {
 		b = de_getbyte(pos);
 		if(b==0x00) {
-			dr->is_dir_00 = 1;
+			dr->is_thisdir = 1;
 		}
 		else if(b==0x01) {
-			dr->is_dir_01 = 1;
+			dr->is_parentdir = 1;
 		}
 	}
 
-	pos += dr->file_id_len;
+	if(dr->is_dir && !dr->is_thisdir && !dr->is_parentdir) {
+		de_strarray_push(d->curpath, dr->fname);
+		do_directory(c, d, dr->extent_blk * d->secsize, dr->data_len, nesting_level+1);
+		de_strarray_pop(d->curpath);
+	}
+	else if(!dr->is_dir) {
+		do_file(c, d, dr);
+	}
 
-	ucstring_destroy(s);
+	pos += dr->file_id_len;
+	retval = 1;
+
+done:
 	ucstring_destroy(tmps);
+	return retval;
+}
+
+// A sequence of directory_records
+static void do_directory(deark *c, lctx *d, i64 pos1, i64 len, int nesting_level)
+{
+	struct dir_record *dr = NULL;
+	i64 pos = pos1;
+	int saved_indent_level;
+
+	de_dbg_indent_save(c, &saved_indent_level);
+	if(pos1<=0) goto done;
+
+	de_dbg(c, "directory at %"I64_FMT, pos1);
+	de_dbg_indent(c, 1);
+
+	if(!de_inthashtable_add_item(c, d->dirs_seen, pos1, NULL)) {
+		de_warn(c, "Duplicate directory or loop detected (@%"I64_FMT")", pos1);
+		goto done;
+	}
+
+	if(nesting_level>32) {
+		de_err(c, "Maximum directory nesting level exceeded");
+		goto done;
+	}
+
+	while(1) {
+		int ret;
+
+		if(pos >= pos1+len) break;
+
+		de_dbg(c, "directory record at %"I64_FMT" (for directory@%"I64_FMT")", pos, pos1);
+		dr = de_malloc(c, sizeof(struct dir_record));
+		de_dbg_indent(c, 1);
+		ret = do_directory_record(c, d, pos, dr, nesting_level);
+		de_dbg_indent(c, -1);
+		if(!ret) break;
+		if(dr->len_dir_rec<1) break;
+
+		pos += dr->len_dir_rec; // + ext_len??
+		free_dir_record(c, dr);
+		dr = NULL;
+	}
+
+done:
+	if(dr) free_dir_record(c, dr);
+	de_dbg_indent_restore(c, saved_indent_level);
 }
 
 static void do_path_table_record(deark *c, lctx *d, i64 idx,
@@ -148,7 +265,7 @@ static void do_path_table_record(deark *c, lctx *d, i64 idx,
 	de_dbg(c, "parent dir number: %u", (unsigned int)n);
 
 	s = ucstring_create(c);
-	dbuf_read_to_ucstring(c->infile, pos, dir_id_len, s, 0, DE_ENCODING_ASCII);
+	dbuf_read_to_ucstring(c->infile, pos, dir_id_len, s, 0, DE_ENCODING_PRINTABLEASCII);
 	de_dbg(c, "dir id: \"%s\"", ucstring_getpsz_d(s));
 	pos += dir_id_len;
 
@@ -196,7 +313,6 @@ static void do_primary_volume_descriptor(deark *c, lctx *d, i64 pos1)
 	i64 block_size;
 	i64 n;
 	de_ucstring *tmpstr = NULL;
-	struct dir_record *root_dr = NULL;
 
 	tmpstr = ucstring_create(c);
 	read_iso_string(c, d, pos, 32, tmpstr);
@@ -234,8 +350,9 @@ static void do_primary_volume_descriptor(deark *c, lctx *d, i64 pos1)
 
 	de_dbg(c, "dir record for root dir");
 	de_dbg_indent(c, 1);
-	root_dr = de_malloc(c, sizeof(struct dir_record));
-	do_directory_record(c, d, pos, root_dr);
+	d->root_dr = de_malloc(c, sizeof(struct dir_record));
+	do_directory_record(c, d, pos, d->root_dr, 0);
+
 	de_dbg_indent(c, -1);
 	pos += 34;
 
@@ -275,7 +392,6 @@ static void do_primary_volume_descriptor(deark *c, lctx *d, i64 pos1)
 	d->file_structure_version = de_getbyte_p(&pos);
 	de_dbg(c, "file structure version: %u", (unsigned int)d->file_structure_version);
 
-	free_dir_record(c, root_dr);
 	ucstring_destroy(tmpstr);
 }
 
@@ -342,14 +458,28 @@ static void de_run_iso9660(deark *c, de_module_params *mparams)
 		cursec++;
 	}
 
-	if(d->path_table_L_secnum) {
-		do_path_table(c, d, d->secsize * d->path_table_L_secnum, 1);
-	}
-	else if(d->path_table_M_secnum) {
-		do_path_table(c, d, d->secsize * d->path_table_M_secnum, 0);
+	if(de_get_ext_option_bool(c, "iso9660:readpathtable", 0)) {
+		if(d->path_table_L_secnum) {
+			do_path_table(c, d, d->secsize * d->path_table_L_secnum, 1);
+		}
+		else if(d->path_table_M_secnum) {
+			do_path_table(c, d, d->secsize * d->path_table_M_secnum, 0);
+		}
 	}
 
-	de_free(c, d);
+	d->dirs_seen = de_inthashtable_create(c);
+	d->curpath = de_strarray_create(c);
+
+	if(d->root_dr) {
+		do_directory(c, d, d->secsize * d->root_dr->extent_blk, d->root_dr->data_len, 0);
+	}
+
+	if(d) {
+		free_dir_record(c, d->root_dr);
+		de_strarray_destroy(d->curpath);
+		de_inthashtable_destroy(c, d->dirs_seen);
+		de_free(c, d);
+	}
 }
 
 static int de_identify_iso9660(deark *c)
