10f3158ec46942682500626b2f176854f203d924
Jason Summers <jason1@pobox.com>
2019-07-05 14:22:44 -0400

    Added a custom manifest file for VS builds
    This is basically to declare that Deark knows that Windows 10 exists.
    
    (Seems like there must be a better way to do this, as it's about the
    most standard manifest you can have.)

--- scripts/makerelease.sh
+++ scripts/makerelease.sh
@@ -18,7 +18,7 @@ D=".build-tmp/deark-$VER"
 mkdir $D
 
 mkdir $D/src
-cp -p src/*.c src/*.h src/*.rc $D/src/
+cp -p src/*.c src/*.h src/*.rc src/*.manifest $D/src/
 
 mkdir $D/modules
 cp -p modules/*.c $D/modules/
