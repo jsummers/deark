7e539cd548bb29254d79d8a1d993518e6b924862
Jason Summers <jason1@pobox.com>
2019-08-14 07:08:02 -0400

    unimplode6a.h: Made the terms of use independent of Deark's

--- foreign/unimplode6a.h
+++ foreign/unimplode6a.h
@@ -1,11 +1,74 @@
-// ZIP type 6 "implode" decompression.
-// Based on Mark Adler's public domain code from Info-ZIP UnZip v5.4
-// (mainly from explode.c and inflate.c).
-// See the file readme-explode.txt for more information.
-// The code has been heavily modified for Deark (2019-03).
-// This file (unimplode6a.h) is hereby left in the public domain; or it may, at
-// your option, be distributed under the same terms as the main Deark software.
-// -JS
+// *** Pre-release version. This file is used by Deark, but is not yet ready
+// for general use. ***
+
+// Unimplode6a: A single-header-file library for decompressing ZIP method 6
+// ("implode") compression
+//
+// This file, normally named unimplode6a.h, hereinafter referred to as "this
+// file" or "this software", is an independent software library.
+//
+// This software was originally derived from Mark Adler's public domain code
+// from Info-ZIP UnZip v5.4 (the files explode.c and inflate.c).
+// It has been heavily modified by Jason Summers.
+// UnZip 5.4 is typically found in a file named "unzip540.tar.gz" or
+// "unzip540.zip".
+
+/*
+==============================================================================
+These terms of use apply only to this file, and not to any other files that
+might appear alongside it.
+
+This software is dual-licensed. Choose the license you prefer:
+------------------------------------------------------------------------------
+Licence option #1: MIT
+Copyright (C) 2019 Jason Summers
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+------------------------------------------------------------------------------
+Licence option #2: Public domain
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+==============================================================================
+*/
 
 // Selected comments from original explode.c and inflate.c:
 
