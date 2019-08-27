bd7131510d5fe83c13d6ccc0f54fcd6609bf7c81
Jason Summers <jason1@pobox.com>
2019-08-27 09:30:43 -0400

    Working on arc module

--- modules/arc.c
+++ modules/arc.c
@@ -6,13 +6,19 @@
 
 #include <deark-config.h>
 #include <deark-private.h>
+#include <deark-fmtutil.h>
 DE_DECLARE_MODULE(de_module_arc);
+
+struct localctx_struct;
+typedef struct localctx_struct lctx;
+struct member_data;
+typedef int (*decompressor_fn)(deark *c, lctx *d, struct member_data *md, dbuf *outf);
 
 struct cmpr_meth_info {
 	u8 cmpr_meth;
 	unsigned int flags;
 	const char *name;
-	void *reserved;
+	decompressor_fn decompressor;
 };
 
 struct member_data {
@@ -20,19 +26,37 @@ struct member_data {
 	const struct cmpr_meth_info *cmi;
 	i64 cmpr_size;
 	i64 orig_size;
-	unsigned int crc;
+	i64 cmpr_data_pos;
+	u32 crc_reported;
+	u32 crc_calc;
 	de_ucstring *fn;
 };
 
-typedef struct localctx_struct {
+struct localctx_struct {
+	int input_encoding;
 	int member_count;
-} lctx;
+	struct de_crcobj *crco;
+};
 
+static int decompress_stored(deark *c, lctx *d, struct member_data *md, dbuf *outf)
+{
+	dbuf_copy(c->infile, md->cmpr_data_pos, md->cmpr_size, outf);
+	return 1;
+}
+
+static int decompress_packed(deark *c, lctx *d, struct member_data *md, dbuf *outf)
+{
+	int ret;
+
+	ret = de_fmtutil_decompress_rle90(c->infile, md->cmpr_data_pos, md->cmpr_size, outf,
+		1, md->orig_size, 0);
+	return ret;
+}
 
 static const struct cmpr_meth_info cmpr_meth_info_arr[] = {
-	{ 0x01, 0, "stored (old format)", NULL },
-	{ 0x02, 0, "stored", NULL },
-	{ 0x03, 0, "packed (RLE)", NULL },
+	{ 0x01, 0, "stored (old format)", decompress_stored },
+	{ 0x02, 0, "stored", decompress_stored },
+	{ 0x03, 0, "packed (RLE)", decompress_packed },
 	{ 0x04, 0, "squeezed (Huffman)", NULL },
 	{ 0x05, 0, "crunched5 (static LZW)", NULL },
 	{ 0x06, 0, "crunched6 (RLE + static LZW)", NULL },
@@ -53,6 +77,12 @@ static const struct cmpr_meth_info *get_cmpr_meth_info(u8 cmpr_meth)
 	return NULL;
 }
 
+static void our_writecallback(dbuf *f, const u8 *buf, i64 buf_len)
+{
+	struct de_crcobj *crco = (struct de_crcobj*)f->userdata;
+	de_crcobj_addbuf(crco, buf, buf_len);
+}
+
 // Returns 1 if we parsed this member successfully, and it's not the
 // EOF marker.
 static int do_member(deark *c, lctx *d, i64 pos1, i64 *bytes_consumed)
@@ -63,6 +93,8 @@ static int do_member(deark *c, lctx *d, i64 pos1, i64 *bytes_consumed)
 	u8 magic;
 	u8 cmpr_meth;
 	struct member_data *md = NULL;
+	dbuf *outf = NULL;
+	de_finfo *fi = NULL;
 
 	de_dbg_indent_save(c, &saved_indent_level);
 	magic = de_getbyte_p(&pos);
@@ -95,14 +127,15 @@ static int do_member(deark *c, lctx *d, i64 pos1, i64 *bytes_consumed)
 		(md->cmi ? md->cmi->name : "?"));
 
 	dbuf_read_to_ucstring(c->infile, pos, 13, md->fn, DE_CONVFLAG_STOP_AT_NUL,
-		DE_ENCODING_CP437_G);
+		d->input_encoding);
 	de_dbg(c, "filename: \"%s\"", ucstring_getpsz_d(md->fn));
 	pos += 13;
 
 	md->cmpr_size = de_getu32le_p(&pos);
 	de_dbg(c, "cmpr size: %"I64_FMT, md->cmpr_size);
 	pos += 4; // date/time
-	pos += 2; // crc
+	md->crc_reported = (u32)de_getu16le_p(&pos);
+	de_dbg(c, "crc (reported): 0x%04x", (unsigned int)md->crc_reported);
 	if(md->cmpr_meth == 1) {
 		md->orig_size = md->cmpr_size;
 	}
@@ -111,11 +144,40 @@ static int do_member(deark *c, lctx *d, i64 pos1, i64 *bytes_consumed)
 		de_dbg(c, "orig size: %"I64_FMT, md->orig_size);
 	}
 
+	md->cmpr_data_pos = pos;
 	pos += md->cmpr_size;
+	if(pos > c->infile->len) goto done;
 	retval = 1;
+
+	if(!md->cmi || !md->cmi->decompressor) {
+		de_err(c, "%s: Compression type %u not supported", ucstring_getpsz_d(md->fn),
+			(unsigned int)md->cmpr_meth);
+		goto done;
+	}
+
+	fi = de_finfo_create(c);
+	de_finfo_set_name_from_ucstring(c, fi, md->fn, 0);
+	fi->original_filename_flag = 1;
+	outf = dbuf_create_output_file(c, NULL, fi, 0);
+
+	de_crcobj_reset(d->crco);
+	outf->writecallback_fn = our_writecallback;
+	outf->userdata = (void*)d->crco;
+
+	if(!md->cmi->decompressor(c, d, md, outf)) {
+		goto done;
+	}
+
+	md->crc_calc = de_crcobj_getval(d->crco);
+	de_dbg(c, "crc (calculated): 0x%04x", (unsigned int)md->crc_calc);
+	if(md->crc_calc != md->crc_reported) {
+		de_err(c, "%s: CRC check failed", ucstring_getpsz_d(md->fn));
+	}
 
 done:
 	*bytes_consumed = pos - pos1;
+	dbuf_close(outf);
+	de_finfo_destroy(c, fi);
 	if(md) {
 		ucstring_destroy(md->fn);
 		de_free(c, md);
@@ -130,6 +192,9 @@ static void de_run_arc(deark *c, de_module_params *mparams)
 	i64 pos = 0;
 
 	d = de_malloc(c, sizeof(lctx));
+	d->input_encoding = de_get_input_encoding(c, NULL, DE_ENCODING_CP437_G);
+	d->crco = de_crcobj_create(c, DE_CRCOBJ_CRC16_ARC);
+
 	while(1) {
 		int ret;
 		i64 bytes_consumed = 0;
@@ -141,17 +206,25 @@ static void de_run_arc(deark *c, de_module_params *mparams)
 		d->member_count++;
 	}
 
-	de_free(c, d);
+	if(d) {
+		de_crcobj_destroy(d->crco);
+		de_free(c, d);
+	}
 }
 
 // TODO: Better identification
 static int de_identify_arc(deark *c)
 {
-	static const char *exts[] = {"arc", "pak", "spk"};
+	static const char *exts[] = {"arc", "ark", "pak", "spk"};
 	int has_ext = 0;
+	int ends_with_trailer = 0;
 	size_t k;
+	u8 cmpr_meth;
 
 	if(de_getbyte(0) != 0x1a) return 0;
+	cmpr_meth = de_getbyte(1);
+	// TODO: We might be able to parse some files with cmpr_meth>9.
+	if(cmpr_meth<1 || cmpr_meth>9) return 0;
 
 	for(k=0; k<DE_ITEMS_IN_ARRAY(exts); k++) {
 		if(de_input_file_has_ext(c, exts[k])) {
@@ -159,9 +232,19 @@ static int de_identify_arc(deark *c)
 			break;
 		}
 	}
-	if(!has_ext) return 0;
 
-	return 15;
+	if(de_getu16be(c->infile->len-2) == 0x1a00) {
+		ends_with_trailer = 1;
+	}
+	else if(de_getu32be(c->infile->len-8) == 0x504baa55) {
+		// PKARC trailer, for files with comments
+		ends_with_trailer = 1;
+	}
+
+	if(has_ext && ends_with_trailer) return 90;
+	if(ends_with_trailer) return 25;
+	if(has_ext) return 15;
+	return 0;
 }
 
 void de_module_arc(deark *c, struct deark_module_info *mi)
