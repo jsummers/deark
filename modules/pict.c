3b9cfc515e1783fa024673e4ddb244dc6ad942f2
Jason Summers <jason1@pobox.com>
2019-09-10 14:20:14 -0400

    pict: Tolerate BkPixPat, PnPixPat, FillPixPat opcodes

--- modules/pict.c
+++ modules/pict.c
@@ -561,6 +561,57 @@ done:
 	return retval;
 }
 
+static int handler_pixpat(deark *c, lctx *d, i64 opcode, i64 pos1, i64 *bytes_used)
+{
+	unsigned int pattype;
+	i64 pos = pos1;
+	int needmsg = 1;
+	int retval = 0;
+	i64 colortable_size = 0;
+	i64 pixdata_size = 0;
+	struct fmtutil_macbitmap_info *bi = NULL;
+
+	pattype = (unsigned int)de_getu16be_p(&pos);
+	de_dbg(c, "PatType: %u", pattype);
+	pos += 8; // Pat1Data
+
+	if(pattype==2) { // ditherPat(?)
+		pos += 6; // RGB
+		retval = 1;
+		goto done;
+	}
+
+	bi = de_malloc(c, sizeof(struct fmtutil_macbitmap_info));
+
+	fmtutil_macbitmap_read_rowbytes_and_bounds(c, c->infile, bi, pos);
+	pos += 10;
+	fmtutil_macbitmap_read_pixmap_only_fields(c, c->infile, bi, pos);
+	pos += 36;
+
+	bi->uses_pal = 1;
+	if(!fmtutil_macbitmap_read_colortable(c, c->infile, bi, pos, &colortable_size)) goto done;
+	pos += colortable_size;
+
+	if(!get_pixdata_size(c, d, bi, pos, &pixdata_size)) {
+		goto done;
+	}
+	// Note: We could extract the "PixMap" pattern easily enough here, by calling
+	// decode_pixdata(). But if we do that, maybe we should also extract the
+	// Pat1Data data above, as well as other opcodes like BkPat.
+	pos += pixdata_size;
+	retval = 1;
+
+done:
+	if(!retval && needmsg) {
+		de_err(c, "Failed to parse PixPat data");
+	}
+	if(retval) {
+		*bytes_used = pos - pos1;
+	}
+	de_free(c, bi);
+	return retval;
+}
+
 static void do_iccprofile_item(deark *c, lctx *d, i64 pos, i64 len)
 {
 	i64 selector;
@@ -741,9 +792,9 @@ static const struct opcode_info opcode_info_arr[] = {
 	{ 0x000f, SZCODE_EXACT,   4,  "BkColor", NULL },
 	{ 0x0010, SZCODE_EXACT,   8,  "TxRatio", NULL },
 	{ 0x0011, SZCODE_EXACT,   1,  "Version", handler_11 },
-	{ 0x0012, SZCODE_SPECIAL, 0,  "BkPixPat", NULL }, // TODO
-	{ 0x0013, SZCODE_SPECIAL, 0,  "PnPixPat", NULL }, // TODO
-	{ 0x0014, SZCODE_SPECIAL, 0,  "FillPixPat", NULL }, // TODO
+	{ 0x0012, SZCODE_SPECIAL, 0,  "BkPixPat", handler_pixpat },
+	{ 0x0013, SZCODE_SPECIAL, 0,  "PnPixPat", handler_pixpat },
+	{ 0x0014, SZCODE_SPECIAL, 0,  "FillPixPat", handler_pixpat },
 	{ 0x0015, SZCODE_EXACT,   2,  "PnLocHFrac", NULL },
 	{ 0x0016, SZCODE_EXACT,   2,  "ChExtra", NULL },
 	{ 0x001a, SZCODE_EXACT,   6,  "RGBFgCol", handler_RGBColor },
