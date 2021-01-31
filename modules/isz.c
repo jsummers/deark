85f5c4bdcb8d0f32f8f7ed201abb6e120d1e0ec2
Jason Summers <jason1@pobox.com>
2021-01-31 12:08:31 -0500

    Working on is_z module

--- modules/isz.c
+++ modules/isz.c
@@ -11,6 +11,7 @@ DE_DECLARE_MODULE(de_module_is_z);
 #define ISZ_MAX_DIRS          1000 // arbitrary
 #define ISZ_MAX_FILES         5000 // arbitrary
 #define ISZ_MAX_DIR_NAME_LEN  32768 // arbitrary
+#define ISZ_SIGNATURE  0x8c655d13U
 
 struct dir_array_item {
 	de_ucstring *dname;
@@ -23,6 +24,7 @@ struct member_data {
 	UI dir_id;
 	de_ucstring *fname;
 	de_ucstring *full_fname;
+	struct de_timestamp mod_time;
 };
 
 typedef struct localctx_struct {
@@ -52,6 +54,7 @@ static void extract_file(deark *c, lctx *d, struct member_data *md)
 	fi = de_finfo_create(c);
 	de_finfo_set_name_from_ucstring(c, fi, md->full_fname, DE_SNFLAG_FULLPATH);
 	fi->original_filename_flag = 1;
+	fi->timestamp[DE_TIMESTAMPIDX_MODIFY] = md->mod_time;
 
 	outf = dbuf_create_output_file(c, NULL, fi, 0);
 
@@ -75,6 +78,20 @@ done:
 	de_finfo_destroy(c, fi);
 }
 
+static void read_timestamp(deark *c, lctx *d, struct de_timestamp *ts,
+	i64 pos, const char *name)
+{
+	i64 mod_time_raw, mod_date_raw;
+	char timestamp_buf[64];
+
+	mod_date_raw = de_getu16le(pos);
+	mod_time_raw = de_getu16le(pos+2);
+	de_dos_datetime_to_timestamp(ts, mod_date_raw, mod_time_raw);
+	ts->tzcode = DE_TZCODE_LOCAL;
+	de_timestamp_to_string(ts, timestamp_buf, sizeof(timestamp_buf), 0);
+	de_dbg(c, "%s: %s", name, timestamp_buf);
+}
+
 static int do_one_file(deark *c, lctx *d, i64 pos1, i64 *pbytes_consumed)
 {
 	i64 pos = pos1;
@@ -86,7 +103,6 @@ static int do_one_file(deark *c, lctx *d, i64 pos1, i64 *pbytes_consumed)
 	int saved_indent_level;
 
 	de_dbg_indent_save(c, &saved_indent_level);
-
 	md = de_malloc(c, sizeof(struct member_data));
 	de_dbg(c, "file entry at %"I64_FMT, pos1);
 	de_dbg_indent(c, 1);
@@ -108,8 +124,10 @@ static int do_one_file(deark *c, lctx *d, i64 pos1, i64 *pbytes_consumed)
 	md->cmpr_data_pos = de_getu32le_p(&pos); // 11
 	de_dbg(c, "cmpr data pos: %"I64_FMT, md->cmpr_data_pos);
 
-	pos += 4; // datetime
-	pos += 4; // ?
+	read_timestamp(c, d, &md->mod_time, pos, "mod time");
+	pos += 4;
+
+	pos += 4; // ? (maybe a bit-field?)
 
 	segment_size = de_getu16le_p(&pos);
 	de_dbg(c, "segment size: %"I64_FMT, segment_size);
@@ -145,7 +163,7 @@ done:
 	return retval;
 }
 
-static int do_filelist(deark *c, lctx *d)
+static void do_filelist(deark *c, lctx *d)
 {
 	i64 pos = d->filelist_pos;
 	i64 i;
@@ -159,7 +177,7 @@ static int do_filelist(deark *c, lctx *d)
 	}
 
 done:
-	return 1;
+	;
 }
 
 // Convert backslashes to slashes, and append a slash if path is not empty.
@@ -245,22 +263,28 @@ static void de_run_is_z(deark *c, de_module_params *mparams)
 {
 	lctx *d = NULL;
 	int saved_indent_level;
+	struct de_timestamp tmp_timestamp;
 
 	de_dbg_indent_save(c, &saved_indent_level);
-
 	d = de_malloc(c, sizeof(lctx));
 	d->input_encoding = de_get_input_encoding(c, NULL, DE_ENCODING_WINDOWS1252);
 
 	// 0   13 5D 65 8C   = signature
 	// 4   3A 01 02 00   = ?
-	// 5...
+	// 8... ?
 	// 12  ui16  number of files
-	// 14  byte[6] ?
-	// ...
+	// 14  ui16  date
+	// 16  ui16  time
+	// 18... ?
 	// 41  ui32  offset of directory sequence
-	// 45...
+	// 45... ?
 	// 49  ui16  number of dirs
 	// 51  ui32  offset of files sequence
+
+	if(de_getu32le(0)!=ISZ_SIGNATURE) {
+		de_err(c, "Not an InstallShield Z file");
+		goto done;
+	}
 
 	de_dbg(c, "main header");
 	de_dbg_indent(c, 1);
@@ -269,11 +293,13 @@ static void de_run_is_z(deark *c, de_module_params *mparams)
 	de_dbg(c, "total number of files: %"I64_FMT, d->num_files_total);
 	if(d->num_files_total>ISZ_MAX_FILES) goto done;
 
+	read_timestamp(c, d, &tmp_timestamp, 14, "timestamp");
+
 	d->directory_pos = de_getu32le(41);
-	de_dbg(c, "start of directory entries: %"I64_FMT, d->directory_pos);
+	de_dbg(c, "start of dir entries: %"I64_FMT, d->directory_pos);
 
 	d->num_dirs = de_getu16le(49);
-	de_dbg(c, "num dirs: %"I64_FMT, d->num_dirs);
+	de_dbg(c, "number of dirs: %"I64_FMT, d->num_dirs);
 	if(d->num_dirs>ISZ_MAX_DIRS) goto done;
 
 	d->filelist_pos = de_getu32le(51);
@@ -284,7 +310,6 @@ static void de_run_is_z(deark *c, de_module_params *mparams)
 	d->dir_array = de_mallocarray(c, d->num_dirs, sizeof(struct dir_array_item));
 
 	do_dirlist(c, d);
-
 	do_filelist(c, d);
 
 done:
@@ -302,7 +327,7 @@ done:
 
 static int de_identify_is_z(deark *c)
 {
-	if(!dbuf_memcmp(c->infile, 0, "\x13\x5d\x65\x8c", 4))
+	if(de_getu32le(0)==ISZ_SIGNATURE)
 		return 100;
 	return 0;
 }
@@ -313,5 +338,4 @@ void de_module_is_z(deark *c, struct deark_module_info *mi)
 	mi->desc = "InstallShield Z";
 	mi->run_fn = de_run_is_z;
 	mi->identify_fn = de_identify_is_z;
-	mi->flags |= DE_MODFLAG_NONWORKING;
 }
