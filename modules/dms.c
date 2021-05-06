jsummers/deark

commit aebe90a69f101554ef5e41302ce3235b64cfeed2

Minor refactoring, related to lzah

modules/dms.c
@@ -610,21 +610,25 @@ static void do_mediumlz77_internal(struct medium_ctx *mctx, struct dmsmedium_cmp
 			UI tmp_code;
 			UI length;
 			UI offset_rel;
+			UI d_code1, d_len1;
+			UI d_code2, d_len2;
 
 			// TODO: This seems overly complicated. Is there a simpler way to
 			// implement this?
 
 			first_code = (UI)de_bitreader_getbits(&mctx->bitrd, 8);
-			length = (UI)fmtutil_get_lzhuf_d_code(first_code) + 3;
+			fmtutil_get_lzhuf_d_code_and_len(first_code, &d_code1, &d_len1);
+			length = d_code1 + 3;
 
-			ocode1_nbits = (UI)fmtutil_get_lzhuf_d_len(first_code);
+			ocode1_nbits = d_len1;
 			ocode1 = (UI)de_bitreader_getbits(&mctx->bitrd, ocode1_nbits);
 
 			tmp_code = ((first_code << ocode1_nbits) | ocode1) & 0xff;
-			ocode2_nbits = (UI)fmtutil_get_lzhuf_d_len(tmp_code);
+			fmtutil_get_lzhuf_d_code_and_len(first_code, &d_code2, &d_len2);
+			ocode2_nbits = d_len2;
 			ocode2 = (UI)de_bitreader_getbits(&mctx->bitrd, ocode2_nbits);
 
-			offset_rel = ((UI)fmtutil_get_lzhuf_d_code(tmp_code) << 8) | (((tmp_code << ocode2_nbits) | ocode2) & 0xff);
+			offset_rel = (d_code2 << 8) | (((tmp_code << ocode2_nbits) | ocode2) & 0xff);
 			de_lz77buffer_copy_from_hist(mdst->ringbuf, mdst->ringbuf->curpos - 1 - offset_rel, length);
 		}
 	}

foreign/lzhuf.h
@@ -36,6 +36,7 @@ struct lzahuf_ctx {
 	UI lzhuf_T;
 	UI lzhuf_R; /* position of root */ /* (LZHUF_T - 1) */
 	UI num_special_codes;
+	u8 crlzh20_style_positions;
 
 	int errflag;
 	i64 nbytes_written;
@@ -268,17 +269,17 @@ static UI lzhuf_DecodeChar(struct lzahuf_ctx *cctx)
 static UI lzhuf_DecodePosition(struct lzahuf_ctx *cctx)
 {
 	UI i, j, c;
+	UI d_code, d_len;
 
 	/* recover upper bits from table */
 	i = (UI)de_bitreader_getbits(&cctx->bitrd, 8);
-	c = (UI)fmtutil_get_lzhuf_d_code(i);
-	c <<= (cctx->lh1p.is_crlzh20 ? 5 : 6);
-	j = fmtutil_get_lzhuf_d_len(i);
+	fmtutil_get_lzhuf_d_code_and_len(i, &d_code, &d_len);
+	c = d_code << (cctx->crlzh20_style_positions ? 5 : 6);
 
 	/* read lower bits verbatim */
-	j -= (cctx->lh1p.is_crlzh20 ? 3 : 2);
+	j = d_len - (cctx->crlzh20_style_positions ? 3 : 2);
 	i = (i<<j) | (UI)de_bitreader_getbits(&cctx->bitrd, j);
-	i &= (cctx->lh1p.is_crlzh20 ? 0x1f : 0x3f);
+	i &= (cctx->crlzh20_style_positions ? 0x1f : 0x3f);
 	if(cctx->bitrd.eof_flag) {
 		cctx->errflag = 1;
 	}
@@ -368,6 +369,9 @@ static void lzhuf_Decode(struct lzahuf_ctx *cctx)  /* recover */
 		cctx->num_length_codes = 58;
 		cctx->match_length_bias = 254;
 		rb_size = 2048;
+		if(cctx->lh1p.is_crlzh20) {
+			cctx->crlzh20_style_positions = 1;
+		}
 	}
 	else if(cctx->lh1p.is_arc_trimmed) {
 		// codes 0-255 = literals 0x00-0xff

src/deark-fmtutil.h
@@ -270,8 +270,7 @@ void fmtutil_dclimplode_codectype1(deark *c, struct de_dfilter_in_params *dcmpri
 	struct de_dfilter_out_params *dcmpro, struct de_dfilter_results *dres,
 	void *codec_private_params);
 
-UI fmtutil_get_lzhuf_d_code(UI n);
-UI fmtutil_get_lzhuf_d_len(UI n);
+void fmtutil_get_lzhuf_d_code_and_len(UI n, UI *pd_code, UI *pd_len);
 
 struct de_lh1_params {
 	u8 is_crlzh11, is_crlzh20;

src/fmtutil-lzah.c
@@ -10,23 +10,14 @@
 
 #include "../foreign/lzhuf.h"
 
-UI fmtutil_get_lzhuf_d_code(UI n)
+void fmtutil_get_lzhuf_d_code_and_len(UI n, UI *pd_code, UI *pd_len)
 {
-	if(n<32 || n>=256) return 0;
-	if(n<80) return (n-16)>>4;
-	if(n<144) return (n-48)>>3;
-	if(n<192) return (n-96)>>2;
-	if(n<240) return (n-144)>>1;
-	return n-192;
-}
-
-UI fmtutil_get_lzhuf_d_len(UI n)
-{
-	static const u8 d_len[16] = {
-		3,3,4,4,4,5,5,5,5,6,6,6,7,7,7,8
-	};
-
-	return (UI)d_len[(n&0xff)/16];
+	if(n<32 || n>=256) { *pd_code = 0; *pd_len = 3; }
+	else if(n<80) { *pd_code = (n-16)>>4; *pd_len = 4; }
+	else if(n<144) { *pd_code = (n-48)>>3; *pd_len = 5; }
+	else if(n<192) { *pd_code = (n-96)>>2; *pd_len = 6; }
+	else if(n<240) { *pd_code = (n-144)>>1; *pd_len = 7; }
+	else { *pd_code = n-192; *pd_len = 8; };
 }
 
 // codec_private_params: 'struct de_lh1_params'. Can be NULL.

commit 79946133c512de94f6adeceba0ee269dbe9a28fb

dms: Fixed "medium" decompression
Broken by aebe90a69f10.

modules/dms.c
@@ -624,7 +624,7 @@ static void do_mediumlz77_internal(struct medium_ctx *mctx, struct dmsmedium_cmp
 			ocode1 = (UI)de_bitreader_getbits(&mctx->bitrd, ocode1_nbits);
 
 			tmp_code = ((first_code << ocode1_nbits) | ocode1) & 0xff;
-			fmtutil_get_lzhuf_d_code_and_len(first_code, &d_code2, &d_len2);
+			fmtutil_get_lzhuf_d_code_and_len(tmp_code, &d_code2, &d_len2);
 			ocode2_nbits = d_len2;
 			ocode2 = (UI)de_bitreader_getbits(&mctx->bitrd, ocode2_nbits);
 