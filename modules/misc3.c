2d4110f844ad03ef7d269cc9a4db473b5028bffd
Jason Summers <jason1@pobox.com>
2021-10-31 13:00:11 -0400

    dwc: Various improvements

--- modules/misc3.c
+++ modules/misc3.c
@@ -28,6 +28,7 @@ struct member_data {
 
 	// Private use fields for the format decoder:
 	UI cmpr_meth;
+	UI file_flags;
 
 	// The extract_member_file() will temporarily set dcmpri/dcmpro/dres,
 	// and call ->dfn() if it is set.
@@ -114,7 +115,7 @@ static void read_field_cmpr_len_p(struct member_data *md, i64 *ppos)
 // tstype:
 //   1 = Unix
 //   2 = DOS,date first
-static void read_field_dos_dttm_mod_p(struct member_data *md,
+static void read_field_dttm_mod_p(lctx *d,
 	struct de_timestamp *ts, const char *name,
 	int tstype, i64 *ppos)
 {
@@ -122,16 +123,17 @@ static void read_field_dos_dttm_mod_p(struct member_data *md,
 	char timestamp_buf[64];
 	int is_set = 0;
 
+	ts->is_valid = 0;
 	if(tstype==1) {
-		n1 = dbuf_getu32x(md->c->infile, *ppos, md->d->is_le);
+		n1 = dbuf_getu32x(d->c->infile, *ppos, d->is_le);
 		de_unix_time_to_timestamp(n1, ts, 0x1);
 		is_set = 1;
 	}
 	else if(tstype==2) {
 		i64 dosdt, dostm;
 
-		n1 = dbuf_getu16x(md->c->infile, *ppos, md->d->is_le);
-		n2 = dbuf_getu16x(md->c->infile, *ppos+2, md->d->is_le);
+		n1 = dbuf_getu16x(d->c->infile, *ppos, d->is_le);
+		n2 = dbuf_getu16x(d->c->infile, *ppos+2, d->is_le);
 		dosdt = n1;
 		dostm = n2;
 
@@ -148,7 +150,7 @@ static void read_field_dos_dttm_mod_p(struct member_data *md,
 	else {
 		de_snprintf(timestamp_buf, sizeof(timestamp_buf), "[not set]");
 	}
-	de_dbg(md->c, "%s time: %s", name, timestamp_buf);
+	de_dbg(d->c, "%s time: %s", name, timestamp_buf);
 
 	*ppos += 4;
 }
@@ -244,8 +246,8 @@ static void cpshrink_decompressor_fn(struct member_data *md)
 static void cpshrink_do_member(deark *c, lctx *d, struct member_data *md)
 {
 	i64 pos = md->member_hdr_pos;
-	UI udata_crc_reported;
-	UI udata_crc_calc;
+	UI cdata_crc_reported;
+	UI cdata_crc_calc;
 
 	int saved_indent_level;
 
@@ -256,8 +258,8 @@ static void cpshrink_do_member(deark *c, lctx *d, struct member_data *md)
 		(UI)md->member_idx, md->member_hdr_pos, md->cmpr_pos);
 	de_dbg_indent(c, 1);
 
-	udata_crc_reported = (u32)de_getu32le_p(&pos);
-	de_dbg(c, "CRC of unc. data (reported): 0x%08x", (UI)udata_crc_reported);
+	cdata_crc_reported = (u32)de_getu32le_p(&pos);
+	de_dbg(c, "CRC of cmpr. data (reported): 0x%08x", (UI)cdata_crc_reported);
 
 	dbuf_read_to_ucstring(c->infile, pos, 15, md->filename, DE_CONVFLAG_STOP_AT_NUL,
 		d->input_encoding);
@@ -271,7 +273,7 @@ static void cpshrink_do_member(deark *c, lctx *d, struct member_data *md)
 	read_field_cmpr_len_p(md, &pos);
 	d->cmpr_data_curpos += md->cmpr_len;
 
-	read_field_dos_dttm_mod_p(md, &md->tmstamp[DE_TIMESTAMPIDX_MODIFY], "mod", 2, &pos);
+	read_field_dttm_mod_p(d, &md->tmstamp[DE_TIMESTAMPIDX_MODIFY], "mod", 2, &pos);
 
 	if(!good_cmpr_data_pos(md)) {
 		d->errflag = 1;
@@ -280,11 +282,12 @@ static void cpshrink_do_member(deark *c, lctx *d, struct member_data *md)
 
 	de_crcobj_reset(d->crco);
 	de_crcobj_addslice(d->crco, c->infile, md->cmpr_pos, md->cmpr_len);
-	udata_crc_calc = de_crcobj_getval(d->crco);
-	if(udata_crc_calc!=udata_crc_reported) {
+	cdata_crc_calc = de_crcobj_getval(d->crco);
+	de_dbg(c, "CRC of cmpr. data (calculated): 0x%08x", (UI)cdata_crc_calc);
+	if(cdata_crc_calc!=cdata_crc_reported) {
 		de_err(c, "File data CRC check failed (expected 0x%08x, got 0x%08x). "
-			"CPZ file may be corrupted.", (UI)udata_crc_reported,
-			(UI)udata_crc_calc);
+			"CPZ file may be corrupted.", (UI)cdata_crc_reported,
+			(UI)cdata_crc_calc);
 	}
 
 	md->dfn = cpshrink_decompressor_fn;
@@ -385,10 +388,16 @@ void de_module_cpshrink(deark *c, struct deark_module_info *mi)
 // DWC archive
 // **************************************************************************
 
-static void do_dwc_member(deark *c, lctx *d, i64 pos1)
+static void do_dwc_member(deark *c, lctx *d, i64 pos1, i64 fhsize)
 {
 	i64 pos = pos1;
 	struct member_data *md = NULL;
+	i64 cmt_len = 0;
+	i64 path_len = 0;
+	UI cdata_crc_reported = 0;
+	UI cdata_crc_calc;
+	u8 have_cdata_crc = 0;
+	u8 b;
 
 	md = create_md(c, d);
 
@@ -400,63 +409,148 @@ static void do_dwc_member(deark *c, lctx *d, i64 pos1)
 	pos += 13;
 
 	read_field_orig_len_p(md, &pos);
-	read_field_dos_dttm_mod_p(md, &md->tmstamp[DE_TIMESTAMPIDX_MODIFY], "mod", 1, &pos);
+	read_field_dttm_mod_p(d, &md->tmstamp[DE_TIMESTAMPIDX_MODIFY], "mod", 1, &pos);
 	read_field_cmpr_len_p(md, &pos);
 	md->cmpr_pos = de_getu32le_p(&pos);
 	de_dbg(c, "cmpr. data pos: %"I64_FMT, md->cmpr_pos);
-	md->cmpr_meth = (UI)de_getbyte_p(&pos);
+
+	b = de_getbyte_p(&pos);
+	md->cmpr_meth = ((UI)b) & 0x0f;
+	md->file_flags = ((UI)b) >> 4;
 	de_dbg(c, "cmpr. method: %u", md->cmpr_meth);
-	// pos += 2; // ?
-	// pos += 2; // CRC
+	de_dbg(c, "flags: 0x%x", md->file_flags);
+
+	if(fhsize>=31) {
+		cmt_len = (i64)de_getbyte_p(&pos);
+		de_dbg(c, "comment len: %d", (int)cmt_len);
+	}
+	if(fhsize>=32) {
+		path_len = (i64)de_getbyte_p(&pos);
+		de_dbg(c, "path len: %d", (int)path_len);
+	}
+	if(fhsize>=34) {
+		cdata_crc_reported = (u32)de_getu16le_p(&pos);
+		de_dbg(c, "CRC of cmpr. data (reported): 0x%04x", (UI)cdata_crc_reported);
+		have_cdata_crc = 1;
+	}
+
+	if(!good_cmpr_data_pos(md)) {
+		d->errflag = 1;
+		goto done;
+	}
+
+	if(have_cdata_crc) {
+		if(!d->crco) {
+			d->crco = de_crcobj_create(c, DE_CRCOBJ_CRC16_ARC);
+		}
+		de_crcobj_reset(d->crco);
+		de_crcobj_addslice(d->crco, c->infile, md->cmpr_pos, md->cmpr_len);
+		cdata_crc_calc = de_crcobj_getval(d->crco);
+		de_dbg(c, "CRC of cmpr. data (calculated): 0x%04x", (UI)cdata_crc_calc);
+		if(cdata_crc_calc!=cdata_crc_reported) {
+			de_err(c, "File data CRC check failed (expected 0x%04x, got 0x%04x). "
+				"DWC file may be corrupted.", (UI)cdata_crc_reported,
+				(UI)cdata_crc_calc);
+		}
+	}
+
+done:
 	de_dbg_indent(c, -1);
 	destroy_md(c, md);
+}
+
+static int has_dwc_sig(deark *c)
+{
+	return !dbuf_memcmp(c->infile, c->infile->len-3, (const u8*)"DWC", 3);
 }
 
 static void de_run_dwc(deark *c, de_module_params *mparams)
 {
 	lctx *d = NULL;
 	i64 trailer_pos;
+	i64 trailer_len;
 	i64 nmembers;
 	i64 fhsize; // size of each file header
 	i64 pos;
 	i64 i;
+	struct de_timestamp tmpts;
+	int need_errmsg = 0;
+	int saved_indent_level;
 
+	de_dbg_indent_save(c, &saved_indent_level);
 	de_info(c, "Note: DWC files can be parsed, but no files can be extracted from them.");
 	d = create_lctx(c);
 	d->is_le = 1;
 	d->input_encoding = de_get_input_encoding(c, NULL, DE_ENCODING_CP437);
 
-	trailer_pos = c->infile->len - 27;
-	pos = trailer_pos;
-	de_dbg(c, "trailer at %"I64_FMT, trailer_pos);
+	de_dbg(c, "trailer");
 	de_dbg_indent(c, 1);
-	pos += 2; // trailer length (27)
-	fhsize = de_getu16le_p(&pos);
-	pos += 16;
+
+	if(!has_dwc_sig(c)) {
+		de_err(c, "Not a DWC file");
+		goto done;
+	}
+
+	pos = c->infile->len - 27; // Position of the "trailer size" field
+	trailer_len = de_getu16le_p(&pos); // Usually 27
+	trailer_pos = c->infile->len - trailer_len;
+	de_dbg(c, "size: %"I64_FMT" (starts at %"I64_FMT")", trailer_len, trailer_pos);
+	if(trailer_len<27 || trailer_pos<0) {
+		need_errmsg = 1;
+		goto done;
+	}
+
+	fhsize = (i64)de_getbyte_p(&pos);
+	de_dbg(c, "file header entry size: %d", (int)fhsize);
+	if(fhsize<30) {
+		need_errmsg = 1;
+		goto done;
+	}
+
+	pos += 13; // TODO?: name of header file ("h" command)
+	read_field_dttm_mod_p(d, &tmpts, "archive last-modified", 1, &pos);
+
 	nmembers = de_getu16le_p(&pos);
 	de_dbg(c, "number of member files: %d", (int)nmembers);
 	de_dbg_indent(c, -1);
 
 	pos = trailer_pos - fhsize*nmembers;
+	if(pos<0) {
+		need_errmsg = 1;
+		goto done;
+	}
 	for(i=0; i<nmembers; i++) {
-		if(pos<0 || pos>(trailer_pos-fhsize)) break;
-		do_dwc_member(c, d, pos);
+		do_dwc_member(c, d, pos, fhsize);
+		if(d->errflag) goto done;
 		pos += fhsize;
 	}
 
+done:
+	if(need_errmsg) {
+		de_err(c, "Bad DWC file");
+	}
 	destroy_lctx(c, d);
+	de_dbg_indent_restore(c, saved_indent_level);
 }
 
 static int de_identify_dwc(deark *c)
 {
-	if(!de_input_file_has_ext(c, "dwc")) return 0;
-	if(dbuf_memcmp(c->infile, c->infile->len-3, (const u8*)"DWC", 3)) {
-		return 0;
+	i64 tsize;
+	int has_ext;
+	u8 dsize;
+
+	if(!has_dwc_sig(c)) return 0;
+	tsize = de_getu16le(c->infile->len-27);
+	if(tsize<27 || tsize>c->infile->len) return 0;
+	dsize = de_getbyte(c->infile->len-25);
+	if(dsize<30) return 0;
+	has_ext = de_input_file_has_ext(c, "dwc");
+	if(tsize==27 && dsize==34) {
+		if(has_ext) return 100;
+		return 60;
 	}
-	if(de_getu16le(c->infile->len-27) != 27) {
-		return 0;
-	}
-	return 70;
+	if(has_ext) return 10;
+	return 0;
 }
 
 void de_module_dwc(deark *c, struct deark_module_info *mi)
