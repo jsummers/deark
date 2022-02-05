405a4f9c0ef2b0c3fe509234635c56dc87520a60
Jason Summers <jason1@pobox.com>
2022-02-05 15:58:02 -0500

    New "storyboard" module

--- deps.mk
+++ deps.mk
@@ -254,6 +254,8 @@ $(OBJDIR)/modules/sis.o: modules/sis.c src/deark-config.h \
  src/deark-private.h src/deark.h src/deark-fmtutil.h
 $(OBJDIR)/modules/spectrum512.o: modules/spectrum512.c src/deark-config.h \
  src/deark-private.h src/deark.h src/deark-fmtutil.h
+$(OBJDIR)/modules/storyboard.o: modules/storyboard.c src/deark-private.h \
+ src/deark.h src/deark-config.h
 $(OBJDIR)/modules/stuffit.o: modules/stuffit.c src/deark-private.h \
  src/deark.h src/deark-config.h src/deark-fmtutil.h
 $(OBJDIR)/modules/sunras.o: modules/sunras.c src/deark-config.h \
