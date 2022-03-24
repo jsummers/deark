45bb565935025e39889e50f7b55f8496de79ad14
Jason Summers <jason1@pobox.com>
2022-03-24 08:41:39 -0400

    cdi_image: Partial support for RL7 decompression

--- modules/misc2.c
+++ modules/misc2.c
@@ -1950,25 +1950,129 @@ static void do_cdi_imag_PLTE(deark *c, struct cdi_imag_ctx *d, struct de_iffctx 
 		&d->pal[offset], 256-offset, 0);
 }
 
+static int cdi_imag_decompress_rl7(deark *c, struct cdi_imag_ctx *d, struct de_iffctx *ictx,
+	dbuf *unc_pixels)
+{
+	i64 pos, endpos;
+	i64 xpos = 0;
+	i64 ypos = 0;
+	int need_errmsg = 0;
+
+	pos = ictx->chunkctx->dpos;
+	endpos = pos + ictx->chunkctx->dlen;
+
+	while(1) {
+		u8 x;
+		u8 palent;
+		u8 eoln_flag = 0;
+		i64 count;
+
+		if(ypos >= d->h) break;
+
+		if(pos >= endpos) {
+			need_errmsg = 1;
+			goto done;
+		}
+
+		x = dbuf_getbyte_p(ictx->f, &pos);
+		palent = x & 0x7f;
+		if(x & 0x80) { // run
+			u8 r;
+
+			r = dbuf_getbyte_p(ictx->f, &pos);
+			if(r>=2) {
+				count = (i64)r;
+			}
+			else if(r==0) {
+				eoln_flag = 1;
+				count = d->npwidth - xpos;
+			}
+			else {
+				de_err(c, "Unsupported compression feature");
+				goto done;
+			}
+		}
+		else { // single pixel
+			count = 1;
+		}
+
+		if(xpos+count > d->npwidth) {
+			need_errmsg = 1;
+			count = d->npwidth - xpos;
+		}
+		dbuf_write_run(unc_pixels, palent, count);
+		xpos += count;
+
+		if(eoln_flag) {
+			ypos++;
+			xpos = 0;
+		}
+	}
+
+done:
+	dbuf_flush(unc_pixels);
+	if(need_errmsg) {
+		de_err(c, "Image decompression failed");
+	}
+	return (unc_pixels->len>0);
+}
+
+static int do_cdi_imag_model8(deark *c, struct cdi_imag_ctx *d, struct de_iffctx *ictx,
+	de_bitmap *img)
+{
+	int retval = 0;
+	dbuf *unc_pixels = NULL;
+
+	if(d->depth!=8) return 0;
+
+	unc_pixels = dbuf_create_membuf(c, d->npwidth*d->h, 0x1);
+	dbuf_enable_wbuffer(unc_pixels);
+	if(!cdi_imag_decompress_rl7(c, d, ictx, unc_pixels)) goto done;
+	// Note: There are ways to change the palette partway through the image,
+	// so if we were to fully support this model, we could not use
+	// de_convert_image_paletted().
+	de_convert_image_paletted(unc_pixels, 0, (i64)d->depth,
+		d->npwidth, d->pal, img, 0);
+	retval = 1;
+
+done:
+	dbuf_close(unc_pixels);
+	return retval;
+}
+
 static void do_cdi_imag_IDAT(deark *c, struct cdi_imag_ctx *d, struct de_iffctx *ictx)
 {
 	de_bitmap *img = NULL;
+	int ret = 0;
 
 	if(!d->found_IHDR) goto done;
-	if(d->model!=6) {
-		de_err(c, "Unsupported image type: model=%u", d->model);
-		goto done;
+
+	if((d->model==6 && d->depth==4) ||
+		(d->model==8 && d->depth==8))
+	{
+		;
 	}
-	if(d->depth!=4) {
-		de_err(c, "Unsupported bits/pixel: %u", d->depth);
+	else {
+		de_err(c, "Unsupported image type: model=%u, depth=%u", d->model, d->depth);
 		goto done;
 	}
 
 	if(!de_good_image_dimensions(c, d->npwidth, d->h)) goto done;
+
 	img = de_bitmap_create2(c, d->npwidth, d->pdwidth, d->h, 3);
-	de_convert_image_paletted(ictx->f, ictx->chunkctx->dpos, (i64)d->depth,
-		d->rowspan, d->pal, img, 0);
-	de_bitmap_write_to_file(img, NULL, 0);
+
+	if(d->model==6) {
+		de_convert_image_paletted(ictx->f, ictx->chunkctx->dpos, (i64)d->depth,
+			d->rowspan, d->pal, img, 0);
+		ret = 1;
+	}
+	else if(d->model==8) {
+		ret = do_cdi_imag_model8(c, d, ictx, img);
+	}
+
+	if(ret) {
+		de_bitmap_write_to_file(img, NULL, 0);
+	}
 
 done:
 	de_bitmap_destroy(img);
