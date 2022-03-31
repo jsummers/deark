jsummers/deark

commit 1d406c8359804b5fc580da43640da923296bf99f

Started work on new "-padpix" option

modules/bmp.c
@@ -35,7 +35,7 @@ typedef struct localctx_struct {
 	i64 bitcount;
 	u32 compression_field;
 	i64 size_image; // biSizeImage
-	i64 width, height;
+	i64 width, pdwidth, height;
 	int top_down;
 	i64 pal_entries; // Actual number stored in file. 0 means no palette.
 	i64 pal_pos;
@@ -230,6 +230,7 @@ static int read_infoheader(deark *c, lctx *d, i64 pos)
 		}
 		nplanes = de_getu16le(pos+12);
 	}
+	d->pdwidth = d->width; // Default "padded width"
 	de_dbg_dimensions(c, d->width, d->height);
 	if(!de_good_image_dimensions(c, d->width, d->height)) {
 		goto done;
@@ -509,7 +510,7 @@ static de_bitmap *bmp_bitmap_create(deark *c, lctx *d, int bypp)
 {
 	de_bitmap *img;
 
-	img = de_bitmap_create(c, d->width, d->height, bypp);
+	img = de_bitmap_create2(c, d->width, d->pdwidth, d->height, bypp);
 	img->flipped = !d->top_down;
 	return img;
 }
@@ -533,9 +534,24 @@ static void do_image_24bit(deark *c, lctx *d, dbuf *bits, i64 bits_offset)
 
 	img = bmp_bitmap_create(c, d, 3);
 	for(j=0; j<d->height; j++) {
-		for(i=0; i<d->width; i++) {
-			clr = dbuf_getRGB(bits, bits_offset + j*d->rowspan + 3*i, DE_GETRGBFLAG_BGR);
+		i64 rowpos = bits_offset + j*d->rowspan;
+		i64 pos_in_this_row = 0;
+		u8 cbuf[3];
+
+		for(i=0; i<d->pdwidth; i++) {
+			dbuf_read(bits, cbuf, rowpos + pos_in_this_row, 3);
+			if(pos_in_this_row+3 > d->rowspan) {
+				// If -padpix was used, a partial pixel at the end of the row is
+				// possible. Happens when width == 1 or 2 (mod 4).
+				// To handle that, zero out the byte(s) that we shouldn't have read.
+				cbuf[2] = 0;
+				if(pos_in_this_row+2 > d->rowspan) {
+					cbuf[1] = 0;
+				}
+			}
+			clr = DE_MAKE_RGB(cbuf[2], cbuf[1], cbuf[0]);
 			de_bitmap_setpixel_rgb(img, i, j, clr);
+			pos_in_this_row += 3;
 		}
 	}
 	de_bitmap_write_to_file_finfo(img, d->fi, 0);
@@ -563,7 +579,7 @@ static void do_image_16_32bit(deark *c, lctx *d, dbuf *bits, i64 bits_offset)
 
 	img = bmp_bitmap_create(c, d, has_transparency?4:3);
 	for(j=0; j<d->height; j++) {
-		for(i=0; i<d->width; i++) {
+		for(i=0; i<d->pdwidth; i++) {
 			if(d->bitcount==16) {
 				v = (u32)dbuf_getu16le(bits, bits_offset + j*d->rowspan + 2*i);
 			}
@@ -621,7 +637,7 @@ static void do_image_rle_4_8_24(deark *c, lctx *d, dbuf *bits, i64 bits_offset)
 
 		// Stop if we reach the end of the output image.
 		if(ypos>=d->height) break;
-		if(ypos==(d->height-1) && xpos>=d->width) break;
+		if(ypos==(d->height-1) && xpos>=d->pdwidth) break;
 
 		// Read the next two bytes from the input file.
 		b1 = dbuf_getbyte(bits, pos++);
@@ -738,6 +754,17 @@ static void do_image(deark *c, lctx *d)
 	}
 
 	d->rowspan = ((d->bitcount*d->width +31)/32)*4;
+	if(d->compression_type==CMPR_NONE) {
+		if(c->padpix && d->bitcount==24) {
+			// The 24-bit decoder can handle partial pixels.
+			d->pdwidth = (d->rowspan+2)/3;
+		}
+		else {
+			// By default, ignore a partial-pixel's worth of padding.
+			// bits-per-row / bits-per-pixel
+			d->pdwidth = (d->rowspan*8) / d->bitcount;
+		}
+	}
 
 	if(d->bitcount>=1 && d->bitcount<=8 && d->compression_type==CMPR_NONE) {
 		do_image_paletted(c, d, c->infile, d->bits_offset);

modules/misc.c
@@ -570,7 +570,7 @@ static void de_run_hpicn(deark *c, de_module_params *mparams)
 
 	width = de_getu16le(4);
 	height = de_getu16le(6);
-	de_convert_and_write_image_bilevel(c->infile, 8, width, height, (width+7)/8,
+	de_convert_and_write_image_bilevel2(c->infile, 8, width, height, (width+7)/8,
 		DE_CVTF_WHITEISZERO, NULL, 0);
 }
 
@@ -1269,7 +1269,7 @@ static void de_run_vbm(deark *c, de_module_params *mparams)
 	}
 	width = de_getu16be(4);
 	height = de_getu16be(6);
-	de_convert_and_write_image_bilevel(c->infile, 8, width, height, (width+7)/8,
+	de_convert_and_write_image_bilevel2(c->infile, 8, width, height, (width+7)/8,
 		DE_CVTF_WHITEISZERO, NULL, 0);
 }
 
@@ -1304,7 +1304,7 @@ static void de_run_fp_art(deark *c, de_module_params *mparams)
 	width = de_getu16le(2);
 	height = de_getu16le(6);
 	rowspan = ((width+15)/16)*2;
-	de_convert_and_write_image_bilevel(c->infile, 8, width, height, rowspan, 0, NULL, 0);
+	de_convert_and_write_image_bilevel2(c->infile, 8, width, height, rowspan, 0, NULL, 0);
 }
 
 static int de_identify_fp_art(deark *c)
@@ -1647,7 +1647,7 @@ static void de_run_crg(deark *c, de_module_params *mparams)
 	}
 	de_dbg(c, "decompressed to %d bytes", (int)unc_pixels->len);
 
-	de_convert_and_write_image_bilevel(unc_pixels, 0, width, height, rowspan,
+	de_convert_and_write_image_bilevel2(unc_pixels, 0, width, height, rowspan,
 		DE_CVTF_WHITEISZERO, NULL, 0);
 
 done:
@@ -2242,7 +2242,7 @@ static void de_run_cdr_wl(deark *c, de_module_params *mparams)
 	pos += 4; // bmBits
 
 	if(!de_good_image_dimensions(c, w, h)) goto done;
-	img = de_bitmap_create(c, w, h, 1);
+	img = de_bitmap_create2(c, w, rowspan*8, h, 1);
 	de_convert_image_bilevel(c->infile, pos, rowspan, img, 0);
 	de_bitmap_write_to_file(img, "preview", DE_CREATEFLAG_IS_AUX);
 

src/deark-bitmap.c
@@ -380,6 +380,22 @@ de_bitmap *de_bitmap_create(deark *c, i64 width, i64 height, int bypp)
 	return img;
 }
 
+de_bitmap *de_bitmap_create2(deark *c, i64 npwidth, i64 pdwidth, i64 height, int bypp)
+{
+	de_bitmap *img;
+
+	img = de_bitmap_create(c, pdwidth, height, bypp);
+
+	if(npwidth>0 && npwidth<img->width) {
+		img->unpadded_width = npwidth;
+	}
+	else {
+		img->unpadded_width = img->width;
+	}
+
+	return img;
+}
+
 void de_bitmap_destroy(de_bitmap *b)
 {
 	if(b) {
@@ -514,6 +530,9 @@ void de_convert_image_bilevel(dbuf *f, i64 fpos, i64 rowspan,
 	}
 }
 
+// TODO: Review everything using this function, and convert to ..._bilevel2()
+// when appropriate.
+// Maybe remove/rename this function.
 void de_convert_and_write_image_bilevel(dbuf *f, i64 fpos,
 	i64 width, i64 height, i64 rowspan, unsigned int cvtflags,
 	de_finfo *fi, unsigned int createflags)
@@ -524,6 +543,23 @@ void de_convert_and_write_image_bilevel(dbuf *f, i64 fpos,
 	if(!de_good_image_dimensions(c, width, height)) return;
 
 	img = de_bitmap_create(c, width, height, 1);
+	de_convert_image_bilevel(f, fpos, rowspan, img, cvtflags);
+	de_bitmap_write_to_file_finfo(img, fi, createflags);
+	de_bitmap_destroy(img);
+}
+
+// This function automatically handles padding pixels, for the -padpix option.
+// This means the "rowspan" param cannot be used to do clever things --
+// there cannot be any data between the rows, other than padding bits.
+void de_convert_and_write_image_bilevel2(dbuf *f, i64 fpos,
+	i64 width, i64 height, i64 rowspan, unsigned int cvtflags,
+	de_finfo *fi, unsigned int createflags)
+{
+	de_bitmap *img = NULL;
+	deark *c = f->c;
+
+	if(!de_good_image_dimensions(c, width, height)) return;
+	img = de_bitmap_create2(c, width, rowspan*8, height, 1);
 	de_convert_image_bilevel(f, fpos, rowspan, img, cvtflags);
 	de_bitmap_write_to_file_finfo(img, fi, createflags);
 	de_bitmap_destroy(img);

src/deark-cmd.c
@@ -392,6 +392,7 @@ enum opt_id_enum {
  DE_OPT_NULL=0, DE_OPT_D, DE_OPT_D2, DE_OPT_D3, DE_OPT_L,
  DE_OPT_NOINFO, DE_OPT_NOWARN,
  DE_OPT_NOBOM, DE_OPT_NODENS, DE_OPT_ASCIIHTML, DE_OPT_NONAMES,
+ DE_OPT_PADPIX,
  DE_OPT_NOOVERWRITE, DE_OPT_MODTIME, DE_OPT_NOMODTIME,
  DE_OPT_Q, DE_OPT_VERSION, DE_OPT_HELP, DE_OPT_LICENSE, DE_OPT_ID,
  DE_OPT_MAINONLY, DE_OPT_AUXONLY, DE_OPT_EXTRACTALL, DE_OPT_ZIP, DE_OPT_TAR,
@@ -427,6 +428,7 @@ struct opt_struct option_array[] = {
 	{ "n",            DE_OPT_NOOVERWRITE,  0 },
 	{ "modtime",      DE_OPT_MODTIME,      0 },
 	{ "nomodtime",    DE_OPT_NOMODTIME,    0 },
+	{ "padpix",       DE_OPT_PADPIX,       0 },
 	{ "q",            DE_OPT_Q,            0 },
 	{ "version",      DE_OPT_VERSION,      0 },
 	{ "h",            DE_OPT_HELP,         0 },
@@ -666,6 +668,9 @@ static void parse_cmdline(deark *c, struct cmdctx *cc, int argc, char **argv)
 				break;
 			case DE_OPT_NONAMES:
 				de_set_std_option_int(c, DE_STDOPT_FILENAMES_FROM_FILE, 0);
+				break;
+			case DE_OPT_PADPIX:
+				de_set_std_option_int(c, DE_STDOPT_PADPIX, 1);
 				break;
 			case DE_OPT_NOOVERWRITE:
 				de_set_std_option_int(c, DE_STDOPT_OVERWRITE_MODE, DE_OVERWRITEMODE_NEVER);

src/deark-png.c
@@ -25,6 +25,7 @@ struct deark_png_encode_info {
 	deark *c;
 	dbuf *outf;
 	int width, height;
+	int rowspan;
 	int num_chans;
 	int flip;
 	unsigned int level;
@@ -131,7 +132,6 @@ static void write_png_chunk_tEXt(struct deark_png_encode_info *pei,
 static int write_png_chunk_IDAT(struct deark_png_encode_info *pei, dbuf *cdbuf,
 	const u8 *src_pixels)
 {
-	int bpl = pei->width * pei->num_chans; // bytes per row in src_pixels
 	int y;
 	static const char nulbyte = '\0';
 	int retval = 0;
@@ -145,8 +145,8 @@ static int write_png_chunk_IDAT(struct deark_png_encode_info *pei, dbuf *cdbuf,
 
 	for (y = 0; y < pei->height; ++y) {
 		fmtutil_tdefl_compress_buffer(tdctx, &nulbyte, 1, FMTUTIL_TDEFL_NO_FLUSH);
-		fmtutil_tdefl_compress_buffer(tdctx, &src_pixels[(pei->flip ? (pei->height - 1 - y) : y) * bpl],
-			bpl, FMTUTIL_TDEFL_NO_FLUSH);
+		fmtutil_tdefl_compress_buffer(tdctx, &src_pixels[(pei->flip ? (pei->height - 1 - y) : y) * pei->rowspan],
+			(size_t)pei->width * (size_t)pei->num_chans, FMTUTIL_TDEFL_NO_FLUSH);
 	}
 	if (fmtutil_tdefl_compress_buffer(tdctx, NULL, 0, FMTUTIL_TDEFL_FINISH) !=
 		FMTUTIL_TDEFL_STATUS_DONE)
@@ -257,7 +257,13 @@ int de_write_png(deark *c, de_bitmap *img, dbuf *f)
 	}
 
 	pei->outf = f;
-	pei->width = (int)img->width;
+	if(!c->padpix && img->unpadded_width>0 && img->unpadded_width<img->width) {
+		pei->width = (int)img->unpadded_width;
+	}
+	else {
+		pei->width = (int)img->width;
+	}
+	pei->rowspan = (int)(img->width * img->bytes_per_pixel);
 	pei->height = (int)img->height;
 	pei->flip = img->flipped;
 	pei->num_chans = img->bytes_per_pixel;

src/deark-private.h
@@ -240,6 +240,7 @@ struct deark_bitmap_struct {
 	deark *c;
 	i64 width;
 	i64 height;
+	i64 unpadded_width;
 	int invalid_image_flag;
 	int bytes_per_pixel;
 	// 'flipped' changes the coordinate system when writing the bitmap to a file.
@@ -391,6 +392,7 @@ struct deark_struct {
 	u8 filenames_from_file;
 	u8 macformat_known;
 	u8 macformat;
+	u8 padpix;
 	int overwrite_mode;
 	u8 preserve_file_times;
 	u8 preserve_file_times_archives;
@@ -811,6 +813,7 @@ void de_bitmap_setpixel_rgba(de_bitmap *img, i64 x, i64 y, de_color color);
 de_color de_bitmap_getpixel(de_bitmap *img, i64 x, i64 y);
 
 de_bitmap *de_bitmap_create(deark *c, i64 width, i64 height, int bypp);
+de_bitmap *de_bitmap_create2(deark *c, i64 npwidth, i64 pdwidth, i64 height, int bypp);
 void de_bitmap_destroy(de_bitmap *b);
 
 #define DE_COLOR_A(x)  ((de_colorsample)(((x)>>24)&0xff))
@@ -850,6 +853,9 @@ void de_convert_image_bilevel(dbuf *f, i64 fpos, i64 rowspan,
 	de_bitmap *img, unsigned int flags);
 
 void de_convert_and_write_image_bilevel(dbuf *f, i64 fpos,
+	i64 w, i64 h, i64 rowspan, unsigned int cvtflags,
+	de_finfo *fi, unsigned int createflags);
+void de_convert_and_write_image_bilevel2(dbuf *f, i64 fpos,
 	i64 w, i64 h, i64 rowspan, unsigned int cvtflags,
 	de_finfo *fi, unsigned int createflags);
 

src/deark-user.c
@@ -715,6 +715,9 @@ void de_set_std_option_int(deark *c, enum de_stdoptions_enum o, int x)
 	case DE_STDOPT_OVERWRITE_MODE:
 		c->overwrite_mode = x;
 		break;
+	case DE_STDOPT_PADPIX:
+		c->padpix = (u8)x;
+		break;
 	default:
 		de_err(c, "Internal error");
 		de_fatalerror(c);

src/deark-user.h
@@ -33,7 +33,9 @@ enum de_stdoptions_enum {
 	// DE_OVERWRITEMODE_DEFAULT = Overwrite, unless the filename is a symlink, in which case fail.
 	// ..._NEVER = Fail if the output file exists (or if the filename is a symlink).
 	// ..._STANDARD = Do whatever fopen() normally does (overwrite, and follow symlinks).
-	DE_STDOPT_OVERWRITE_MODE
+	DE_STDOPT_OVERWRITE_MODE,
+
+	DE_STDOPT_PADPIX
 };
 
 void de_set_std_option_int(deark *c, enum de_stdoptions_enum o, int x);

commit 0f4737def00f06fa75ace3785bd70271a357488b

bmp: Fixed bug, an invalid file could cause division by 0
Introduced by 1d406c8359804b5f.

modules/bmp.c
@@ -807,7 +807,7 @@ static void do_image(deark *c, lctx *d)
 			// The 24-bit decoder can handle partial pixels.
 			d->pdwidth = (d->rowspan+2)/3;
 		}
-		else {
+		else if(d->bitcount>=1 && d->bitcount<=24) {
 			// By default, ignore a partial-pixel's worth of padding.
 			// bits-per-row / bits-per-pixel
 			d->pdwidth = (d->rowspan*8) / d->bitcount;