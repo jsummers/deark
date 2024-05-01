6c33cad8995d1c587f269bb52526cc388b54e27d
Jason Summers <jason1@pobox.com>
2024-05-01 07:40:33 -0400

    Updated version number for development

--- src/deark-version.h
+++ src/deark-version.h
@@ -2,6 +2,6 @@
 // Copyright (C) 2020-2021 Jason Summers
 // See the file COPYING for terms of use.
 
-#define DE_VERSION_NUMBER 0x01060700U
+#define DE_VERSION_NUMBER 0x01060701U
 #define DE_VERSION_SUFFIX ""
 #define DE_COPYRIGHT_YEAR_STRING "2024"
