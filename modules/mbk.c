b82224aa63364d0633e8b3aba76ed7fb3cfab1cd
Jason Summers <jason1@pobox.com>
2024-06-04 15:27:46 -0400

    stos: Better identification of Picture formats

--- modules/mbk.c
+++ modules/mbk.c
@@ -8,6 +8,10 @@
 #include <deark-private.h>
 #include <deark-fmtutil.h>
 DE_DECLARE_MODULE(de_module_mbk);
+DE_DECLARE_MODULE(de_module_stos_pp1);
+DE_DECLARE_MODULE(de_module_stos_pp2);
+DE_DECLARE_MODULE(de_module_stos_pp3);
+DE_DECLARE_MODULE(de_module_stos_daj);
 
 #define BANKID_PKSCREEN 0x06071963U
 #define BANKID_SPRITE   0x19861987U
@@ -19,6 +23,8 @@ DE_DECLARE_MODULE(de_module_mbk);
 #define PKPIC_IMGTYPE_PP1     110
 #define PKPIC_IMGTYPE_PP3     112
 #define PKPIC_IMGTYPE_M4P     120
+
+static const u8 *g_lion_sig = (const u8*)"Lionpoubnk";
 
 struct pkpic_ctx {
 	UI res_code;
@@ -35,6 +41,7 @@ struct pkpic_ctx {
 };
 
 typedef struct localctx_struct {
+	u8 is_container_fmt;
 	UI imgtype_of_res0; // PKPIC_IMGTYPE_*
 	UI imgtype_of_res1;
 	i64 banknum;
@@ -560,7 +567,8 @@ static void do_pkscreen_bank(deark *c, lctx *d, i64 pos1)
 	de_bitmap *img = NULL;
 	struct atari_img_decode_data *adata = NULL;
 	de_finfo *fi = NULL;
-	i64 pos = pos1;
+	const char *tname = NULL;
+	i64 pos;
 	UI num_planes;
 	UI bits_per_pixel;
 	int saved_indent_level;
@@ -569,6 +577,7 @@ static void do_pkscreen_bank(deark *c, lctx *d, i64 pos1)
 	pp = de_malloc(c, sizeof(struct pkpic_ctx));
 	de_zeromem(d->pal, sizeof(d->pal));
 
+	pos = pos1+4;
 	pp->res_code = (UI)de_getu16be_p(&pos);
 	de_dbg(c, "res: %u", pp->res_code);
 
@@ -583,9 +592,20 @@ static void do_pkscreen_bank(deark *c, lctx *d, i64 pos1)
 		break;
 	case 1:
 		if(d->imgtype_of_res1==PKPIC_IMGTYPE_UNKNOWN) {
-			de_warn(c, "Ambiguous image. If it looks wrong, try \"-opt stos:pp1\" or "
-				"\"-opt stos:pp3\".");
-			pp->imgtype = PKPIC_IMGTYPE_MED;
+			const char *tmps;
+
+			if(d->is_container_fmt) {
+				pp->imgtype = PKPIC_IMGTYPE_MED;
+				tmps = "l|h";
+			}
+			else {
+				// Unfortunately, a file with res=1 (that couldn't be identified by
+				// its extension) seems to be more likely to be the oddball PP1
+				// format than the nice standard STOS medium-resolution format.
+				pp->imgtype = PKPIC_IMGTYPE_PP1;
+				tmps = "m|h";
+			}
+			de_warn(c, "Ambiguous image. If it looks wrong, try \"-opt stos:res1=<%s>\".", tmps);
 		}
 		else {
 			pp->imgtype = d->imgtype_of_res1;
@@ -599,30 +619,39 @@ static void do_pkscreen_bank(deark *c, lctx *d, i64 pos1)
 	case PKPIC_IMGTYPE_LOW:
 		num_planes = 4;
 		bits_per_pixel = 4;
+		tname = "4-plane low res";
 		break;
 	case PKPIC_IMGTYPE_M4P:
 		num_planes = 4;
 		bits_per_pixel = 2;
+		tname = "DAJ 4-plane med res";
 		break;
 	case PKPIC_IMGTYPE_MED:
 		num_planes = 2;
 		bits_per_pixel = 2;
+		tname = "2-plane med res";
 		break;
 	case PKPIC_IMGTYPE_HIGH:
 		num_planes = 1;
 		bits_per_pixel = 1;
+		tname = "1-plane high res";
 		break;
 	case PKPIC_IMGTYPE_PP1:
 		num_planes = 2;
 		bits_per_pixel = 4;
+		tname = "PP1 2-plane low res";
 		break;
 	case PKPIC_IMGTYPE_PP3:
 		num_planes = 2;
 		bits_per_pixel = 1;
+		tname = "PP3 2-plane high res";
 		break;
 	default:
 		de_err(c, "Unsupported picture resolution: %u", pp->res_code);
 		goto done;
+	}
+	if(tname) {
+		de_dbg(c, "interpreted image type: %s", tname);
 	}
 
 	pos += 4;
@@ -655,14 +684,14 @@ static void do_pkscreen_bank(deark *c, lctx *d, i64 pos1)
 	pp->picdata_rel = 70;
 	pp->rledata_rel = de_getu32be_p(&pos);
 	pp->pointdata_rel = de_getu32be_p(&pos);
-	pp->picdata_abs = pos1-4 + pp->picdata_rel;
-	pp->rledata_abs = pos1-4 + pp->rledata_rel;
-	pp->pointdata_abs = pos1-4 + pp->pointdata_rel;
+	pp->picdata_abs = pos1 + pp->picdata_rel;
+	pp->rledata_abs = pos1 + pp->rledata_rel;
+	pp->pointdata_abs = pos1 + pp->pointdata_rel;
 	de_dbg(c, "picdata: %"I64_FMT, pp->picdata_abs);
 	de_dbg(c, "rledata: %"I64_FMT, pp->rledata_abs);
 	de_dbg(c, "pointdata: %"I64_FMT, pp->pointdata_abs);
 
-	pos = pos1+34;
+	pos = pos1+38;
 	de_dbg(c, "palette at %"I64_FMT, pos);
 	de_dbg_indent(c, 1);
 	fmtutil_read_atari_palette(c, c->infile, pos, d->pal, 16, 16, 0);
@@ -743,7 +772,7 @@ static void do_mbk_data_bank(deark *c, lctx *d, i64 pos)
 		do_icon_bank(c, d, pos);
 		break;
 	case BANKID_PKSCREEN:
-		do_pkscreen_bank(c, d, pos+4);
+		do_pkscreen_bank(c, d, pos);
 		break;
 	}
 	de_dbg_indent(c, -1);
@@ -790,28 +819,59 @@ static void do_mbs(deark *c, lctx *d)
 	de_dbg(c, "MBS header at %d", (int)pos);
 }
 
-static void de_run_mbk_mbs(deark *c, de_module_params *mparams)
+static void run_mbk_mbs_internal(deark *c, de_module_params *mparams, UI mode)
 {
 	lctx *d = NULL;
 	u32 id;
-	int opt;
+	const char *s;
+	char opt_res0_c = '\0';
+	char opt_res1_c = '\0';
 	u8 buf[10];
 
 	d = de_malloc(c, sizeof(lctx));
 
 	d->imgtype_of_res1 = PKPIC_IMGTYPE_UNKNOWN;
 	d->imgtype_of_res0 = PKPIC_IMGTYPE_UNKNOWN;
-	opt = de_get_ext_option_bool(c, "stos:pp1", 0);
-	if(opt) d->imgtype_of_res1 = PKPIC_IMGTYPE_PP1;
-	opt = de_get_ext_option_bool(c, "stos:pp2", 0);
-	if(opt) d->imgtype_of_res1 = PKPIC_IMGTYPE_MED;
-	opt = de_get_ext_option_bool(c, "stos:pp3", 0);
-	if(opt) d->imgtype_of_res1 = PKPIC_IMGTYPE_PP3;
-	opt = de_get_ext_option_bool(c, "stos:m4p", 0);
-	if(opt) d->imgtype_of_res0 = PKPIC_IMGTYPE_M4P;
+
+	if(mode==1) { // .pp1 extension
+		d->imgtype_of_res1 = PKPIC_IMGTYPE_PP1;
+	}
+	else if(mode==2) {
+		d->imgtype_of_res1 = PKPIC_IMGTYPE_MED;
+	}
+	else if(mode==3) {
+		d->imgtype_of_res1 = PKPIC_IMGTYPE_PP3;
+	}
+	else if(mode==4) { // .daj extension
+		d->imgtype_of_res0 = PKPIC_IMGTYPE_M4P;
+		d->imgtype_of_res1 = PKPIC_IMGTYPE_MED;
+	}
+
+	s = de_get_ext_option(c, "stos:res0");
+	if(s) opt_res0_c = s[0];
+	s = de_get_ext_option(c, "stos:res1");
+	if(s) opt_res1_c = s[0];
+
+	if(opt_res1_c=='l') {
+		d->imgtype_of_res1 = PKPIC_IMGTYPE_PP1;
+	}
+	else if(opt_res1_c=='m') {
+		d->imgtype_of_res1 = PKPIC_IMGTYPE_MED;
+	}
+	else if(opt_res1_c=='h') {
+		d->imgtype_of_res1 = PKPIC_IMGTYPE_PP3;
+	}
+
+	if(opt_res0_c=='l') {
+		d->imgtype_of_res0 = PKPIC_IMGTYPE_LOW;
+	}
+	else if(opt_res0_c=='m') {
+		d->imgtype_of_res0 = PKPIC_IMGTYPE_M4P;
+	}
 
 	de_read(buf, 0, sizeof(buf));
-	if(!de_memcmp(buf, "Lionpoubnk", 10)) {
+	if(!de_memcmp(buf, g_lion_sig, 10)) {
+		d->is_container_fmt = 1;
 		d->banknum = de_getu32be(10);
 		if(d->banknum==0) {
 			de_declare_fmt(c, "STOS MBS");
@@ -831,7 +891,7 @@ static void de_run_mbk_mbs(deark *c, de_module_params *mparams)
 		}
 		else if(id==BANKID_PKSCREEN) {
 			de_declare_fmt(c, "STOS Packed Screen / Picture Packer");
-			do_pkscreen_bank(c, d, 4);
+			do_pkscreen_bank(c, d, 0);
 		}
 		else {
 			de_err(c, "Not a (supported) STOS/MBK format");
@@ -841,20 +901,52 @@ static void de_run_mbk_mbs(deark *c, de_module_params *mparams)
 	de_free(c, d);
 }
 
+static void de_run_mbk_mbs(deark *c, de_module_params *mparams)
+{
+	run_mbk_mbs_internal(c, mparams, 0);
+}
+
+// Detects raw packed screen, and MBK containing packed screen.
+// Returns the resolution in *res.
+static int is_stos_packedscreen(deark *c, UI *res)
+{
+	u8 buf[24];
+	u8 is_mbk = 0;
+	UI id = 0;
+	i64 bpos = 0;
+
+	*res = 0;
+	de_read(buf, 0, sizeof(buf));
+
+	if(!de_memcmp(buf, g_lion_sig, 10)) is_mbk = 1;
+	if(is_mbk) {
+		if((UI)de_getu32be_direct(&buf[10]) == 0) return 0; // screen out MBS
+		if(buf[14] != 0x81) return 0; // must be type 'data'
+		bpos = 18;
+		return 1;
+	}
+
+	id = (UI)de_getu32be_direct(&buf[bpos]);
+	if(id!=BANKID_PKSCREEN) return 0;
+	*res = (UI)de_getu16be_direct(&buf[bpos+4]);
+	return 1;
+}
+
+// Note: Returned values must be kept consistent with other stos modules.
 static int de_identify_mbk(deark *c)
 {
 	u8 buf[10];
 	UI id;
 
 	de_read(buf, 0, sizeof(buf));
-	if(!de_memcmp(buf, "Lionpoubnk", 10))
-		return 100;
+	if(!de_memcmp(buf, g_lion_sig, 10))
+		return 99;
 	id = (UI)de_getu32be_direct(buf);
 	if(id==BANKID_SPRITE) {
 		return 100;
 	}
 	else if(id==BANKID_PKSCREEN) {
-		return 100;
+		return 99;
 	}
 	return 0;
 }
@@ -862,9 +954,8 @@ static int de_identify_mbk(deark *c)
 static void de_help_mbk(deark *c)
 {
 	fmtutil_atari_help_palbits(c);
-	de_msg(c, "-opt stos:pp1 : Assume res 1 pictures are low res");
-	de_msg(c, "-opt stos:pp3 : Assume res 1 pictures are high res");
-	de_msg(c, "-opt stos:m4p : Assume res 0 pictures are med res");
+	de_msg(c, "-opt stos:res0=<l|m> : Assume res 0 pics are low/med res");
+	de_msg(c, "-opt stos:res1=<l|m|h> : Assume res 1 pics are low/med/high res");
 }
 
 void de_module_mbk(deark *c, struct deark_module_info *mi)
@@ -873,5 +964,120 @@ void de_module_mbk(deark *c, struct deark_module_info *mi)
 	mi->desc = "STOS Memory Bank (.MBK)";
 	mi->run_fn = de_run_mbk_mbs;
 	mi->identify_fn = de_identify_mbk;
+	mi->help_fn = de_help_mbk;
+	mi->id_alias[0] = "mbk";
+}
+
+// Note: Returned values must be kept consistent with other stos modules.
+static int de_identify_stos_pp1(deark *c)
+{
+	int has_ext;
+	int is_pkscr;
+	UI res;
+
+	has_ext = de_input_file_has_ext(c, "pp1");
+	if(!has_ext) return 0;
+	is_pkscr = is_stos_packedscreen(c, &res);
+	if(is_pkscr && res==1) return 100;
+	return 0;
+}
+
+static void de_run_stos_pp1(deark *c, de_module_params *mparams)
+{
+	run_mbk_mbs_internal(c, mparams, 1);
+}
+
+void de_module_stos_pp1(deark *c, struct deark_module_info *mi)
+{
+	mi->id = "stos_pp1";
+	mi->desc = "Picture Packer low res";
+	mi->run_fn = de_run_stos_pp1;
+	mi->identify_fn = de_identify_stos_pp1;
+	mi->help_fn = de_help_mbk;
+}
+
+static void de_run_stos_pp2(deark *c, de_module_params *mparams)
+{
+	run_mbk_mbs_internal(c, mparams, 2);
+}
+
+// Note: Returned values must be kept consistent with other stos modules.
+static int de_identify_stos_pp2(deark *c)
+{
+	int has_ext;
+	int is_pkscr;
+	UI res;
+
+	has_ext = de_input_file_has_ext(c, "pp2");
+	if(!has_ext) return 0;
+	is_pkscr = is_stos_packedscreen(c, &res);
+	if(is_pkscr && res==1) return 100;
+	return 0;
+}
+
+void de_module_stos_pp2(deark *c, struct deark_module_info *mi)
+{
+	mi->id = "stos_pp2";
+	mi->desc = "Picture Packer med res";
+	mi->run_fn = de_run_stos_pp2;
+	mi->identify_fn = de_identify_stos_pp2;
+	mi->help_fn = de_help_mbk;
+}
+
+static void de_run_stos_pp3(deark *c, de_module_params *mparams)
+{
+	run_mbk_mbs_internal(c, mparams, 3);
+}
+
+// Note: Returned values must be kept consistent with other stos modules.
+static int de_identify_stos_pp3(deark *c)
+{
+	int has_ext;
+	int is_pkscr;
+	UI res;
+
+	has_ext = de_input_file_has_ext(c, "pp3");
+	if(!has_ext) return 0;
+	is_pkscr = is_stos_packedscreen(c, &res);
+	if(is_pkscr && res==1) return 100;
+	return 0;
+}
+
+void de_module_stos_pp3(deark *c, struct deark_module_info *mi)
+{
+	mi->id = "stos_pp3";
+	mi->desc = "Picture Packer high res";
+	mi->run_fn = de_run_stos_pp3;
+	mi->identify_fn = de_identify_stos_pp3;
+	mi->help_fn = de_help_mbk;
+}
+
+static void de_run_stos_daj(deark *c, de_module_params *mparams)
+{
+	run_mbk_mbs_internal(c, mparams, 4);
+}
+
+// Note: Returned values must be kept consistent with other stos modules.
+static int de_identify_stos_daj(deark *c)
+{
+	int has_ext;
+	int is_pkscr;
+	UI res;
+
+	has_ext = de_input_file_has_ext(c, "daj");
+	if(!has_ext) return 0;
+	is_pkscr = is_stos_packedscreen(c, &res);
+	if(is_pkscr && (res==0 || res==1)) return 100;
+	return 0;
+}
+
+// .DAJ, a 4-plane medium res format, seems to be used only by a presentation-like
+// viewer used by the ST+ (ST Plus) Atari diskmagazine.
+void de_module_stos_daj(deark *c, struct deark_module_info *mi)
+{
+	mi->id = "stos_daj";
+	mi->desc = "Picture Packer DAJ";
+	mi->run_fn = de_run_stos_daj;
+	mi->identify_fn = de_identify_stos_daj;
 	mi->help_fn = de_help_mbk;
 }
