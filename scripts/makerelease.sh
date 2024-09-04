57fe57f13bee6f8fe2b314943d47007757ea8b5d
Jason Summers <jason1@pobox.com>
2024-09-04 10:29:35 -0400

    Added an icon to the Windows executable

--- scripts/makerelease.sh
+++ scripts/makerelease.sh
@@ -24,7 +24,7 @@ D=".build-tmp/deark-$VER"
 mkdir $D
 
 mkdir $D/src
-cp -p src/*.c src/*.h src/*.rc src/*.manifest $D/src/
+cp -p src/*.c src/*.h src/*.rc src/*.ico src/*.manifest $D/src/
 
 mkdir $D/modules
 cp -p modules/*.c $D/modules/
