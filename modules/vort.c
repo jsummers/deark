e353b149ee7350de893ef493cd8073ce18beba95
Jason Summers <jason1@pobox.com>
2024-09-25 12:46:43 -0400

    vort: Fixed an issue related to deciding if a palette is used
    Ref: GitHub issue #93

--- modules/vort.c
+++ modules/vort.c
@@ -55,23 +55,28 @@ DE_DECLARE_MODULE(de_module_vort);
 #define VORT_I_IMWIDTH   1
 #define VORT_I_IMHEIGHT  2
 #define VORT_I_IMDEPTH   3
+#define VORT_I_ALPHA     7
+#define VORT_I_COLORMAP  10
 #define VORT_I_RLE_CODED 11
 
 // Primitive object types for V_COLORMAP:
 #define VORT_C_ADDR      0
 #define VORT_C_SIZE      1
 
-typedef struct localctx_struct {
+typedef struct localctx_struct_vort {
+	u8 need_errmsg;
 	int nesting_level;
 
 	// A file can potentially contain multiple images and/or multiple colormaps.
 	// But I'm just going to cross my fingers and hope it doesn't.
 	i64 image_data_pos;
 	i64 image_width, image_height;
-	i64 image_depth;
+	i64 reported_image_depth;
 	i64 colormap_pos;
 	i64 colormap_size;
+	i64 image_colormap_addr;
 	u8 rle_flag;
+	u8 image_alpha_flag;
 	struct de_timestamp timestamp;
 
 	u32 pal[256];
@@ -161,7 +166,9 @@ static void decode_simple_item(deark *c, lctx *d,
 		case VORT_I_ADDR: d->image_data_pos = val; break;
 		case VORT_I_IMWIDTH: d->image_width = val; break;
 		case VORT_I_IMHEIGHT: d->image_height = val; break;
-		case VORT_I_IMDEPTH: d->image_depth = val; break;
+		case VORT_I_IMDEPTH: d->reported_image_depth = val; break;
+		case VORT_I_ALPHA: d->image_alpha_flag = 1; break;
+		case VORT_I_COLORMAP: d->image_colormap_addr = val; break;
 		case VORT_I_RLE_CODED: d->rle_flag = 1; break;
 		}
 	}
@@ -184,10 +191,10 @@ static const struct obj_type_info_struct obj_type_info_arr[] = {
 	{ 0x03, VORT_V_IMAGE, 4  /* I_RED */, "red channel flag", NULL },
 	{ 0x03, VORT_V_IMAGE, 5  /* I_GREEN */, "green channel flag", NULL },
 	{ 0x03, VORT_V_IMAGE, 6  /* I_BLUE */, "blue channel flag", NULL },
-	{ 0x03, VORT_V_IMAGE, 7  /* I_ALPHA */, "alpha channel flag", NULL },
+	{ 0x03, VORT_V_IMAGE, VORT_I_ALPHA, "alpha channel flag", decode_simple_item },
 	{ 0x03, VORT_V_IMAGE, 8  /* I_BACKGND */, "background color", NULL },
 	{ 0x01, VORT_V_IMAGE, 9  /* I_DATE */, "creation date", decode_date },
-	{ 0x03, VORT_V_IMAGE, 10 /* I_COLORMAP */, "address of colormap object", NULL },
+	{ 0x03, VORT_V_IMAGE, VORT_I_COLORMAP, "address of colormap object", decode_simple_item },
 	{ 0x03, VORT_V_IMAGE, VORT_I_RLE_CODED, "run length encoded flag", decode_simple_item },
 	{ 0x03, VORT_V_IMAGE, 12 /* I_XADDR */, "x coord if fragment", NULL },
 	{ 0x03, VORT_V_IMAGE, 13 /* I_YADDR */, "y coord if fragment", NULL },
@@ -354,8 +361,11 @@ static void do_decompress(deark *c, lctx *d, i64 pos1, dbuf *unc_pixels,
 		u8 b;
 		i64 count;
 
-		if(pos>c->infile->len) break;
 		if(pixel_count>=num_pixels) break;
+		if(pos>c->infile->len) {
+			de_warn(c, "Unexpected end of file");
+			break;
+		}
 
 		b = de_getbyte(pos);
 		pos++;
@@ -378,6 +388,9 @@ static void do_decompress(deark *c, lctx *d, i64 pos1, dbuf *unc_pixels,
 			pixel_count += count;
 		}
 	}
+
+	dbuf_flush(unc_pixels);
+	de_dbg(c, "decompressed %"I64_FMT" bytes to %"I64_FMT, (pos - pos1), unc_pixels->len);
 }
 
 static void do_image(deark *c, lctx *d)
@@ -386,7 +399,10 @@ static void do_image(deark *c, lctx *d)
 	de_finfo *fi = NULL;
 	dbuf *unc_pixels = NULL;
 	i64 i, j;
-	i64 bytes_per_pixel;
+	i64 pos = 0;
+	u8 uses_colormap = 0;
+	u8 has_alpha = 0;
+	i64 bytes_per_pixel = 0;
 
 	if(d->image_data_pos==0) return;
 
@@ -395,21 +411,49 @@ static void do_image(deark *c, lctx *d)
 
 	if(!de_good_image_dimensions(c, d->image_width, d->image_height)) goto done;
 
-	img = de_bitmap_create(c, d->image_width, d->image_height, 3);
+	if(d->image_colormap_addr!=0 && !d->image_alpha_flag &&
+		(d->reported_image_depth==8 || d->reported_image_depth==24))
+	{
+		bytes_per_pixel = 1;
+		uses_colormap = 1;
+	}
+	else if(d->image_colormap_addr!=0 && d->image_alpha_flag &&
+		(d->reported_image_depth==16 || d->reported_image_depth==32))
+	{
+		bytes_per_pixel = 2;
+		uses_colormap = 1;
+		has_alpha = 1;
+	}
+	else if(d->image_colormap_addr==0 && !d->image_alpha_flag && d->reported_image_depth==24)
+	{
+		bytes_per_pixel = 3;
+	}
+	else if(d->image_colormap_addr==0 && d->image_alpha_flag && d->reported_image_depth==32)
+	{
+		bytes_per_pixel = 4;
+		has_alpha = 1;
+	}
 
-	if(d->image_depth!=8 && d->image_depth!=24) {
-		de_err(c, "Unsupported bits/pixel: %d", (int)d->image_depth);
+	if(!bytes_per_pixel) {
+		d->need_errmsg = 1;
 		goto done;
 	}
-	bytes_per_pixel = d->image_depth/8;
 
-	if(d->image_depth==8 && d->colormap_pos==0) {
-		de_err(c, "Missing colormap");
-		goto done;
+	if(uses_colormap) {
+		// FIXME: We're cheating, by assuming there's only one colormap in the file.
+		if(d->colormap_pos==0) {
+			de_err(c, "Missing colormap");
+			goto done;
+		}
 	}
+
+	de_dbg(c, "interpreted bytes/pixel: %d", (int)bytes_per_pixel);
+
+	img = de_bitmap_create(c, d->image_width, d->image_height, has_alpha?4:3);
 
 	if(d->rle_flag) {
 		unc_pixels = dbuf_create_membuf(c, 0, 0);
+		dbuf_enable_wbuffer(unc_pixels);
 		do_decompress(c, d, d->image_data_pos, unc_pixels,
 			d->image_width*d->image_height, bytes_per_pixel);
 	}
@@ -420,16 +464,22 @@ static void do_image(deark *c, lctx *d)
 
 	for(j=0; j<d->image_height; j++) {
 		for(i=0; i<d->image_width; i++) {
-			if(d->image_depth==8) {
-				u8 b;
-				b = dbuf_getbyte(unc_pixels, j*d->image_width + i);
-				de_bitmap_setpixel_rgb(img, i, j, d->pal[(unsigned int)b]);
+			u8 b;
+			de_color clr;
+
+			if(uses_colormap) {
+				b = dbuf_getbyte_p(unc_pixels, &pos);
+				clr = d->pal[(UI)b];
 			}
-			else if(d->image_depth==24) {
-				u32 clr;
-				clr = dbuf_getRGB(unc_pixels, (j*d->image_width + i)*bytes_per_pixel, 0);
-				de_bitmap_setpixel_rgb(img, i, j, clr);
+			else {
+				clr = dbuf_getRGB(unc_pixels, pos, 0);
+				pos += 3;
 			}
+			if(has_alpha) {
+				b = dbuf_getbyte_p(unc_pixels, &pos);
+				clr = DE_SET_ALPHA(clr, b);
+			}
+			de_bitmap_setpixel_rgba(img, i, j, clr);
 		}
 	}
 
@@ -467,7 +517,12 @@ static void de_run_vort(deark *c, de_module_params *mparams)
 	do_image(c, d);
 
 done:
-	de_free(c, d);
+	if(d) {
+		if(d->need_errmsg) {
+			de_err(c, "Bad or unsupported image type");
+		}
+		de_free(c, d);
+	}
 }
 
 static int de_identify_vort(deark *c)
