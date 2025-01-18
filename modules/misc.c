3bc299a0d668dc04b8fbaf88b9b8befce5717beb
Jason Summers <jason1@pobox.com>
2025-01-18 11:32:31 -0500

    New "fontedit" module

--- modules/misc.c
+++ modules/misc.c
@@ -22,6 +22,7 @@ DE_DECLARE_MODULE(de_module_mrw);
 DE_DECLARE_MODULE(de_module_vgafont);
 DE_DECLARE_MODULE(de_module_fontmania);
 DE_DECLARE_MODULE(de_module_pcrfont);
+DE_DECLARE_MODULE(de_module_fontedit);
 DE_DECLARE_MODULE(de_module_zbr);
 DE_DECLARE_MODULE(de_module_compress);
 DE_DECLARE_MODULE(de_module_hpi);
@@ -826,28 +827,7 @@ struct vgafont_ctx {
 	u8 need_errmsg;
 };
 
-static void vgafont_main(deark *c, struct vgafont_ctx *d)
-{
-	i64 i;
-	struct de_encconv_state es;
-
-	de_encconv_init(&es, d->encoding_to_use);
-
-	for(i=0; i<d->font->num_chars; i++) {
-		d->font->char_array[i].codepoint_nonunicode = (i32)i;
-		if(d->font->has_unicode_codepoints) {
-			d->font->char_array[i].codepoint_unicode = de_char_to_unicode_ex((i32)i, &es);
-		}
-		d->font->char_array[i].width = d->font->nominal_width;
-		d->font->char_array[i].height = d->font->nominal_height;
-		d->font->char_array[i].rowspan = 1;
-		d->font->char_array[i].bitmap = &d->fontdata[i*d->font->nominal_height];
-	}
-
-	de_font_bitmap_font_to_image(c, d->font, NULL, 0);
-}
-
-static void vgafont_common_config(deark *c, struct vgafont_ctx *d)
+static void vgafont_common_config1(deark *c, struct vgafont_ctx *d)
 {
 	d->encoding_req = de_get_input_encoding(c, NULL, DE_ENCODING_UNKNOWN);
 	if(d->encoding_req!=DE_ENCODING_UNKNOWN)
@@ -864,6 +844,30 @@ static void vgafont_common_config(deark *c, struct vgafont_ctx *d)
 	d->font->nominal_height = (int)d->height;
 	d->font->char_array = de_mallocarray(c, d->font->num_chars,
 		sizeof(struct de_bitmap_font_char));
+}
+
+static void vgafont_common_config2(deark *c, struct vgafont_ctx *d)
+{
+	i64 i;
+	struct de_encconv_state es;
+
+	de_encconv_init(&es, d->encoding_to_use);
+
+	for(i=0; i<d->font->num_chars; i++) {
+		d->font->char_array[i].codepoint_nonunicode = (i32)i;
+		if(d->font->has_unicode_codepoints) {
+			d->font->char_array[i].codepoint_unicode = de_char_to_unicode_ex((i32)i, &es);
+		}
+		d->font->char_array[i].width = d->font->nominal_width;
+		d->font->char_array[i].height = d->font->nominal_height;
+		d->font->char_array[i].rowspan = 1;
+		d->font->char_array[i].bitmap = &d->fontdata[i*d->font->nominal_height];
+	}
+}
+
+static void vgafont_main(deark *c, struct vgafont_ctx *d, de_finfo *fi, UI createflags)
+{
+	de_font_bitmap_font_to_image(c, d->font, fi, createflags);
 }
 
 static void destroy_vgafont_ctx(deark *c, struct vgafont_ctx *d)
@@ -907,8 +911,9 @@ static void de_run_vgafont(deark *c, de_module_params *mparams)
 		goto done;
 	}
 
-	vgafont_common_config(c, d);
-	vgafont_main(c, d);
+	vgafont_common_config1(c, d);
+	vgafont_common_config2(c, d);
+	vgafont_main(c, d, NULL, 0);
 
 done:
 	destroy_vgafont_ctx(c, d);
@@ -952,8 +957,9 @@ static void de_run_fontmania(deark *c, de_module_params *mparams)
 	d->fontdata = de_malloc(c, d->font_data_size);
 	de_read(d->fontdata, d->font_data_pos, d->font_data_size);
 
-	vgafont_common_config(c, d);
-	vgafont_main(c, d);
+	vgafont_common_config1(c, d);
+	vgafont_common_config2(c, d);
+	vgafont_main(c, d, NULL, 0);
 
 done:
 	if(d) {
@@ -966,6 +972,7 @@ done:
 
 static int de_identify_fontmania(deark *c)
 {
+	if(c->infile->len>65280) return 0;
 	if(de_getbyte(0) != 0xeb) return 0;
 	if(dbuf_memcmp(c->infile, 8, (const u8*)"FONT MANIA, V", 13)) {
 		return 0;
@@ -1014,8 +1021,9 @@ static void de_run_pcrfont(deark *c, de_module_params *mparams)
 	d->fontdata = de_malloc(c, d->font_data_size);
 	dbuf_read(c->infile, d->fontdata, d->font_data_pos, d->font_data_size);
 
-	vgafont_common_config(c, d);
-	vgafont_main(c, d);
+	vgafont_common_config1(c, d);
+	vgafont_common_config2(c, d);
+	vgafont_main(c, d, NULL, 0);
 
 done:
 	if(d) {
@@ -1044,6 +1052,119 @@ void de_module_pcrfont(deark *c, struct deark_module_info *mi)
 	mi->desc = "PCR font";
 	mi->run_fn = de_run_pcrfont;
 	mi->identify_fn = de_identify_pcrfont;
+}
+
+// **************************************************************************
+// FONTEDIT
+// (Michael J. Mefford, PC Magazine)
+// **************************************************************************
+
+static void de_run_fontedit(deark *c, de_module_params *mparams)
+{
+	struct vgafont_ctx *d = NULL;
+	i64 jmp1;
+	u8 opt_extract_template;
+	de_finfo *fi_tmpl = NULL;
+	struct de_crcobj *crco = NULL;
+
+	d = de_malloc(c, sizeof(struct vgafont_ctx));
+	opt_extract_template = (u8)de_get_ext_option_bool(c, "fontedit:template",
+		(c->extract_level>=2 ? 1 : 0));
+
+	jmp1 = de_getbyte(1) + 2;
+	d->height = (i64)de_getbyte(jmp1-3);
+	de_dbg(c, "char size: 8"DE_CHAR_TIMES"%d", (int)d->height);
+	d->font_data_pos = de_getu16le(jmp1+25) - 0x100;
+	de_dbg(c, "data pos: %"I64_FMT, d->font_data_pos);
+	d->font_data_size = d->height*256;
+
+	if(d->height<VGAFONT_MINH || d->height>VGAFONT_MAXH ||
+		(d->font_data_pos + d->font_data_size > c->infile->len))
+	{
+		d->need_errmsg = 1;
+		goto done;
+	}
+
+	d->fontdata = de_malloc(c, d->font_data_size);
+	de_read(d->fontdata, d->font_data_pos, d->font_data_size);
+
+	vgafont_common_config1(c, d);
+	vgafont_common_config2(c, d);
+	vgafont_main(c, d, NULL, 0);
+
+	if(opt_extract_template) {
+		u32 crc1, crc2;
+
+		crco = de_crcobj_create(c, DE_CRCOBJ_CRC32_IEEE);
+		de_crcobj_addbuf(crco, d->fontdata, d->font_data_size);
+		crc1 = de_crcobj_getval(crco);
+
+		d->font_data_pos += 4096;
+		if(d->font_data_pos + d->font_data_size > c->infile->len) {
+			de_dbg(c, "[template not present]");
+			goto done;
+		}
+		de_dbg(c, "template pos: %"I64_FMT, d->font_data_pos);
+
+		// This is a bit hacky, but we can just change the font data out
+		// from under the pointers created by vgafont_common_config2().
+		de_read(d->fontdata, d->font_data_pos, d->font_data_size);
+		de_crcobj_reset(crco);
+		de_crcobj_addbuf(crco, d->fontdata, d->font_data_size);
+		crc2 = de_crcobj_getval(crco);
+
+		if(crc2==crc1) {
+			de_dbg(c, "[template is same as main font]");
+			goto done;
+		}
+
+		fi_tmpl = de_finfo_create(c);
+		de_finfo_set_name_from_sz(c, fi_tmpl, "template", 0, DE_ENCODING_LATIN1);
+		vgafont_main(c, d, fi_tmpl, DE_CREATEFLAG_IS_AUX);
+	}
+
+done:
+	if(d) {
+		if(d->need_errmsg) {
+			de_err(c, "Bad or unsupported FONTEDIT font");
+		}
+		destroy_vgafont_ctx(c, d);
+	}
+	de_finfo_destroy(c, fi_tmpl);
+	de_crcobj_destroy(crco);
+}
+
+static int de_identify_fontedit(deark *c)
+{
+	u8 b1;
+	i64 jmp1;
+
+	if(c->infile->len>0x2200 || c->infile->len<(54+3*256)) return 0;
+	if(de_getbyte(0) != 0xeb) return 0;
+	b1 = de_getbyte(1);
+	if(b1<0x32 || b1>0x33) return 0;
+	jmp1 = (i64)b1 + 2;
+	// Code for "INT 10/AX=1110h": the system call that sets the custom font
+	if(dbuf_memcmp(c->infile, jmp1+39,
+		(const void*)"\xb8\x10\x11\xcd\x10", 5))
+	{
+		return 0;
+	}
+	return 90;
+}
+
+static void de_help_fontedit(deark *c)
+{
+	de_msg(c, "-opt fontedit:template : Also extract the template font, if different");
+}
+
+void de_module_fontedit(deark *c, struct deark_module_info *mi)
+{
+	mi->id = "fontedit";
+	mi->desc = "FONTEDIT font";
+	mi->run_fn = de_run_fontedit;
+	mi->identify_fn = de_identify_fontedit;
+	mi->help_fn = de_help_fontedit;
 }
 
 // **************************************************************************
