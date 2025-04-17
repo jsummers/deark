0327edabf67ca0c59693878a0fd21ab4d90a3bb9
Jason Summers <jason1@pobox.com>
2025-04-17 07:38:19 -0400

    New "div_map" and "div_fpg" modules

--- deps.mk
+++ deps.mk
@@ -81,6 +81,8 @@ $(OBJDIR)/modules/d64.o: modules/d64.c src/deark-config.h \
  src/deark-private.h src/deark.h
 $(OBJDIR)/modules/diet.o: modules/diet.c src/deark-private.h src/deark.h \
  src/deark-config.h src/deark-fmtutil.h
+$(OBJDIR)/modules/divgs.o: modules/divgs.c src/deark-private.h \
+ src/deark.h src/deark-config.h
 $(OBJDIR)/modules/dlmaker.o: modules/dlmaker.c src/deark-private.h \
  src/deark.h src/deark-config.h
 $(OBJDIR)/modules/dms.o: modules/dms.c src/deark-private.h src/deark.h \
