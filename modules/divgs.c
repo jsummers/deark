48cb3e536500d9765384eb9510c40be2be281326
Jason Summers <jason1@pobox.com>
2025-04-20 12:53:46 -0400

    div_fpg: Support "f16" format

--- modules/divgs.c
+++ modules/divgs.c
@@ -8,9 +8,14 @@
 DE_DECLARE_MODULE(de_module_div_map);
 DE_DECLARE_MODULE(de_module_div_fpg);
 
+#define DIVFMT_MAP 1
+#define DIVFMT_FPG 2
+#define DIVFMT_F16 3
+
 struct div_image_ctx {
 	i64 imgpos;
 	i64 item_len;
+	i64 bitmap_pos;
 	i64 w, h;
 	de_ucstring *descr;
 	de_ucstring *fn;
@@ -19,10 +24,24 @@ struct div_image_ctx {
 
 struct div_ctx {
 	de_encoding input_encoding;
-	u8 is_fpg;
+	u8 fmt;
+	u8 is_archive; // fpg or f16
 	u8 need_errmsg;
 	de_color pal[256];
 };
+
+static u8 identify_div_fmt(deark *c)
+{
+	u8 buf[7];
+
+	if(de_getbyte(3) != 0x1a) return 0;
+	de_read(buf, 0, sizeof(buf));
+	if(de_memcmp(&buf[4], (const void*)"\x0d\x0a\0", 3)) return 0;
+	if(!de_memcmp(buf, (const void*)"map", 3)) return DIVFMT_MAP;
+	if(!de_memcmp(buf, (const void*)"fpg", 3)) return DIVFMT_FPG;
+	if(!de_memcmp(buf, (const void*)"f16", 3)) return DIVFMT_F16;
+	return 0;
+}
 
 static void div_image_destroy(deark *c, struct div_image_ctx *md)
 {
@@ -53,16 +72,41 @@ static void div_read_descr_p(deark *c, struct div_ctx *d,
 	*ppos += 32;
 }
 
-static void div_do_bitmap(deark *c, struct div_ctx *d, struct div_image_ctx *md,
-	i64 bitmap_pos)
+static void div_convert_f16(deark *c, struct div_ctx *d, struct div_image_ctx *md,
+	de_bitmap *img)
+{
+	i64 i, j;
+
+	for(j=0; j<md->h; j++) {
+		i64 pxpos;
+
+		pxpos = md->bitmap_pos + md->w*2*j;
+		for(i=0; i<md->w; i++) {
+			UI x;
+			de_color clr;
+
+			x = (UI)de_getu16le_p(&pxpos);
+			clr = de_rgb565_to_888(x);
+			de_bitmap_setpixel_rgb(img, i, j, clr);
+		}
+	}
+}
+
+static void div_do_bitmap(deark *c, struct div_ctx *d, struct div_image_ctx *md)
 {
 	de_bitmap *img = NULL;
 
-	de_dbg(c, "bitmap at %"I64_FMT, bitmap_pos);
+	de_dbg(c, "bitmap at %"I64_FMT, md->bitmap_pos);
 	if(!de_good_image_dimensions(c, md->w, md->h)) goto done;
 	img = de_bitmap_create(c, md->w, md->h, 3);
-	de_convert_image_paletted(c->infile, bitmap_pos, 8,
-		md->w, d->pal, img, 0);
+
+	if(d->fmt==DIVFMT_F16) {
+		div_convert_f16(c, d, md, img);
+	}
+	else {
+		de_convert_image_paletted(c->infile, md->bitmap_pos, 8,
+			md->w, d->pal, img, 0);
+	}
 	de_bitmap_write_to_file_finfo(img, md->fi, DE_CREATEFLAG_OPT_IMAGE);
 done:
 	de_bitmap_destroy(img);
@@ -73,7 +117,7 @@ static void div_read_cpoints_p(deark *c, struct div_ctx *d, i64 *ppos)
 	i64 n;
 
 	// number of 4-byte "cpoints" items
-	if(d->is_fpg) {
+	if(d->is_archive) {
 		n = de_getu32le_p(ppos);
 	}
 	else {
@@ -92,6 +136,7 @@ static void de_run_div_map(deark *c, de_module_params *mparams)
 	struct div_image_ctx *md = NULL;
 	i64 pos;
 
+	de_declare_fmt(c, "DIV MAP");
 	d = de_malloc(c, sizeof(struct div_ctx));
 	d->input_encoding = de_get_input_encoding(c, NULL, DE_ENCODING_CP850);
 
@@ -116,7 +161,8 @@ static void de_run_div_map(deark *c, de_module_params *mparams)
 
 	div_read_cpoints_p(c, d, &pos);
 
-	div_do_bitmap(c, d, md, pos);
+	md->bitmap_pos = pos;
+	div_do_bitmap(c, d, md);
 
 done:
 	div_image_destroy(c, md);
@@ -194,7 +240,8 @@ static void div_fpg_image(deark *c, struct div_ctx *d,
 
 	div_read_cpoints_p(c, d, &pos);
 
-	div_do_bitmap(c, d, md, pos);
+	md->bitmap_pos = pos;
+	div_do_bitmap(c, d, md);
 
 done:
 	de_bitmap_destroy(img);
@@ -208,7 +255,18 @@ static void de_run_div_fpg(deark *c, de_module_params *mparams)
 	i64 pos;
 
 	d = de_malloc(c, sizeof(struct div_ctx));
-	d->is_fpg = 1;
+	d->fmt = identify_div_fmt(c);
+	if(d->fmt==DIVFMT_FPG) {
+		de_declare_fmt(c, "DIV FPG");
+	}
+	else if(d->fmt==DIVFMT_F16) {
+		de_declare_fmt(c, "DIV F16");
+	}
+	else {
+		d->need_errmsg = 1;
+		goto done;
+	}
+	d->is_archive = 1;
 	d->input_encoding = de_get_input_encoding(c, NULL, DE_ENCODING_CP850);
 
 	if(de_getbyte(7) != 0) {
@@ -217,7 +275,9 @@ static void de_run_div_fpg(deark *c, de_module_params *mparams)
 	}
 
 	pos = 8;
-	div_read_pal_p(c, d, &pos);
+	if(d->fmt==DIVFMT_FPG) {
+		div_read_pal_p(c, d, &pos);
+	}
 
 	de_dbg(c, "item sequence at: %"I64_FMT, pos);
 
@@ -247,10 +307,10 @@ done:
 
 static int de_identify_div_fpg(deark *c)
 {
-	if(!dbuf_memcmp(c->infile, 0, (const void*)"fpg\x1a\x0d\x0a\0", 7)) {
-		return 100;
-	}
-	// TODO: "f16" format?
+	u8 x;
+
+	x = identify_div_fmt(c);
+	if(x==DIVFMT_FPG || x==DIVFMT_F16) return 100;
 	return 0;
 }
 
