98508fa092ee019aca89400ad325545d9d0c0507
Jason Summers <jason1@pobox.com>
2025-10-07 15:57:16 -0400

    grabber: Decode some COM-based CGA bitmaps

--- modules/grabber.c
+++ modules/grabber.c
@@ -14,7 +14,7 @@ DE_DECLARE_MODULE(de_module_grabber);
 
 struct grabber_id_data {
 	u8 is_grabber;
-	UI fmt_class;
+	UI fmt_class; // A representative version, times 10
 	i64 jmppos;
 };
 
@@ -23,29 +23,35 @@ typedef struct localctx_grabber {
 	u8 need_errmsg;
 
 	u8 screen_mode;
+	u8 pal_info;
 	i64 data_pos, data_len;
+	de_finfo *fi;
 
 	struct de_char_context *charctx;
 	struct fmtutil_char_simplectx csctx;
 	struct grabber_id_data gi;
+	de_color pal[256];
 } lctx;
 
 static void free_lctx(deark *c, lctx *d)
 {
 	if(!d) return;
+	de_finfo_destroy(c, d->fi);
 	de_free_charctx(c, d->charctx);
 	de_free(c, d);
 }
 
 static void grabber_id_com(deark *c, u8 b0, struct grabber_id_data *gi)
 {
+	u8 b;
+
 	de_zeromem(gi, sizeof(struct grabber_id_data));
 
 	if(b0==0xfb) {
 		if(!dbuf_memcmp(c->infile, 1,
 			(const void*)"\xbe\x81\x00\x8a\x4c\xff\x30\xed\x09\xc9\x74", 11)) {
 			gi->is_grabber = 1;
-			gi->fmt_class = 200;
+			gi->fmt_class = 2000;
 		}
 		return;
 	}
@@ -57,14 +63,20 @@ static void grabber_id_com(deark *c, u8 b0, struct grabber_id_data *gi)
 		(const void*)"\xbe\x81\x00\xad\x80\xfc\x0d\x74\x17\x3c\x0d\x74", 12))
 	{
 		gi->is_grabber = 1;
-		gi->fmt_class = 300;
+		b = de_getbyte(gi->jmppos+22);
+		if(b==0x04) {
+			gi->fmt_class = 3000; // 3.00-3.20
+		}
+		else {
+			gi->fmt_class = 3201; // 3.20K-3.30
+		}
 		return;
 	}
 	if(!dbuf_memcmp(c->infile, gi->jmppos,
 		(const void*)"\xbe\x81\x00\xfc\xad\x80\xfc\x0d\x74\x1c\x3c\x0d\x74", 13))
 	{
 		gi->is_grabber = 1;
-		gi->fmt_class = 334;
+		gi->fmt_class = 3340;
 		return;
 	}
 }
@@ -99,7 +111,7 @@ static void decode_grabber_com(deark *c, lctx *d)
 	d->data_pos -= 256;
 	de_dbg(c, "data pos: %"I64_FMT, d->data_pos);
 
-	if(d->gi.fmt_class<300) {
+	if(d->gi.fmt_class<3000) {
 		pos_of_mode = d->data_pos - 7;
 	}
 	else {
@@ -108,31 +120,22 @@ static void decode_grabber_com(deark *c, lctx *d)
 
 	d->screen_mode = de_getbyte(pos_of_mode);
 	de_dbg(c, "mode: 0x%02x", (UI)d->screen_mode);
-	if(d->screen_mode != 3) {
-		d->errflag = 1;
-		d->need_errmsg = 1;
-		goto done;
+
+	if(d->screen_mode==4) {
+		d->pal_info = de_getbyte(pos_of_mode+1);
+		de_dbg(c, "palette info: 0x%02x", (UI)d->pal_info);
 	}
 
 	d->data_len = de_getu16le(pos_of_mode+2);
 	de_dbg(c, "data len: %"I64_FMT, d->data_len);
-	d->csctx.width_in_chars = 80;
+
 done:
 	de_free(c, mem);
 }
 
-static void do_grabber_com(deark *c, lctx *d, de_module_params *mparams, u8 b0)
+static void do_grabber_textmode(deark *c, lctx *d)
 {
-	grabber_id_com(c, b0, &d->gi);
-	if(!d->gi.is_grabber) {
-		d->need_errmsg = 1;
-		goto done;
-	}
-
-	de_dbg(c, "format class: %u", d->gi.fmt_class);
-	decode_grabber_com(c, d);
-	if(d->errflag) goto done;
-
+	d->csctx.width_in_chars = 80;
 	d->charctx->screen_image_flag = 1;
 	d->csctx.height_in_chars = de_pad_to_n(d->data_len, d->csctx.width_in_chars*2) /
 		(d->csctx.width_in_chars*2);
@@ -152,6 +155,112 @@ static void do_grabber_com(deark *c, lctx *d, de_module_params *mparams, u8 b0)
 	d->csctx.inf_pos = d->data_pos;
 	d->csctx.inf_len = d->data_len;
 	fmtutil_char_simple_run(c, &d->csctx, d->charctx);
+done:
+	;
+}
+
+static void read_and_deinterlace_cga(deark *c, dbuf *inf, i64 pos1, dbuf *outf)
+{
+	i64 j;
+
+	for(j=0; j<200; j++) {
+		i64 spos;
+
+		spos = pos1 + (j/2)*80 + (j%2)*8192;
+		dbuf_copy(inf, spos, 80, outf);
+	}
+}
+
+static void do_grabber_cga(deark *c, lctx *d, dbuf *inf, i64 pos1, i64 len)
+{
+	de_bitmap *img = NULL;
+	dbuf *tmpf = NULL;
+
+	tmpf = dbuf_create_membuf(c, 16384, 0);
+	read_and_deinterlace_cga(c, inf, pos1, tmpf);
+
+	d->fi->density.code = DE_DENSITY_UNK_UNITS;
+
+	if(d->screen_mode==6) {
+		d->fi->density.xdens = 480.0;
+		d->fi->density.ydens = 200.0;
+		de_make_grayscale_palette(d->pal, 2, 0);
+		img = de_bitmap_create(c, 640, 200, 1);
+		de_convert_image_paletted(tmpf, 0, 1, 80, d->pal, img, 0);
+	}
+	else {
+		int pal_subid = 3;
+
+		d->fi->density.xdens = 240.0;
+		d->fi->density.ydens = 200.0;
+
+		// Intended to reflect how the file actually displays itself (on a
+		// system with CGA-only graphics).
+		// I.e., not necessarily the image that should have been captured.
+		// GRABBER seems buggy, but I guess we'll copy the bugs.
+		if(d->gi.fmt_class<=3200) { // v2.10-3.20
+			switch((d->pal_info & 0x30)>>4) {
+			case 0: pal_subid = 1; break;
+			case 1: pal_subid = 4; break;
+			case 2: pal_subid = 0; break;
+			case 3: pal_subid = 3; break;
+			}
+		}
+		else { // v3.20K-3.35
+			pal_subid = (d->pal_info & 0x10)?0:1;
+		}
+		de_copy_std_palette(DE_PALID_CGA, pal_subid, 0, d->pal, 256, 0);
+		d->pal[0] = de_get_std_palette_entry(DE_PALID_PC16, 0, (d->pal_info & 0x0f));
+
+		img = de_bitmap_create(c, 320, 200, 3);
+		de_convert_image_paletted(tmpf, 0, 2, 80, d->pal, img, 0);
+	}
+
+	de_bitmap_write_to_file_finfo(img, d->fi, DE_CREATEFLAG_OPT_IMAGE);
+
+	de_bitmap_destroy(img);
+	dbuf_close(tmpf);
+}
+
+static void do_grabber_bitmapmode(deark *c, lctx *d)
+{
+	d->fi = de_finfo_create(c);
+
+	if(d->screen_mode==4 || d->screen_mode==6) {
+		do_grabber_cga(c, d, c->infile, d->data_pos, d->data_len);
+	}
+	else {
+		d->errflag = 1;
+		d->need_errmsg = 1;
+		goto done;
+	}
+done:
+	;
+}
+static void do_grabber_com(deark *c, lctx *d, de_module_params *mparams, u8 b0)
+{
+	grabber_id_com(c, b0, &d->gi);
+	if(!d->gi.is_grabber) {
+		d->need_errmsg = 1;
+		goto done;
+	}
+
+	de_dbg(c, "format class: %u", d->gi.fmt_class);
+	decode_grabber_com(c, d);
+	if(d->errflag) goto done;
+
+
+	if(d->screen_mode==3) {
+		do_grabber_textmode(c, d);
+	}
+	else if(d->screen_mode==4 || d->screen_mode==6) {
+		do_grabber_bitmapmode(c, d);
+	}
+	else {
+		d->errflag = 1;
+		d->need_errmsg = 1;
+		goto done;
+	}
 
 done:
 	;
