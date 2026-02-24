c05ac785e08105d7b9b90592e837b5839e8d64aa
Jason Summers <jason1@pobox.com>
2026-02-24 16:40:43 -0500

    New "spacemaker" module

---
+++ modules/spacemaker.c
@@ -0,0 +1,273 @@
+// This file is part of Deark.
+// Copyright (C) 2026 Jason Summers
+// See the file COPYING for terms of use.
+
+// Realia Spacemaker executable compression
+
+#include <deark-private.h>
+DE_DECLARE_MODULE(de_module_spacemaker);
+
+#define SM_PREAMBLE_SIZE 25
+
+typedef struct localctx_spacemaker {
+	u8 errflag;
+	u8 need_errmsg;
+	u8 raw_mode; // 0xff = not set
+	u8 original_was_exe;
+	i64 orig_len;
+	i64 part1_pos, part1_len; // first 25 bytes
+	i64 part2_pos, part2_endpos, part2_len; // rest of file
+	i64 part3_pos, part3_len; // reloc. table(?)
+	dbuf *dcmpr_code;
+} lctx;
+
+static void sm_decompress(deark *c, lctx *d)
+{
+	i64 ipos; // in c->infile
+	i64 opos; // in d->dcmpr_code
+	u8 b[2];
+	int saved_indent_level;
+
+	de_dbg_indent_save(c, &saved_indent_level);
+	de_dbg(c, "[decompressing]");
+	de_dbg_indent(c, 1);
+
+	de_zeromem(b, sizeof(b));
+	dbuf_truncate(d->dcmpr_code, d->orig_len);
+
+	opos = d->orig_len;
+	ipos = d->part2_endpos;
+	while(1) {
+		i64 count;
+#define CT_U 1 // An uncompressed segment
+#define CT_R 2 // A run of 0-valued bytes
+		u8 code_type;
+
+		if(ipos<=SM_PREAMBLE_SIZE) goto done;
+
+		code_type = 0;
+		count = 0;
+
+		ipos--;
+		b[0] = de_getbyte(ipos);
+		if(b[0] & 0x01) {
+			// ... xxxxxxx1
+			ipos--;
+			b[1] = de_getbyte(ipos);
+			if(b[1] & 0x01) {
+				//   b[1]     b[0]
+				// rrrrrrr1 rrrrrrr1
+				code_type = CT_R;
+				count = ((b[0]&0xfe)<<6) | (b[1]>>1);
+			}
+			else {
+				//   b[1]     b[0]
+				// uuuuuuu0 uuuuuuu1
+				code_type = CT_U;
+				count = ((b[0]&0xfe)<<6) | (b[1]>>1);
+			}
+		}
+		else if(b[0] & 0x02) {
+			// rrrrrr10
+			code_type = CT_R;
+			count = b[0]>>2;
+		}
+		else {
+			// uuuuuu00
+			code_type = CT_U;
+			count = b[0]>>2;
+		}
+
+		if(count==0 && code_type==CT_U) {
+			// All remaining data is uncompressed.
+			count = ipos - d->part2_pos;
+		}
+
+		if(code_type==CT_R) {
+			de_dbg3(c, "i=%"I64_FMT" o=%"I64_FMT" rle %"I64_FMT, ipos, opos, count);
+		}
+		else {
+			de_dbg3(c, "i=%"I64_FMT" o=%"I64_FMT" unc %"I64_FMT, ipos, opos, count);
+		}
+
+		// For CT_R, this is all we need to do. The part of the file we skip
+		// over was initialized to 0, so just leave it that way.
+		opos -= count;
+		if(opos<SM_PREAMBLE_SIZE) {
+			d->errflag = 1;
+			d->need_errmsg = 1;
+			goto done;
+		}
+
+		if(code_type==CT_U) {
+			ipos -= count;
+			if(ipos<SM_PREAMBLE_SIZE) {
+				d->errflag = 1;
+				d->need_errmsg = 1;
+				goto done;
+			}
+			dbuf_copy_at(c->infile, ipos, count, d->dcmpr_code, opos);
+		}
+	}
+
+done:
+	if(!d->errflag) {
+		if(ipos!=SM_PREAMBLE_SIZE || opos!=SM_PREAMBLE_SIZE) {
+			d->errflag = 1;
+			d->need_errmsg = 1;
+		}
+	}
+	if(!d->errflag) {
+		de_dbg(c, "decompressed %"I64_FMT" bytes to %"I64_FMT, d->part2_len,
+			(i64)(d->dcmpr_code->len-SM_PREAMBLE_SIZE));
+	}
+	de_dbg_indent_restore(c, saved_indent_level);
+}
+
+static void spacemaker_main(deark *c, lctx *d)
+{
+	UI x;
+	i64 n;
+	i64 seg;
+	i64 jmppos;
+	i64 raw_field1, raw_field2;
+	UI ver;
+	const char *ext;
+	dbuf *outf = NULL;
+
+	seg = de_getu16le(10);
+	n = de_getu16le(14);
+	jmppos = 16*seg + n;
+	de_dbg(c, "jmp pos: %"I64_FMT, jmppos);
+
+	x = (UI)de_getu16be(jmppos);
+	if(x!=0x5751) {
+		de_err(c, "Bad file or unknown version");
+		d->errflag = 1;
+		goto done;
+	}
+
+	x = (UI)de_getu16be(jmppos+13);
+	if(x==0xd78c) ver = 103;
+	else if(x==0x8ec5) ver = 106;
+	else {
+		de_err(c, "Unknown version");
+		d->errflag = 1;
+		goto done;
+	}
+	de_dbg(c, "version: %u.%02u", ver/100, ver%100);
+
+	// apparently part2 endpos, minus 1
+	if(ver==106) n = 126;
+	else n = 170;
+	raw_field1 = de_getu16le(jmppos+n);
+
+	// apparently original size, minus 1
+	if(ver==106) n = 135;
+	else n = 179;
+	raw_field2 = de_getu16le(jmppos+n);
+
+	d->orig_len = raw_field2 + 1;
+	de_dbg(c, "orig len: %"I64_FMT, d->orig_len);
+
+	d->part2_endpos = raw_field1 + 1;
+	d->part1_pos = d->part2_endpos;
+	d->part2_pos = SM_PREAMBLE_SIZE;
+	d->part3_pos = d->part1_pos + SM_PREAMBLE_SIZE;
+	d->part3_len = jmppos - d->part3_pos;
+	d->part1_len = de_min_int(d->orig_len, SM_PREAMBLE_SIZE);
+	d->part2_len = d->part2_endpos - d->part2_pos;
+
+	de_dbg(c, "part1 at %"I64_FMT", len=%"I64_FMT, d->part1_pos, d->part1_len);
+	de_dbg(c, "part2 at %"I64_FMT", len=%"I64_FMT, d->part2_pos, d->part2_len);
+	de_dbg(c, "part3 at %"I64_FMT", len=%"I64_FMT, d->part3_pos, d->part3_len);
+
+	if(d->part3_len!=0) {
+		d->original_was_exe = 1;
+	}
+
+	if(ver==106) n = 33;
+	else n = 77;
+	x = (UI)de_getu32be(jmppos+n);
+	if(x==0x8bc4050aU) {
+		;
+	}
+	else if(x==0x90909090U) {
+		d->original_was_exe = 1;
+	}
+	else {
+		d->errflag = 1;
+		d->need_errmsg = 1;
+		goto done;
+	}
+
+	de_dbg(c, "orig fmt: %s", (d->original_was_exe?"EXE":"COM"));
+	if(d->original_was_exe && d->raw_mode!=1) {
+		de_err(c, "File was converted from EXE (not supported%s)",
+			((d->raw_mode==0xff) ? ", or use \"-opt spacemaker:raw\"" : ""));
+		d->errflag = 1;
+		goto done;
+	}
+
+	d->dcmpr_code = dbuf_create_membuf(c, d->orig_len, 0x1);
+	dbuf_copy(c->infile, d->part1_pos, d->part1_len, d->dcmpr_code);
+
+	if(d->orig_len > SM_PREAMBLE_SIZE) {
+		sm_decompress(c, d);
+	}
+	if(d->errflag) goto done;
+
+	ext = d->original_was_exe ? "bin" : "com";
+	outf = dbuf_create_output_file(c, ext, NULL, 0);
+	dbuf_copy(d->dcmpr_code, 0, d->dcmpr_code->len, outf);
+
+done:
+	if(outf) {
+		dbuf_close(outf);
+		if(!d->errflag) {
+			de_stdwarn_execomp(c);
+		}
+	}
+}
+
+static void de_run_spacemaker(deark *c, de_module_params *mparams)
+{
+	lctx *d = NULL;
+
+	// TODO: Support EXE->COM and EXE->EXE compression.
+	d = de_malloc(c, sizeof(lctx));
+	d->raw_mode = (u8)de_get_ext_option_bool(c, "spacemaker:raw", 0xff);
+	spacemaker_main(c, d);
+	dbuf_close(d->dcmpr_code);
+	if(d->need_errmsg) {
+		de_err(c, "Bad or unsupported Spacemaker file");
+	}
+	de_free(c, d);
+}
+
+static int de_identify_spacemaker(deark *c)
+{
+	u8 x;
+
+	if(dbuf_memcmp(c->infile, 0,
+		(const void*)"\x9c\x55\x56\x8c\xcd\x83\xc5\x10\x8d\xb6", 10))
+	{
+		return 0;
+	}
+	x = !(u8)dbuf_memcmp(c->infile, 18, (const void*)"MEMORY$", 7);
+	return x?100:85;
+}
+
+static void de_help_spacemaker(deark *c)
+{
+	de_msg(c, "-opt spacemaker:raw : Write raw decompressed data");
+}
+
+void de_module_spacemaker(deark *c, struct deark_module_info *mi)
+{
+	mi->id = "spacemaker";
+	mi->desc = "Realia Spacemaker";
+	mi->run_fn = de_run_spacemaker;
+	mi->identify_fn = de_identify_spacemaker;
+	mi->help_fn = de_help_spacemaker;
+}
